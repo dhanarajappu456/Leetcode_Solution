19bb6719c0ab97afb046b21114034cd64d2aad02
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-09-10 00:45:16 +0530

    Time: 42 ms (82.80%) | Memory: 16.4 MB (71.35%) - LeetSync

--- 909-stone-game/stone-game.py
+++ 909-stone-game/stone-game.py
@@ -1,7 +1,22 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
 
-        return True
+        def rec(i,j):
+
+
+            if i>j:
+                return 0
+
+            if len(j-i+1)%2==0:
+
+                return max(piles[i] + rec(i+1,j) ,  piles[j] + rec(i,j-1) )
+            
+            else:
+                return max(  rec(i+1,j) ,  rec(i,j-1) )
+        n = len(piles)
+        return(0,n-1)
+
+
 
 
         
