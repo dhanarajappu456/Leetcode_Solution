1e22761c57ab6a8ed760af4dda61b52eec41f6cc
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-09-13 20:45:12 +0530

    Time: 119 ms (99.97%) | Memory: 19.2 MB (68.60%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -1,27 +1,44 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
+        
 
-        #sort the (rating , index)
-
-        #process each of them in the sorted ordering 
-
-        #when processing check if the neib value is less than than current value , 
-        #then take in to account candies assigned to neibour  +1 
-
-        arr = [(val,ind) for ind, val  in enumerate(ratings)]
-        arr.sort()
+    
+        '''
+        the idea is  we have to go on giving 1 extra cand y as we find the increasing slope  , 
+        if we find decreasing slope , then we must star incremneting the canding starting from the dip of it(it is same 
+        as incermenting the candy as we go down the decreasing slope)
+        '''
+        
         n =len(ratings)
+        i=1
+        cand = n #1 candy should be alwways given
 
-        candies  =[0 for i in range(n)]
+        while(i<n): 
+            
+            #if it is plateau, then just continue , as already 1 candy has been added to the calulation 
 
-        for val,ind in arr:
-            neib_cand =0
-            if ind-1>=0 and ratings[ind-1]< ratings[ind]:
-                neib_cand = max(neib_cand, candies[ind-1] )
+            if ratings[i-1] ==ratings[i]:
+                i+=1
+                continue
+            #increasing slope
+            peak=0
+            while(i<n and ratings[i-1]<ratings[i]):
+                peak+=1
+                cand += peak
+                i+=1 
+
+                if i ==n:
+                    return cand
+            #decreasing slope
+            dip   = 0
             
-            if ind+1<n and ratings[ind+1]< ratings[ind]:
-                neib_cand = max(neib_cand, candies[ind+1] )
+            while(i<n and ratings[i-1]>ratings[i]):
+                dip+=1
+                cand += dip
+                i+=1 
+
+            #the peak candy is always the max among left and right neib, so remove the min candy at the peak alreasy #added to the candy
+            cand = cand - min(peak,dip)
+        return cand
             
-            candies[ind] = neib_cand+1
-        return sum(candies)
             
