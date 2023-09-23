b5ee647155e60998e234c91519ecad13a20b8840
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-09-23 19:50:41 +0530

    Time: 2438 ms (59.32%) | Memory: 221.1 MB (25.27%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.py
+++ 1522-stone-game-iii/stone-game-iii.py
@@ -4,11 +4,13 @@ class Solution:
         #infact we need  not have to have the 2d state, 
 
         '''
-        as we are asked to find the condition of who wins, 
-        we find the max difference from the perspective of the current player 
-        the idea  is brute for all possibility 
 
-        and the state i -> indicate  the max difference  out of all three possibility of choosing  the  elemeents , you can get for the current player 
+        we brute force , each player selecting the possible piles 
+        ans 
+        as we are asked to find the condition of who wins, 
+        we find the max difference from the perspective of the current player , and return it , 
+
+        ie,  the state i -> indicate  the max difference  out of all three possibility of choosing  the  elements , you can get for the current player 
         this player returns this max diff, and then next player calculate max diff for him/her, using this max diff
         
         '''
@@ -21,15 +23,14 @@ class Solution:
 
             if i >=n :
                 return 0 
-            ans = -float("inf")
-            ans = max( ans, piles[i] -rec(i+1)) 
+            maxDiff = -float("inf")
+            maxDiff = max( maxDiff, piles[i] -rec(i+1)) 
             
             if i+1<n:
-                ans = max(ans, piles[i] + piles[i+1] - rec(i+2)) 
+                maxDiff = max(maxDiff, piles[i] + piles[i+1] - rec(i+2)) 
             if i+2<n :
-
-                ans  = max(ans, piles[i] + piles[i+1]+ piles[i+2]- rec(i+3))
-            return ans 
+                maxDiff  = max(maxDiff, piles[i] + piles[i+1]+ piles[i+2]- rec(i+3))
+            return maxDiff 
         aliceMaxDiff = rec(0)
         if aliceMaxDiff ==0:
             return "Tie"
