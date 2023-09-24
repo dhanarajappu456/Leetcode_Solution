d57de57bfc3842d2b7775a514eb26d6af7a99edd
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-09-24 23:59:58 +0530

    Time: 155 ms (15.38%) | Memory: 16.3 MB (72.75%) - LeetSync

--- 815-champagne-tower/champagne-tower.py
+++ 815-champagne-tower/champagne-tower.py
@@ -1,6 +1,14 @@
 class Solution:
     def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
+        '''
 
+        simulate the pouring 
+
+        we conatruct  2d array representing the glass, 
+
+        now four a glass at (i,j) it can receive the champagne , from two parents, one at (i-1,j-1) , and other at (i-1, j),
+        also  remember, when a glass receive the champagne , from the  parent, it receive the a qunatitiy ,  after the parent took 1 glass, and , which is split into half 
+        '''
 
         glass  = [ [ 0 for j in range(i+1)] for i in range(query_row+1) ]
 
@@ -18,11 +26,14 @@ class Solution:
                     left_liq = glass[i-1][j-1]
 
                 if j<i:
-                    print(i,j)
+           
                     right_liq = glass[i-1][j]
-                
+            
                 glass[i][ j ] = max(0, left_liq -1 )*0.5  + max(0, right_liq -1 )*0.5 
    
+        
+        #remember, the qunatity of champagne at any glass, can't be more than 1 , no matter how much we poured from the above
+        
 
         return min(1,glass[query_row][query_glass] )
         
