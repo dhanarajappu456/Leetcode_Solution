c59cf264d7cbfa0d347eba09af183ea639049a5e
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-09-27 00:54:00 +0530

    Time: 42 ms (60.99%) | Memory: 16.4 MB (18.14%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.py
+++ 316-remove-duplicate-letters/remove-duplicate-letters.py
@@ -1,11 +1,29 @@
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
         
+        '''
+
+        the idea is whene all the  characters are alwats increasing , then that string itself, is the  smallest lexicographical order
+
+
+        but any index if a character is < previous character, then we might remove all the previous character, 
+        which are lesser than the current char iff:
+        a)the current character is new character that is not present in the previously chosen set of character (taken is used for this)
+        and
+        b)the previous character comes again after this current character(that is why , last index store the last occurence of each distinct characater )
+
+        obeying these condition , we can remove the previous character
+        '''
         stk = []
+        #to know if current character  is not present in the set of chosen previous chars, thereny making sure that the 
+        #it is new character, that can  be chosen 
+
         taken = {char:False for char in s}
+        #to determine if the stk top character  ,  appears again after the curr character index, so that top char, can be removed safely 
         lastIndex = {c:i for i,c in enumerate(s)}
     
         for currCharInd , currChar in enumerate(s):
+            #the condition so that the previously chosen character , can be deleted 
             #"abacb" -> expected  = "abc"
             while(stk and ( stk[-1]>currChar and taken[currChar]==False and  lastIndex[stk[-1]]>currCharInd) ) :
 
