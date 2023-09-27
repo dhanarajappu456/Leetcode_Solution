317b259d1c0355dbfe164a39a9d1533a81e94116
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-09-27 23:37:53 +0530

    Time: 30 ms (96.46%) | Memory: 16.2 MB (86.87%) - LeetSync

--- 916-decoded-string-at-index/decoded-string-at-index.py
+++ 916-decoded-string-at-index/decoded-string-at-index.py
@@ -1,8 +1,16 @@
 class Solution:
     def decodeAtIndex(self, s: str, k: int) -> str:
 
-        
+        '''
+        the idea to find the position kth character in each of the multiplied sub-parts
+        for this we calculate the length of entire string , then , we check the position of the required character
+        in each multiplied sub-parts, startng from the entire string length , from backward
+
+
+        '''
         tot_len  = 0
+
+        #calculating the entire string length
         for i,c in enumerate(s):
         
             if c.isdigit():
@@ -11,15 +19,17 @@ class Solution:
             else:
                 tot_len += 1
         
-        
+        #iterating from the end , we find the position k relative to each suparts
         for i in range(len(s)-1, -1 ,-1):
 
-            #if the current character is the  character needed then return it 
+            
 
             c = s[i]
-    
             k%=tot_len 
-
+            #if the current character is the  character needed then return it 
+            #note we need to check the current char is not a digit as well
+            #else it fails in some cases like 
+            # "a23" , k  =6 
             if c.isalpha() and k ==0 :
                 print(tot_len)
                 return c
@@ -28,6 +38,5 @@ class Solution:
                 tot_len//=(int(c))
             else:
                 tot_len-=1
-            #print(k,tot_len)
-            
+        
 
