1f97b5031f1f8ae06f21df17cfd9a4f38f08f038
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-09-28 13:45:02 +0530

    Time: 83 ms (21.04%) | Memory: 17.1 MB (77.37%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.py
+++ 941-sort-array-by-parity/sort-array-by-parity.py
@@ -1,11 +1,16 @@
 class Solution:
+    """
+
+    keep a pointer(i)for pushing in the even numbers at the front and another pointer that iterate(j) on the string as we move 
+
+    """
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
 
 
         n  = len(nums)
         i=0
         for j in range(n):
-
+            #if current number is even push in the number to front , by the positon pointed by the i 
             if nums[j]%2==0:
                 temp =nums[i]
                 nums[i] = nums[j]
