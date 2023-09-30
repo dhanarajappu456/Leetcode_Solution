4c3f202b4cde02c8d6ac8b2c26458cd1272167b7
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-09-30 23:01:26 +0530

    Time: 765 ms (100.00%) | Memory: 22.5 MB (100.00%) - LeetSync

--- 3080-split-array-into-maximum-number-of-subarrays/split-array-into-maximum-number-of-subarrays.py
+++ 3080-split-array-into-maximum-number-of-subarrays/split-array-into-maximum-number-of-subarrays.py
@@ -1,7 +1,18 @@
 class Solution(object):
     def maxSubarrays(self, nums):
         
+        '''
 
+        the crux of the solution lies, in the property of AND
+
+        when we and a couple of numbers one by one , the result is either same as previous result or less than that
+        thus it is always favourable to include all numbers in the sams single array to get the minimum , 
+
+        until and unless when there are 0  or result of the subarray is 0 ,
+
+        when there are zero anding with all the non zero numbres gives 0 as the answer which is the minimum score, 
+        now, essentially we need to split the array , at indices, where we get zero as the answer of anding 
+        '''
 
         curr = (1<<32)-1
         cnt  = 0 
@@ -10,8 +21,7 @@ class Solution(object):
             if curr == 0:
                 cnt+=1
                 curr = (1<<32)-1
-            
-
+        
         return max(1,cnt) 
 
             
