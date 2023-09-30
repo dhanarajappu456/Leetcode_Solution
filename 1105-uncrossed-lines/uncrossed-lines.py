0572204d91f983cb5f59420eb0b55e151c7de149
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-09-30 16:18:35 +0530

    Time: 203 ms (37.44%) | Memory: 24 MB (24.35%) - LeetSync

--- 1105-uncrossed-lines/uncrossed-lines.py
+++ 1105-uncrossed-lines/uncrossed-lines.py
@@ -1,10 +1,18 @@
 class Solution:
     def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
         
+        '''little optimisation , 
+            by making map , that group all the values to the indices, so that we only have to traverse that 
+            list for finiding ind2, not entire array always) 
+            '''
+        m2 =defaultdict(deque)
+        for j in range(len(nums2)) :
+            m2[nums2[j]].append(j)
 
         m,n   = len(nums1) , len( nums2)
         memo  = { }
         def rec(i,j) :
+
             if i>=m or j>=n :
                 return 0 
             if (i,j ) in memo: 
@@ -12,18 +20,28 @@ class Solution:
             tk_ = -1
             not_ = -1 
             max_ =  0 
+          
             ind1=i
+            ind2=-1
             num = nums1[ind1]
-            ind2 =-1
-        
+            if num in m2:
                 
-            for ind in range(n):
-                if nums2[ind]==num and ind>=j:
-                    ind2 = ind
-                    break 
-            if ind2>-1:
-                tk_ = 1+rec(ind1+1, ind2+1 ) 
+                if m2[num]:
+                    
+                    for ind in m2[num]:
+                        if ind >=j :
+                            ind2 = ind
+                            break
+
+
+
+                    if ind2>-1: 
+                  
+                        tk_ = 1+rec(ind1+1, ind2+1 )
+                    m2[num].append(ind2)
             not_ = rec(ind1+1,j)
-            memo[(i, j)] = max(max_ , tk_, not_)
-            return memo[(i, j)]
+            max_ = max(max_ , tk_, not_)
+            memo[(i,j)] = max_
+            return  memo[(i,j)] 
+
         return rec(0, -1)
