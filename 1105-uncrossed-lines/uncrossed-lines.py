680c5d689b14fe381495aaf110533ce1f38521dd
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-10-02 23:26:16 +0530

    Time: 122 ms (40.48%) | Memory: 13.6 MB (41.27%) - LeetSync

--- 1105-uncrossed-lines/uncrossed-lines.py
+++ 1105-uncrossed-lines/uncrossed-lines.py
@@ -9,24 +9,45 @@ class Solution(object):
         '''
 
         m,n  = len(nums1), len(nums2)
-        memo ={}
-        def rec(i,j):
+        # memo ={}
+        # def rec(i,j):
 
-            if i<0 or j< 0:
-                return 0
-            if (i,j) in memo: 
+        #     if i<0 or j< 0:
+        #         return 0
+        #     if (i,j) in memo: 
 
-                return memo[(i,j)]
+        #         return memo[(i,j)]
 
-            if nums1[i] == nums2[j] :
+        #     if nums1[i] == nums2[j] :
 
-                ans =  1+ rec(i-1, j-1)
-            else:
+        #         ans =  1+ rec(i-1, j-1)
+        #     else:
 
-                ans  =  max(rec(i-1, j), rec(i,j-1))
+        #         ans  =  max(rec(i-1, j), rec(i,j-1))
 
-            memo[(i,j)] = ans
-            return memo[(i,j)]
+        #     memo[(i,j)] = ans
+        #     return memo[(i,j)]
         
-        return rec(m-1, n-1)
+        # return rec(m-1, n-1)
+
+
+        dp =[[ 0 for i in range(n+1)] for j in range(m+1)]
+
+
+        for i in range(1,m+ 1): 
+            for j in range(1, n+1 ):
+
+                if nums1[i-1] == nums2[j-1] :
+
+                    ans =  1+ dp[i-1][j-1]
+                else:
+
+                    ans  =  max(dp[i-1][j], dp[i][j-1])
+
+                dp[i][j] = ans
+        return dp[m][n]
+
+
+
+
             
