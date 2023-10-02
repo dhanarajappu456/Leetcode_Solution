23eaa7658f8924bb0550b1b690566a32370f3055
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-10-02 12:45:24 +0530

    Time: 283 ms (8.06%) | Memory: 17.2 MB (62.90%) - LeetSync

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.py
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.py
@@ -1,19 +1,21 @@
 class Solution(object):
     def winnerOfGame(self, colors):
-        
+        '''
+
+        note that , the possibility of favourable   A or B is not going to be increased 
+
+        therefore it is better to count number of A and B  the alice and bob can get in the initial configuraton 
+        and determine which is greater and that plaer wins for sure 
+        '''
         n =len(colors)
         A,B = 0,0
         for i in range(1,n-1):
             c = colors[i]
             if  c== "A" :
                 if i-1>=0 and colors[i-1] == "A" and i+1<=n-1 and colors[i+1] == "A":
-
                     A+=1 
-
             if c== "B":
-
                 if i-1>=0 and colors[i-1] == "B" and i+1<=n-1 and colors[i+1] == "B":
-
                     B+=1 
         return A>B
 
