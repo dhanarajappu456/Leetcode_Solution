52c8e87831ec064c0d98864333bea2c0373a9a64
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-10-10 10:47:14 +0530

    Time: 705 ms (60.53%) | Memory: 35.3 MB (71.05%) - LeetSync

--- 2119-minimum-number-of-operations-to-make-array-continuous/minimum-number-of-operations-to-make-array-continuous.py
+++ 2119-minimum-number-of-operations-to-make-array-continuous/minimum-number-of-operations-to-make-array-continuous.py
@@ -1,19 +1,31 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
+        
+        
+        '''
+        we sort the array (of unique eleements )
 
+        then starting at each index we find longest continous array(values from nums[l] .... nums[l]+n-1)
+        then remaning  elments (n-windowlength) need to be changed to fit into this window.
+
+        minimum among all of them is taken at the end 
+        '''
+        
+        
         #to get only unique elements
         n =len(nums)
-        nums = sorted(set(nums))
-
-      
+        nums = sorted(set(nums))  #nlogn
         r=0 
         ans  = n
-        for l in range(len(nums)):
+        for l in range(len(nums)): #o(n)
             end = nums[l]+n-1
-            while(r<len(nums) and nums[r]<=end):
+            while(r<len(nums) and nums[r]<=end): 
                 r+=1
             windowLen  = r-1-l+1
             ans = min(ans,n - windowLen)
         return ans
      
+
+     #t nlogn
+     #s n (set)
         
