d8bc74f9f0f817b8708f717abc1bb0e8373dde94
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-10-10 16:13:13 +0530

    Time: 415 ms (72.27%) | Memory: 16.2 MB (97.09%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,28 +1,24 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
 
-        S1 ,S2  = s, s[::-1]
-        m,n = len(S1) , len(S2)
-        dp =[ [ "" for j in range(n+1)] for i in range(m+1)]
-        ans = ""
 
-        for i in range(1,m+1):
-            
-            for j in range(1,n+1):
-                
-                if S1[i-1] == S2[j-1]:
-                    
-                    dp[i][j] = dp[i-1][j-1]+S1[i-1]
-                    #ans  = max(ans,dp[i][j])
-                    if len(dp[i][j])> len(ans) and dp[i][j] ==dp[i][j][::-1]:
-                        ans  = dp[i][j]
+        n = len(s)
+        max_len , ans   = 0 ,None
+        for i in range(n):
+            l,r= i,i 
+            while(l>=0 and r<n  and s[l] == s[r]):
+                if max_len < ( r-l+1):
+                    max_len =  ( r-l+1)
+                    ans  = s[l:r+1]
+                l-=1
+                r+=1
+            l,r = i,i+1
+            while(l>=0 and r<n  and s[l] == s[r]):
+                if max_len < ( r-l+1):
+                    max_len =  ( r-l+1)
+                    ans  = s[l:r+1]
+                l-=1
+                r+=1
         return ans
-                    
-     
-                
-
-
-
-
 
 
