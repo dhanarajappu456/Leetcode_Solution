f84cba26be8e2579cc34e19ae58b8a09f4bcd5c7
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-10-11 09:18:32 +0530

    Time: 775 ms (15.76%) | Memory: 54.6 MB (40.59%) - LeetSync

---
+++ 1332-count-vowels-permutation/count-vowels-permutation.py
@@ -0,0 +1,24 @@
+class Solution:
+    def countVowelPermutation(self, n: int) -> int:
+        follow = {'':"aeiou",'a':"e",'e':"ai",'i':"aeou",'o':"iu",'u':"a"}
+
+        memo ={}
+        mod  = 10**9+ 7
+        def rec(char,rem):
+
+            if rem ==0:
+                return 1
+
+
+            if (char , rem ) in memo:
+                return memo[(char, rem)]
+            ans = 0
+
+            for c in follow[char]:
+             
+                ans = (ans + rec(c,rem-1)%mod)%mod
+            memo[(char,rem)] = ans
+            return memo[(char, rem)]
+
+        return rec("",n)%mod
+        
