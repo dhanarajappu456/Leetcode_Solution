6ce6da0a7b1696714cf7b2ececf8454d8e8c3b0f
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-10-11 11:12:32 +0530

    Time: 339 ms (48.26%) | Memory: 16.3 MB (91.35%) - LeetSync

--- 1332-count-vowels-permutation/count-vowels-permutation.py
+++ 1332-count-vowels-permutation/count-vowels-permutation.py
@@ -2,23 +2,45 @@ class Solution:
     def countVowelPermutation(self, n: int) -> int:
         follow = {'':"aeiou",'a':"e",'e':"ai",'i':"aeou",'o':"iu",'u':"a"}
 
-        memo ={}
+        # memo ={}
         mod  = 10**9+ 7
-        def rec(prevChar,rem):
+        # def rec(prevChar,rem):
 
-            if rem ==0:
-                return 1
+        #     if rem ==0:
+        #         return 1
 
 
-            if (prevChar , rem ) in memo:
-                return memo[(prevChar, rem)]
-            ans = 0
+        #     if (prevChar , rem ) in memo:
+        #         return memo[(prevChar, rem)]
+        #     ans = 0
 
-            for c in follow[prevChar]:
+        #     for c in follow[prevChar]:
              
-                ans = (ans + rec(c,rem-1)%mod)%mod
-            memo[(prevChar,rem)] = ans
-            return memo[(prevChar, rem)]
+        #         ans = (ans + rec(c,rem-1)%mod)%mod
+        #     memo[(prevChar,rem)] = ans
+        #     return memo[(prevChar, rem)]
 
-        return rec("",n)
+        # return rec("",n)
+
+        charMap  = {'a':0 , 'e':1, 'i':2,'o':3,'u':4}
+        dp =  [1,1,1,1,1]
+
+        for rem in range(1,n):
+            temp  = [0 for i in range(5)]
+            for prevChar in "aeiou":
+                prev = charMap[prevChar]
+                ans = 0
+                for c in follow[prevChar]:
+                    val  = charMap[c]
+                    ans = (ans + dp[val]%mod)%mod
+                
+                temp[prev] = ans 
+            dp = temp
+    
+        return sum(dp) % mod
+                 
+
+                
+
+            
         
