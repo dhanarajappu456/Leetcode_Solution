e4e5f55a21271a090a65717d75ec2ca8ea11e6ec
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-10-11 12:46:03 +0530

    Time: 959 ms (43.94%) | Memory: 44.9 MB (34.24%) - LeetSync

--- 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.py
+++ 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.py
@@ -3,7 +3,16 @@ import heapq as h
 class Solution:
     def fullBloomFlowers(self, flowers: List[List[int]], people: List[int]) -> List[int]:
 
-       
+        '''
+
+        for each person(sorted based on arrival ) keep  
+        1)flower that bloomed  before  he arrived and  still in bloom and
+        2)  all the flowers that started at the time he arrived. for this we need to have the flowers sorted by start time
+
+        we push these flowers end time to the minHeap so that ,  for next person we might have to pop flowers which have stopped blooming
+
+
+        '''
 
         flowers.sort()
         people_sorted  = sorted(people)
@@ -11,14 +20,11 @@ class Solution:
         n= len(people)
         ptr = 0 
         minHeap = []
-        
-        for i in range(n):
+        for i in range(n): #p
             t = people_sorted[i]
-            while(minHeap and minHeap[0]<t):
+            while(minHeap and minHeap[0]<t): #k*logf
                 h.heappop(minHeap)
-            #print(t, minHeap,minHeap[-1] if minHeap else 0 )
-            while(ptr<len(flowers) and flowers[ptr][0]<= t):
-
+            while(ptr<len(flowers) and flowers[ptr][0]<= t): #klogf
                 #skip all flowers that ended before this t
                 if  flowers[ptr][1]< t:
                     ptr+=1 
@@ -34,6 +40,11 @@ class Solution:
             result[i] = ans[people[i]]
         return result
 
+    '''
+
+    t  =  p *k logf  = , p,f =people and flower, k is average size of heap at anu time 
+    s  =  k , p = heap and ans dictionary size
+    '''
 
            
 
