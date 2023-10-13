5e66f7062585e12386e7b4059280a5166ac13a5e
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-10-13 23:25:31 +0530

    Time: 4650 ms (7.41%) | Memory: 197.4 MB (14.81%) - LeetSync

--- 1393-maximum-value-of-k-coins-from-piles/maximum-value-of-k-coins-from-piles.js
+++ 1393-maximum-value-of-k-coins-from-piles/maximum-value-of-k-coins-from-piles.js
@@ -4,59 +4,58 @@
  * @return {number}
  */
 
-
-var maxValueOfCoins = function(piles, k) {
-
-    const n  = piles.length ;
-
-    const memo = {}
+/*
+we follow take not take approach
 
 
-    function rec(ind , k){
+that is when we  choose top coin from current pile and move to next pile there is no going back to previous pile
 
-
-        if(ind===n || k ===0){
-
-            return 0;
-        }
-
-        const key  = [ind,k ].toString()
-
-        if( key in memo){
-
-            return memo[ key ]
-        } 
-        // not take
-
-
-        let   ans = 0 
-
-
-        ans = Math.max(ans ,   rec(ind+1, k))
-        let  currPileCoin =0 
-
-        for(let i = 0 ; i<Math.min(k,piles[ind].length); i++){
-
-                currPileCoin += piles[ind][i]
-               
-ans = Math.max(ans, currPileCoin + rec(ind + 1, k-(i+1)));
-
-            }
-
-
-        
-
-        memo [  key ] =  ans
-  
-        return ans
+1)take
+that is we take top coin from cuurent pile move to next index with k reduced, or take top 2 coin from current pile and move to next pile  and so on 
 
 
 
+t  n*k* k = n*(k^2)
+s n*k
 
+
+2)
+
+or we never conside taking top cooin from current pile
+
+*/
+var maxValueOfCoins = function (piles, k) {
+  const n = piles.length;
+
+  const memo = {};
+
+  function rec(ind, k) {
+    if (ind === n || k === 0) {
+      return 0;
     }
 
+    const key = [ind, k].toString();
 
-    return rec(0 , k)
+    if (key in memo) {
+      return memo[key];
+    }
+    // not take  any coin from current pile
 
+    let ans = 0;
 
+    ans = Math.max(ans, rec(ind + 1, k));
+    let currPileCoin = 0;
+    //take one or more coin from current pile
+    for (let i = 0; i < Math.min(k, piles[ind].length); i++) {
+      currPileCoin += piles[ind][i];
+
+      ans = Math.max(ans, currPileCoin + rec(ind + 1, k - (i + 1)));
+    }
+
+    memo[key] = ans;
+
+    return ans;
+  }
+
+  return rec(0, k);
 };
