8c2e725b8a4cd9480740c8281be142f012bb5632
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-10-16 23:32:06 +0530

    Time: 632 ms (16.35%) | Memory: 149.7 MB (10.90%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.py
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.py
@@ -1,52 +1,56 @@
 class Solution:
     def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
         
-        rev= str2[::-1]
-        m,n  = len(str1) ,len(str2)
-        dp = [[0 for j in range(n+1)]  for i in range(m+1)]
+        @lru_cache(None)
+        def rec(i,j) :
+
+
+            if i == 0 or j == 0:
+                return ""
+
+            if str1[i-1] == str2[j-1]:
+               
+                return (rec(i-1, j-1) + str1[i-1])
+            else:
+                choice1 = rec(i-1, j)
+                choice2 = rec(i, j-1)
+
+                if len(choice1) <= len(choice2):
+                    return choice2
+                else:
+                    return choice1
+
+  
+
         
-        ans =[]
-
-        for i in range(1,m+1):
-          
-            for  j in range(1,n+1):
-                if str1[i-1] == str2[j-1]:
-                    
-                    dp[i][j] = 1+dp[i-1][j-1]
-                else: 
-                    dp[i][j] =max(dp[i-1][j] , dp[i][j-1])
-         
-        #print(dp)
+        m,n = len(str1),len(str2)
+        lcs =  rec(m,n)
+        i= 0 
+        p1,p2 = 0,0
+        ans = ""
+     
+        while(i<len(lcs)):
             
-   
-        while(i>0 and j>0):
+            while(p1<m and( lcs[i]!=str1[p1])):
+                print("p1",str1[p1],p1,lcs[i],i)
+                ans+=str1[p1]
+                p1+=1
 
-                if str1[i-1] == str2[j-1]:
+            while(p2<n and (lcs[i]!=str2[p2])):
+                print("p2",str2[p2],p2,lcs[i],i)
+                ans+=str2[p2]
+                p2+=1
+            
+            ans+=lcs[i]
+            p1+=1
+            p2+=1
+            i+=1
+        return ans+str1[p1:] + str2[p2:]
 
-                    
-                    ans.append(str1[i-1])
-                    i-=1
-                    j-=1
 
-                else: 
-                    if dp[i-1][j]>=dp[i][j-1]:
+                
 
-                        ans.append(str1[i-1])
-                        i-=1
 
-                    else:           
-                        ans.append(str2[j-1])
-                        j-=1
-        # print(i,j)
-        while(i>0):
-            ans.append(str1[i-1])
-            i-=1
-
-        while(j>0):
-            ans.append(str2[j-1])
-            j-=1
-    
-        return "".join(ans[::-1])
 
                         
                        
