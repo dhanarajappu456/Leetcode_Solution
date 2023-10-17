09429ae32cd83ee9c33c9e7822780f010bcab615
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-10-17 11:08:02 +0530

    Time: 269 ms (65.27%) | Memory: 22.4 MB (25.45%) - LeetSync

--- 1275-validate-binary-tree-nodes/validate-binary-tree-nodes.py
+++ 1275-validate-binary-tree-nodes/validate-binary-tree-nodes.py
@@ -1,5 +1,19 @@
 from collections import defaultdict as dict
+'''
 
+a binary tree has n-1 edges and n nodes , with no cycle , this also ensures that there exist only one path b/w each pair of vertices 
+
+
+
+1)ensuring a single path b/w all pair of vertices  - check if indegree of any node >1, which means it is nor a bin tree
+2) count total edges is n-1
+3)if all the above is true , check if it has cycle 
+
+if all these is obeyed then  it is a binary tree
+
+
+
+'''
 class Solution:
     def validateBinaryTreeNodes(self, n: int, leftChild: List[int], rightChild: List[int]) -> bool:
 
@@ -7,50 +21,32 @@ class Solution:
         indegree =[0 for i in range(n)]
 
         adj = dict(list)
-
-
-
-        
         edgeCount  = 0
         #check if indegree  of any node is more than 1 , in which case is not a tree
     
         for i in range(n):
-
             if leftChild[i]!=-1:
-
                 indegree[leftChild[i]]+=1
                 edgeCount+=1
                 if indegree[leftChild[i]]>1:
-                    
-                    #print("indegree")
                     return False
 
             if rightChild[i]!=-1:
                 indegree[rightChild[i]]+=1
                 edgeCount+=1
-
                 if indegree[rightChild[i]]>1:
-                 
-                    #print("indegree")
                     return False
         
         if edgeCount!=n-1:
-            #print("edge",edgeCount)
             return False
 
-        #if all these true now check if there is cycle
-
-
+        #if all these true now check if there is cycle - in  case cycle , exits,  it is not truw
         cycle  ={}
 
         def rec(node):
 
-            
             if  node  in cycle :
-                print("node",node)
                 return cycle[node] 
-            
-
             cycle[node] = True
 
             
@@ -58,24 +54,14 @@ class Solution:
             r = rightChild[node]
             left,right  = False , False
             if l!=-1:
-                
-            
                 left = rec(l)
             if r!=-1:
-                
                 right  = rec(r)
-            
-            
-            #print("node",node,left,right)
             cycle[node] =  left or right 
-
             return cycle[node]
         
         for i in range(n):
-
             if rec(i):
-                #print(i)
-                #print(cycle)
                 return False
         return True
 
