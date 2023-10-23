955a3c47750c9075e666eaa65b7d6d10f0bb72e6
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-10-23 17:11:22 +0530

    Time: 136 ms (57.14%) | Memory: 82.4 MB (7.14%) - LeetSync

--- 1918-maximum-score-of-a-good-subarray/maximum-score-of-a-good-subarray.js
+++ 1918-maximum-score-of-a-good-subarray/maximum-score-of-a-good-subarray.js
@@ -9,47 +9,47 @@ var maximumScore = function(nums, k) {
     prevSmall = Array(n).fill(0)
     nextSmall = Array(n).fill(0)
     stk =[[-1,-1]]
-        for(let i = 0 ;i<nums.length ;i++){
+    for(let i = 0 ;i<nums.length ;i++){
 
-            num  = nums[i]
-            while(stk && stk[stk.length-1][1]>= num){
+        num  = nums[i]
+        while(stk && stk[stk.length-1][1]>= num){
 
 
-                stk.pop()
-            }
-            
-            prevSmall[i] = stk[stk.length-1][0]
-            stk.push([i,num])
+            stk.pop()
+        }
+        
+        prevSmall[i] = stk[stk.length-1][0]
+        stk.push([i,num])
+
+    }
+        
+    stk =[[n,-1]]
+    for(let i= nums.length-1;i>=0 ; i--){
+
+        num  = nums[i]
+        
+        while(stk &&  stk[stk.length-1][1]>= num){
+            stk.pop()
+        
 
         }
             
-        stk =[[n,-1]]
-        for(let i= nums.length-1;i>=0 ; i--){
+        nextSmall[i] =  stk[stk.length-1][0]
+        stk.push([ i,num])
+    }
+        
+    let ans =  nums[k]
+    for(let p = 0; p<nums.length;p++){
+        num  = nums[ p]
+        i = prevSmall[p]+1
+        j = nextSmall[p]-1
+        //if the subarray is good subarray , take the score into accounte 
+        if (i<=k && k<=j){
+            ans =Math.max(ans, num *(j-i+1))
 
-            num  = nums[i]
-            
-            while(stk &&  stk[stk.length-1][1]>= num){
-                stk.pop()
-            
-
-            }
-              
-            nextSmall[i] =  stk[stk.length-1][0]
-            stk.push([ i,num])
         }
+    }
             
-        let ans =  nums[k]
-        for(let p = 0; p<nums.length;p++){
-            num  = nums[ p]
-            i = prevSmall[p]+1
-            j = nextSmall[p]-1
-            //if the subarray is good subarray , take the score into accounte 
-            if (i<=k && k<=j){
-                ans =Math.max(ans, num *(j-i+1))
-
-            }
-        }
-               
-        return ans
+    return ans
     
 };
