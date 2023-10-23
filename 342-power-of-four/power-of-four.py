c12727506abc275b974d83c13f4fa47164218029
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-10-23 11:54:14 +0530

    Time: 44 ms (26.07%) | Memory: 16.2 MB (39.09%) - LeetSync

--- 342-power-of-four/power-of-four.py
+++ 342-power-of-four/power-of-four.py
@@ -1,12 +1,17 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
-        if n<=0:
+        '''
+
+        any num which is power  of 4 has property , 
+        1) it is power of 2  and
+        2) num-1 is mul(3)
+
+        '''
+        
+        if n<=0: 
             return False
-        
 
-        return math.log(n,4)%1 ==0
-
-        
+        return  n&(n-1 ) ==0   and (n-1)%3==0
 
         
 
