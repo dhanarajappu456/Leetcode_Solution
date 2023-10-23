f05b67e6ad40b73661633442a1b5a09c8b95cd45
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-10-23 15:28:50 +0530

    Time: 1325 ms (5.48%) | Memory: 34.6 MB (5.49%) - LeetSync

--- 1918-maximum-score-of-a-good-subarray/maximum-score-of-a-good-subarray.py
+++ 1918-maximum-score-of-a-good-subarray/maximum-score-of-a-good-subarray.py
@@ -1,13 +1,42 @@
 class Solution:
     def maximumScore(self, nums: List[int], k: int) -> int:
-        ans = mn = nums[k]
-        lo = hi = k
-        while 0 <= lo-1 or hi+1 < len(nums): 
-            if lo == 0 or hi+1 < len(nums) and nums[lo-1] < nums[hi+1]: 
-                hi += 1
-                mn = min(mn, nums[hi])
-            else: 
-                lo -= 1
-                mn = min(mn, nums[lo])
-            ans = max(ans, mn * (hi-lo+1))
-        return ans 
+
+        n = len(nums)
+        prevSmall = [0 for i in range(n)]
+        nextSmall = [0 for i in range(n)]
+
+
+        stk =[(-1,-1)]
+
+
+        for i ,num in enumerate(nums):
+            while(stk and stk[-1][1]>= num):
+                stk.pop(-1)
+            
+            prevSmall[i] = stk[-1][0]
+            stk.append((i,num))
+        stk =[(n,-1)]
+        for i ,num in reversed( list(enumerate(nums))):
+            while(stk and stk[-1][1]>= num):
+                stk.pop(-1)
+            
+            nextSmall[i] = stk[-1][0]
+            stk.append(( i,num))
+        #print(prevSmall , nextSmall)
+
+
+        
+
+        ans =  nums[k]
+
+        for p ,  num in enumerate(nums):
+
+            i = prevSmall[p]+1
+            j = nextSmall[p]-1
+
+            if i<=k<=j:
+                #print(i,j, k ,num *(j-i+1))
+                ans =max(ans, num *(j-i+1))
+        return ans
+
+        
