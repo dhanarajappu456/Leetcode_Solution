39048c79957b12d0b6cd3328aa73687e43e3cde4
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-10-23 15:31:37 +0530

    Time: 1287 ms (5.48%) | Memory: 34.4 MB (5.49%) - LeetSync

--- 1918-maximum-score-of-a-good-subarray/maximum-score-of-a-good-subarray.py
+++ 1918-maximum-score-of-a-good-subarray/maximum-score-of-a-good-subarray.py
@@ -1,14 +1,17 @@
 class Solution:
     def maximumScore(self, nums: List[int], k: int) -> int:
+        '''
 
+            find the index of prevsmall and next small number  for each number 
+
+            then iterate on the nums array , and each num as minimum of the  subarray and score  and also including k 
+            find the score and store max score
+
+        '''
         n = len(nums)
         prevSmall = [0 for i in range(n)]
         nextSmall = [0 for i in range(n)]
-
-
         stk =[(-1,-1)]
-
-
         for i ,num in enumerate(nums):
             while(stk and stk[-1][1]>= num):
                 stk.pop(-1)
@@ -22,20 +25,12 @@ class Solution:
             
             nextSmall[i] = stk[-1][0]
             stk.append(( i,num))
-        #print(prevSmall , nextSmall)
-
-
-        
-
         ans =  nums[k]
-
         for p ,  num in enumerate(nums):
-
             i = prevSmall[p]+1
             j = nextSmall[p]-1
-
+            #if the subarray is good subarray , take the score into account
             if i<=k<=j:
-                #print(i,j, k ,num *(j-i+1))
                 ans =max(ans, num *(j-i+1))
         return ans
 
