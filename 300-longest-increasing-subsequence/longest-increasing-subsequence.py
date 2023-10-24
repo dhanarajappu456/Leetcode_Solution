56cf866a7939f3897379ca4e50c7ecb96fef0106
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-10-24 21:15:00 +0530

    Time: 8515 ms (5.00%) | Memory: 145.6 MB (5.18%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,19 +1,18 @@
+
+
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         
         n= len(nums)
         
       
-        next_ = [0 for j in range(n+1)] 
+        dp =[[0 for j in range(n+1)] for i in range(n+1)]
         
         for ind in range(n-1, -1,-1):
-            temp =  [0 for j in range(n+1)] 
-            for preInd in range(-1,ind):
+            for preInd in range(0,ind+1):
                 take= float("-inf")
-                if preInd==-1 or nums[ind]>nums[preInd]:
-                    take   = 1+ next_[ind+1]
-                notTake = next_[preInd+1]
-                temp[preInd+1] =  max(take,notTake)
-            next_ =temp
-        return next_[0]
-
+                if preInd== 0 or nums[ind]>nums[preInd-1]:
+                    take   = 1+ dp[ind+1][ind+1]
+                notTake = dp[ind+1][preInd]
+                dp[ind][preInd] =  max(take,notTake)
+        return dp[0][0]
