ad15dbfec72cfd6184079b6f1f0ec07917154daf
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-10-24 22:02:00 +0530

    Time: 64 ms (97.34%) | Memory: 16.5 MB (75.73%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -2,20 +2,15 @@ import bisect as bs
 
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-
-
         Lis  = []
-
         ans = 0 
         for i ,num in enumerate(nums):
             ind = bs.bisect_left(Lis,num)
             if ind <len(Lis):
-
                 Lis[ind] = num
             else:
                 Lis.append(num)
             ans = max(ans, len(Lis))
-      
         return ans 
 
         
