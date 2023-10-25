5410ebb8fff6854fa61bb4729e57d5dcdf365def
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-10-25 14:43:34 +0530

    Time: 91 ms (11.55%) | Memory: 59.9 MB (19.27%) - LeetSync

--- 1286-constrained-subsequence-sum/constrained-subsequence-sum.java
+++ 1286-constrained-subsequence-sum/constrained-subsequence-sum.java
@@ -1,30 +1,48 @@
 class Solution {
     public int constrainedSubsetSum(int[] nums, int k) {
-        int n = nums.length;
-        int[] t = Arrays.copyOf(nums, n);
-        int maxR = t[0];
-        Deque<Integer> deq = new ArrayDeque<>();
-        
-        for (int i = 0; i < n; i++) {
-            // First get rid of out of range indices
-            while (!deq.isEmpty() && deq.peekFirst() < i - k) {
-                deq.pollFirst();
-            }
-            
-            if (!deq.isEmpty()) {
-                t[i] = Math.max(t[i], nums[i] + t[deq.peekFirst()]);
-            }
-            
-            // Maintain the deque in descending order
-            // So that you can get the optimum value at once from the front
-            while (!deq.isEmpty() && t[i] >= t[deq.peekLast()]) {
-                deq.pollLast();
-            }
-            
-            deq.offerLast(i);
-            maxR = Math.max(maxR, t[i]);
+    int n = nums.length;
+
+    PriorityQueue<int[]> maxHeap = new PriorityQueue<>((int[] a, int[] b)->{ return b[0]-a[0];});
+
+
+    int [] dp  = new int[n];
+
+    int ans =Integer.MIN_VALUE;
+
+    for(int i=0 ; i<n;i++){
+        int j = i-1 ; 
+        dp[i] = nums[i];
+        // while(j>=0){
+
+        //     dp[i] = Math.max(dp[i], nums[i]+ dp[j]);
+        //     j-=1;
+        // }
+
+        while(maxHeap.size()> 0 && i-maxHeap.peek()[1]>k){
+
+                maxHeap.poll();
+
+
         }
-        
-        return maxR;
+
+        if( maxHeap.size()>0){
+
+            dp[i] =Math.max(dp[i], nums[i]+ maxHeap.peek()[0]);
+        }
+        ans  = Math.max(ans, dp[i]);
+
+        maxHeap.offer(new int[]{dp[i], i});
+
     }
+
+    return ans ; 
+   
+    
+    
+    } 
+
+
+
+        
+    
 }
