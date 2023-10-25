afd1e6102eb1ca47db344eb708d2fa03b58d999d
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-10-25 11:51:00 +0530

    Time: 37 ms (64.05%) | Memory: 16.3 MB (50.00%) - LeetSync

--- 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.py
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.py
@@ -11,24 +11,21 @@ class Solution:
         i,j =0,0
         curr = 0
         while(n>0):
-
+            #move left
+            #if the  k is less than or =  than half the number of nodes in the nth row , then , move left
             if k<=2**(n-1):
 
                 i,j = i+1, 2*j
                 curr = 0  if curr ==0 else 1
-                #move left
-
-
+               
+            #move right 
+            #if the  k is less  > than half the number of nodes in the nth row , then , move right
             else:
                 i,j = i+1, 2*j+1
                 k = k-2**(n-1)
                 curr = 1 if curr ==0 else 0  
             ans = curr
-
-
-
-
-
+            # height reduces by 1 in the new graph
             n-=1
            
         return ans
