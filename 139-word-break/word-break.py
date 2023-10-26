6c9ec5a6b98db84c9ba4b67ca2f8bb8b9e8e06d9
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-10-26 14:10:42 +0530

    Time: 68 ms (5.25%) | Memory: 18 MB (5.05%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,25 +1,62 @@
 '''
 tabulation 
-
-
 the state dp[i] = indicate if we can partition the word from i to end , such that the all the words fall in the dictionary 
+the same tabulation with same complexity , but instead of dictionary lookup we use trie
 '''
+
+
+class Node:
+
+    def __init__(self):
+
+
+        self.end=False
+        self.dic  = {}
+
+class Trie:
+    def __init__(self):
+
+        self.head =Node()
+
+    def present(self,word):
+        temp = self.head
+        for ch in word:
+            if ch not in temp.dic:
+                return False
+            temp = temp.dic[ch]
+        
+        return temp.end
+    
+    def insert(self,word):
+        temp = self.head
+        for ch in word:
+            if ch not in temp.dic:
+                temp.dic[ch] =Node()
+            temp = temp.dic[ch]
+        
+        temp.end = True
 
 
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         
 
+
         n = len(s) 
         dp = [False for i in range(n+1)]
         dp[n]= True
-
         wordDict  = set(wordDict)
+
+        trie = Trie()
+        '''insert all the words in wordsDict to trie)
+        '''
+        for word in wordDict:
+            trie.insert(word)
+        
         
         for st in range(n-1,-1,-1):
-
             for end in range(st,n):
-                if s[st:end+1] in wordDict:  #this lookup takes length of substring time complexity = st-end+1
+                if trie.present(s[st:end+1] ): # this takes same time complexity as of dictioanry lookup , which is end-st+1, 
                     if dp[end+1]:
                         dp[st] = True
                         break
