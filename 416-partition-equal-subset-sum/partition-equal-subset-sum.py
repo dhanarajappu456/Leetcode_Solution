a547a2dbcf99d6b517be6b6f6ded144589f7a66b
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-10-26 16:20:50 +0530

    Time: 457 ms (76.14%) | Memory: 16.5 MB (81.86%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -48,22 +48,39 @@ class Solution:
         #         dp[i][tar] =  ans | dp[i+1][tar]
         # return dp[0][tot//2]
 
+
+        '''
+        as wei iterate from back , we check if the current index can form target, with any of the subset sum obtained so far
+        we store the all the possible , subset sum  found so far  in a set(as there can be duplicate)
+        also note we might need only subset sum< target to be stored in to the set
+        '''
         tot =sum(nums)
         if tot%2 !=0 :
             return False
         n = len(nums)
+        #stores all possible subset sum from i+1 to end
         dp ={0}
         target =tot//2
-        for i in range(n-1,-1,-1):
+        for i in range(n-1,-1,-1):#n
+
             nextDp =set()
 
-            for tar in dp :
+            for tar in dp : #target
                 if target==tar+nums[i]:
                     return True
-                
-                nextDp.add(tar+nums[i])
+                #we need to keep  sunbset sum which are less than target so, that we can 
+                #form the target in future
+                if tar+nums[i]<target:
+                    nextDp.add(tar+nums[i])
                 nextDp.add(tar)
             dp = nextDp
+            '''
+            t n*target
+            s target(size of the dp ) since we only store sums that are less than target in the set,  the size is bounded by target
+
+            '''
+        
+
         
 
 
