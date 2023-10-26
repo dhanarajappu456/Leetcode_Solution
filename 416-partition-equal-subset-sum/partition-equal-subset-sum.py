32727cf1a1f951b881a81a58ef720f3f983d5d55
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-10-26 15:58:09 +0530

    Time: 409 ms (81.91%) | Memory: 17.6 MB (72.01%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,12 +1,14 @@
+
+
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
 
         
         # memo ={}
-        tot =sum(nums)
-        if tot%2 !=0 :
-            return False
-        n = len(nums)
+        # tot =sum(nums)
+        # if tot%2 !=0 :
+        #     return False
+        # n = len(nums)
         # def rec(i, target):
 
             
@@ -28,20 +30,44 @@ class Solution:
 
 
 
+        # tot =sum(nums)
+        # if tot%2 !=0 :
+        #     return False
+        # n = len(nums)
+        # # dp =[[True if t==0 else False for t in range(tot//2+1)  ] for i in range(n+1)]
+        # dp[n][0] = True
 
-        dp =[[True if t==0 else False for t in range(tot//2+1)  ] for i in range(n+1)]
-        dp[n][0] = True
+        # n = len(nums)
+        # for i in range(n-1,-1,-1):
 
-        n = len(nums)
-        for i in range(n-1,-1,-1):
-
-            for tar in range(1,tot//2+1):
-                ans = False
-                if tar-nums[i]>=0:
-                    ans |=dp[i+1][tar-nums[i]]
+        #     for tar in range(1,tot//2+1):
+        #         ans = False
+        #         if tar-nums[i]>=0:
+        #             ans |=dp[i+1][tar-nums[i]]
                 
-                dp[i][tar] =  ans | dp[i+1][tar]
-        return dp[0][tot//2]
+        #         dp[i][tar] =  ans | dp[i+1][tar]
+        # return dp[0][tot//2]
+
+        tot =sum(nums)
+        if tot%2 !=0 :
+            return False
+        n = len(nums)
+        dp ={0}
+        target =tot//2
+        for i in range(n-1,-1,-1):
+            nextDp =set()
+
+            for tar in dp :
+                if target==tar+nums[i]:
+                    return True
+                
+                nextDp.add(tar+nums[i])
+                nextDp.add(tar)
+            dp = nextDp
+        
+
+
+
 
 
 
