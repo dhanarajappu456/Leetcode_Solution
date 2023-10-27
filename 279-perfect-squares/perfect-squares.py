3070b9327e3e57910021e7c774614b6cf6389044
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-10-27 14:46:26 +0530

    Time: 4119 ms (21.24%) | Memory: 27.7 MB (15.11%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.py
@@ -0,0 +1,26 @@
+
+
+
+class Solution:
+    def numSquares(self, n: int) -> int:
+        memo = [-1 for i in range(n+1)]
+
+
+        def rec(num):
+
+            if  num ==0:
+                return 0
+            elif num<0:
+                return float("inf")
+            if memo[num]!=-1:
+                return memo[num ]
+            ans = float("inf")
+            for i in range(1,int(math.sqrt(num))+1):
+                sqr = i*i
+                ans = min(ans, 1+rec(num-sqr)) 
+            memo[num ] =  ans
+            return memo[num ]
+
+        return rec(n)
+
+
