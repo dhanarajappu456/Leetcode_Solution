bcd7487b843f4dcb934420a3398921a360841aa3
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-10-28 20:49:18 +0530

    Time: 1051 ms (7.68%) | Memory: 19.4 MB (98.04%) - LeetSync

--- 472-concatenated-words/concatenated-words.py
+++ 472-concatenated-words/concatenated-words.py
@@ -2,23 +2,23 @@ class Solution:
     def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
 
         words = set(words)
+        #this is dp problem word break 
         def wordPresentChecker(word):
-            n = len(word)
-            dp = [False for i in range(n+1)]
-            dp[n] = True
+            m = len(word)
+            dp = [False for i in range(m+1)]
+            dp[m] = True
         
-            for ind in range(n-1,-1,-1):
-                for j in range(ind,n):
+            for ind in range(m-1,-1,-1):
+                for j in range(ind,m):
+                    #check edge case when the  entire partion is equal to the word itself, 
                     if (word[ind:j+1]!= word )and (word[ind:j+1] in words):
-                        if word == "cat":
-                            print(ind, j)
                         if dp[j+1]:
                             dp[ind] = True
                             break
             return dp[0]
         
 
-        m = len(words)
+    
         ans = []
         for word in words: 
             present =wordPresentChecker(word)
