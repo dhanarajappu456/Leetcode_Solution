00517e2f25c8c3ebc4e1ec2447233d5fef4bce92
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-10-28 11:34:05 +0530

    Time: 58 ms (26.62%) | Memory: 16.7 MB (41.14%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.py
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.py
@@ -1,4 +1,8 @@
+'''
 
+
+use the state (ind, days) ->indicate min code to travel all the days in array from ind to end, with days number of days i can cover still
+'''
 from collections import defaultdict as dict
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
@@ -6,22 +10,29 @@ class Solution:
 
         n = len(days)
         memo = dict(int)
-        def rec(ind,d):
+        daysList = [1,7,30]
+        tickets = list(zip(costs, daysList))
+        def rec(ind): #runs n times 
 
             if ind  ==n:
                 return 0 
-            if (ind,d) in memo:
-                return memo[(ind,d)]
+            if  ind  in memo:
+                return memo[ind]
+        
+            ans = float("inf")
+            #for each ticket, see how many days you 
+            #can travel from the current index and call recursively for next day which you cant suffice with 
+            #the ticket
+            for cost,day_can_travel in tickets: # for loop and while together runs for,  1  + 7+ 230 = 38 times
 
-            if days[ind]<=d:
-                memo[(ind,d)]  = rec(ind+1,d)
-            else:
+                i = ind
+                while(i<n and days[i]<=days[ind]+day_can_travel-1 ):
 
-                 memo[(ind,d)] =  min(
+                    i+=1
+                ans = min(ans, cost+ rec(i))
+            memo[ind] = ans
+            return ans
+        return rec(0)
 
-                    costs[0]+  rec(ind+1,days[ind]),
-                    costs[1] + rec(ind+1, days[ind]+ 6),
-                    costs[2] + rec(ind+1,days[ind]+29)
-                )
-            return memo[(ind,d)]
-        return rec(0,0)
+        t: n + 38 = 365 *38
+        s: n  + aux
