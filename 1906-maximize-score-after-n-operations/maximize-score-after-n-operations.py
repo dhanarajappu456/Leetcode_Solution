d74187bffe53dd74c9038a002bd80d2772217f4a
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-10-31 18:43:24 +0530

    Time: 2231 ms (32.35%) | Memory: 26 MB (48.95%) - LeetSync

--- 1906-maximize-score-after-n-operations/maximize-score-after-n-operations.py
+++ 1906-maximize-score-after-n-operations/maximize-score-after-n-operations.py
@@ -1,12 +1,25 @@
+
+
+'''
+
+bruteforce memoisation  - using bitmask
+we can choose any pair of number at any instant and calculate the gcd, 
+at step1 we can make n^2 pair to calulate the step * gcd(nums[j]*nums[k ])
+and then we can choose to make pairs in next step, with remaining numbers, 
+
+but the step and same set of remaining numbers be repeated again and again ,which we can cache
+the state (i,mask ) -> store max ans, when we are at ith step and mask denote remaining 
+set of numbers we have in the nums array
+
+
+
+'''
 class Solution:
     def maxScore(self, nums: List[int]) -> int:
         
-
         n = len(nums)
         mask  = (1<<n)-1
         memo ={}
-
-
         def rec(i,mask):
           
             if mask ==0:
@@ -14,7 +27,7 @@ class Solution:
             if (i,mask) in memo :
                 return memo[(i,mask)]
             ans = 0 
-            for j in range(n-1):
+            for j in range(n-1): 
                 if ((1<<(n-1-j))&mask)!=0:
                     for k in range(j+1,n):
                         if ((1<<(n-1-k))&mask)!=0:
@@ -24,3 +37,9 @@ class Solution:
             memo[(i,mask)] = ans 
             return ans 
         return rec(1,mask)
+'''
+n = len(nums)
+
+t n*masK *(n^2) = n*(2^n)*n^2  = 14* 2^14  * 14 ^2 = 10^7, range of i , mask , and inner loop 
+s  n*(2^n) +  aux
+'''
