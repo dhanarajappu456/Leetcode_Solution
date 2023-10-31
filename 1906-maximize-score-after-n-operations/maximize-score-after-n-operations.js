5e29e6df3f4e1075d5662d62a5846121b8d3369a
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-10-31 18:46:54 +0530

    Time: 576 ms (34.61%) | Memory: 63.3 MB (30.77%) - LeetSync

---
+++ 1906-maximize-score-after-n-operations/maximize-score-after-n-operations.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxScore = function(nums) {
+     const n = nums.length;
+        const mask = (1 << n) - 1;
+        const memo = new Map();
+
+        function rec(i, mask) {
+            if (mask === 0) {
+                return 0;
+            }
+            if (memo.has(`${i}-${mask}`)) {
+                return memo.get(`${i}-${mask}`);
+            }
+
+            let ans = 0;
+            for (let j = 0; j < n - 1; j++) {
+                if ((1 << (n - 1 - j)) & mask) {
+                    for (let k = j + 1; k < n; k++) {
+                        if ((1 << (n - 1 - k)) & mask) {
+                            let newMask = mask & ~(1 << (n - 1 - j));
+                            newMask = newMask & ~(1 << (n - 1 - k));
+                            ans = Math.max(ans, i * gcd(nums[j], nums[k]) + rec(i + 1, newMask));
+                        }
+                    }
+                }
+            }
+
+            memo.set(`${i}-${mask}`, ans);
+            return ans;
+        }
+
+        return rec(1, mask);
+};
+
+
+
+
+function gcd(a, b) {
+    return b === 0 ? a : gcd(b, a % b);
+}
