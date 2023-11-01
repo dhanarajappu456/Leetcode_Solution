af9de071c689a0a10b8385ce1835b0a918e0c5b9
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-11-01 11:41:38 +0530

    Time: 42 ms (99.34%) | Memory: 20.1 MB (99.02%) - LeetSync

--- 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.py
+++ 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.py
@@ -1,35 +1,44 @@
-from collections import defaultdict as dict
+'''
+inorder 
+________________
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+if we ke have sortede order of the elements, then we can find the top occuring elements easily , since they are adjacent 
+that is if prev element same as current element , then we increase the count , and when the count becomes same as max so far  we add this element to the ans list , 
+if the count exceed the max sofar ,we got new max and all elements added to list is cleared and add current element
+
+
+for getting  sorted order of elements , we use inorder 
+'''
 class Solution:
     def findMode(self, root: Optional[TreeNode]) -> List[int]:
+        ans = []
+        count = 0
+        max_ = 0
+        prev = 10**5 + 1
 
-        count = dict(int)
-        li =[]
-        top = 0
         def rec(root):
-
-            if root ==None:
-                return 
-            nonlocal top;
-            li.append(root.val)
-            count[root.val]+=1
-            if count[root.val]>top:
-                top = count[root.val]
-
+            if root is None:
+                return
+            nonlocal count, max_, prev, ans 
             rec(root.left)
+            #finding count of current element 
+            if prev == root.val:
+                count += 1
+            #if current element is ocurring for first time  count stated from 1
+            else:
+                count = 1
+
+            #if count becomes == max ,then current element added to ans list
+            if count == max_:
+                ans.append(root.val)
+            #if is more than  max_ , then this is the only top icccuring element , so clear all 
+            #prev elemetn 
+            elif count > max_:
+                ans = [root.val]
+                max_ = count
+            #updating prev = current elemetn
+            prev = root.val
             rec(root.right)
-        
+
         rec(root)
-      
-        ans =[]
-        for item in count:
-            if count[item] == top: 
-                ans.append(item)
-        return ans 
-        
+        return ans
