da5bbefc2d3567bcdb0ccde2a4921698f474e050
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-11-01 11:05:05 +0530

    Time: 58 ms (47.28%) | Memory: 20.9 MB (19.06%) - LeetSync

---
+++ 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.py
@@ -0,0 +1,35 @@
+from collections import defaultdict as dict
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findMode(self, root: Optional[TreeNode]) -> List[int]:
+
+        count = dict(int)
+        li =[]
+        top = 0
+        def rec(root):
+
+            if root ==None:
+                return 
+            nonlocal top;
+            li.append(root.val)
+            count[root.val]+=1
+            if count[root.val]>top:
+                top = count[root.val]
+
+            rec(root.left)
+            rec(root.right)
+        
+        rec(root)
+        freq = Counter(li)
+        ans =[]
+        for item in freq:
+            if freq[item] == top: 
+                ans.append(item)
+        return ans 
+        
