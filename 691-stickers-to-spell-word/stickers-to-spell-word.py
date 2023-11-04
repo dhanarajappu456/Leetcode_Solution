7c08d9caa4af15771d4e0cc8b879b130919704aa
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-11-04 16:24:27 +0530

    Time: 2889 ms (23.03%) | Memory: 18.6 MB (20.86%) - LeetSync

--- 691-stickers-to-spell-word/stickers-to-spell-word.py
+++ 691-stickers-to-spell-word/stickers-to-spell-word.py
@@ -13,10 +13,11 @@ class Solution:
         2)remove all the stickers that aint give any char in target 
         '''
 
+        #remove duplicate stickers
         stickers  = set(stickers)
-
-        
         stickerNew  = []
+
+        #only consider stickers having atleast one character in the target
         for stick in stickers: 
             present = False
             for c in stick : 
@@ -26,21 +27,26 @@ class Solution:
             if present:
                 stickerNew.append(stick)
 
+        #new processed stickers list 
         stickers = list(stickerNew)
-
-        charInd = dict(dq)
-        #store the character mapped to the indices 
-        for i,c in enumerate(target):
-            charInd[c].append(i)
+        #maks denoting which are the postion in the target that is needed to be obtained, 
+        #initiallly all chars is needed so all ones 
         n = len(target)
         mask = (1<<n)-1 
+
+        #characters in the target that need to be sufficed, mapped to list of thier indices,
+        #this is needed to know which position in the target  is sufficed currently , 
+        #and therfore  unset the mask position , denoting it is sufficed,
+        charInd = dict(dq)
+        for i,c in enumerate(target):
+            charInd[c].append(i)
+        
         #call the recursive function for the remaining set of chars,(denoted by the mask)
         @lru_cache(None)
         def rec(mask):
-            #print(bin(mask))
+       
             if mask ==0: 
                 return 0 
-            
             #try all sticker if it can suffice any remaining character in the target 
             #if yes, call the rec for remaining chars
             old_mask = mask
@@ -53,7 +59,7 @@ class Solution:
                         indices[c].append(charInd[c][0])
                         ind = charInd[c].popleft()
                         new_mask = new_mask & ~(1<<ind)
-                        
+                #call recursion if atleast one character can be sufficed, by the sticker
                 if indices:
                     ans = min(ans, 1+ rec(new_mask)) 
 
@@ -63,7 +69,7 @@ class Solution:
                         charInd[c].appendleft(ind)
             return ans
         ans = rec(mask)
-        #if not [ossibilites of getting the target , then return -1 
+        #if not possibilites of getting the target , then return -1 
         return -1 if ans == float("inf") else ans 
 
                     
