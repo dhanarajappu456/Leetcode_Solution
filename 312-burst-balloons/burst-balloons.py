9093eaf2b39df5f35c12ceabf6b0f97efd44924c
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-11-05 10:49:58 +0530

    Time: 5117 ms (56.20%) | Memory: 40.6 MB (6.64%) - LeetSync

--- 312-burst-balloons/burst-balloons.py
+++ 312-burst-balloons/burst-balloons.py
@@ -8,6 +8,8 @@ class Solution:
                 return 0
             ans = 0
             for p in range(i,j+1):
+                # since ballon at i-1 and j+1 is after nums[p], they are guranteed to exist to left and right of 
+                # balloon at p 
                 ans  =  max(ans,  nums[i-1]*nums[p]*nums[j+1] + rec(i,p-1)+ rec(p+1,j))
             return ans
         nums = [1]+ nums+ [1]
