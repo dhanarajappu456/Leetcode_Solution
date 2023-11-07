8a3dd89269aa9fc7f3aded11c9aa0f218ee818e5
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-11-07 20:17:35 +0530

    Time: 688 ms (78.55%) | Memory: 32 MB (92.25%) - LeetSync

--- 2049-eliminate-maximum-number-of-monsters/eliminate-maximum-number-of-monsters.py
+++ 2049-eliminate-maximum-number-of-monsters/eliminate-maximum-number-of-monsters.py
@@ -5,9 +5,11 @@ class Solution:
         time =[dist[i]/speed[i] for i in range(n)]
         time.sort()
         cnt = 0
+        weaponAvailableAt  = 0 
         for i in range(n):
-            if time[i]>i:
+            if time[i]>weaponAvailableAt:
                 cnt+=1
+                weaponAvailableAt+=1
             else:
                 break
         return cnt
