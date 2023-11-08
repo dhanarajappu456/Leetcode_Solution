1e8a08659336141fc5364cfb0c4a6c47094d9586
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-11-08 15:45:26 +0530

    Time: 1705 ms (28.17%) | Memory: 172.3 MB (10.78%) - LeetSync

--- 2568-minimum-fuel-cost-to-report-to-the-capital/minimum-fuel-cost-to-report-to-the-capital.py
+++ 2568-minimum-fuel-cost-to-report-to-the-capital/minimum-fuel-cost-to-report-to-the-capital.py
@@ -2,10 +2,10 @@ from collections import defaultdict as dict
 
 class Solution:
     def minimumFuelCost(self, roads: List[List[int]], seats: int) -> int:
-
+        #we start dfs from 0 
         #at any node, keep track of number of poeple reaching the node, based on which 
-        #we determine , number of cars we might need , which my travel up costing 1*number of cars fuel
-
+        #we determine , number of cars we might need  to travel ,up a dist(towrds 0) from the current node , we also need  
+        #the totcars reaching a node, based on which we calulate the fuel needed to reach this node, just from a node below and adj to it , each node also keep a variable fuel , needed to reach that particular node
         vis = set()
         adj = dict(list)
         for e in roads:
@@ -24,11 +24,14 @@ class Solution:
                     peopleSofar , carsSofar , fuelSofar = rec(neib)
                     people+=peopleSofar
                     cars+=carsSofar
+                    #all fuel to reach the neib node(fuelSofar,) + the fuel needed to reach node, from neib 
+                    #which is nothing but totcars travelling from neib to node(each costing 1 litre of fuel)
                     fuel = fuel+  fuelSofar+(1*carsSofar)
-                   
+            #for next node above, tot people increase by one including the currentnode(or representative as they said in the description)
             people = people+1
+            #remeber , this is not clear from the description , 
+            #actually the representatives from other cars reaching a node, can reorganise themselves, to minimise the number of cars used, that is fuels used(meaning unfilled cars from neib node below, can be utilsed by other people travellign in another unfilled car, so that extra cars can be dicarded , that is why we takes people /ceil)
             cars = math.ceil(people/seats)
-            fuel  = fuel 
             return (people, cars, fuel)
         
         people, cars, fuel = rec(0)
