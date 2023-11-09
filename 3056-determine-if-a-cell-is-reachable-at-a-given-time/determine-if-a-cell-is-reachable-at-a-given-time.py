8b58b9221bda2cf902c77c8dc482ce082d2a345a
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-11-09 10:54:53 +0530

    Time: 36 ms (79.94%) | Memory: 16.1 MB (99.28%) - LeetSync

--- 3056-determine-if-a-cell-is-reachable-at-a-given-time/determine-if-a-cell-is-reachable-at-a-given-time.py
+++ 3056-determine-if-a-cell-is-reachable-at-a-given-time/determine-if-a-cell-is-reachable-at-a-given-time.py
@@ -7,15 +7,24 @@ bruteforce - using bfs  to calculate  mintime  and check if given time >= mintim
 t n -tle
 s n  - tle
 
-solution 2 - finding  minTime(via diag cell and then moving either col or row) and check if time>= mintime
+solution 2 - optimised - finding  minTime(via diag cell and then moving either col or row) and check if time>= mintime
 code of which is below 
 
 t 1
 s 1
 
+
+
+solution 3 - ssame same as optimised ,
+
+if u observe the min time to reach the cell fx and fy is max(horizontal dist and vert dist)
+just check if given t>=mintime 
+
+t 1
+s 1
 '''
 
-
+#solution 2
 class Solution:
     def isReachableAtTime(self, sx: int, sy: int, fx: int, fy: int, t: int) -> bool:
         if sx==fx and sy ==fy:
