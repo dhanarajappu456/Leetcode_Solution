e5d63c518c4a201c6a72e97e9f0a46a2174717e2
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-11-10 18:17:28 +0530

    Time: 2624 ms (8.96%) | Memory: 167.9 MB (5.26%) - LeetSync

--- 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.py
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.py
@@ -16,26 +16,51 @@ you can also store just vertex,
 traverse the edge which leads to vertex already visited  and call the dfs for the already called vertex, then return back the recursion in the base case
 '''
 
+from collections import defaultdict as dict
 
-#way 2
+#way1 
 class Solution:
-    def minScore(self, n: int, roads: List[List[int]]) -> int: 
-        adj = defaultdict(list) # node -> list of (neighbor, dist) for src, dst, dist in roads:
-        for src, dst,dist  in roads: 
-            adj[src].append((dst, dist))
-            adj[dst].append((src, dist))
-        def dfs(i):
-            if i in visit:
-                return
-            visit.add(i)
-            nonlocal res
-            for nei, dist in adj[i]: 
-                res = min(res, dist) 
-                dfs(nei)
-        res = float("inf")
-        visit = set()
+    def minScore(self, n: int, roads: List[List[int]]) -> int:
+
+        adj =dict(list)
+
+
+        for a,b, w in roads:
+            adj[a].append((b,w))
+            adj[b].append((a,w))
+
+        ans =[float("inf")]
+        vis = set()
+        def dfs(node):
+
+
+            for neib,w  in adj[node]:
+                if (node , neib ,w ) not in vis:  
+                    vis.add((node,neib ,w ))
+                    vis.add((neib,node ,w ))
+                    ans[0] = min(ans[0],w)
+                    dfs(neib)
         dfs(1)
-        return res
+        return ans[0]
+# #way 2
+# class Solution:
+#     def minScore(self, n: int, roads: List[List[int]]) -> int: 
+#         adj = defaultdict(list) # node -> list of (neighbor, dist) for src, dst, dist in roads:
+#         for src, dst,dist  in roads: 
+#             adj[src].append((dst, dist))
+#             adj[dst].append((src, dist))
+#         def dfs(i):
+#             if i in visit:
+#                 return
+#             visit.add(i)
+#             nonlocal res
+#             for nei, dist in adj[i]: 
+#                 res = min(res, dist) 
+#                 dfs(nei)
+#         res = float("inf")
+#         visit = set()
+#         dfs(1)
+#         return res
 
 
 
