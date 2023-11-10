952d0678b6c65a44a0592b1eab44863dbe60b132
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-11-10 20:59:09 +0530

    Time: 168 ms (31.60%) | Memory: 17 MB (26.80%) - LeetSync

---
+++ 801-is-graph-bipartite/is-graph-bipartite.py
@@ -0,0 +1,33 @@
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+
+        
+
+        def rec(node,prevColor):
+
+            
+            if color[node]!=-1:
+                return prevColor!=color[node]
+
+            color[node] = 1-prevColor
+            
+            for  neib in graph[node]:
+                
+                if rec(neib, color[node]) == False:
+                    return False
+            return True
+        n = len(graph)
+        color =[ -1 for i in range(n)]
+        
+        for i in range(n):
+            if color[i]==-1:
+
+                if rec(i,0) == False :
+                    return False
+        print(color)
+        return  True
+        
+
+
+
+        
