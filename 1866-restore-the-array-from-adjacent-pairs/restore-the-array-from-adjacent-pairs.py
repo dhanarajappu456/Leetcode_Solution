575c1ad72f284d5b9a5261a32f4b646cdd0fe54b
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-11-10 22:40:06 +0530

    Time: 888 ms (81.25%) | Memory: 65.1 MB (77.62%) - LeetSync

--- 1866-restore-the-array-from-adjacent-pairs/restore-the-array-from-adjacent-pairs.py
+++ 1866-restore-the-array-from-adjacent-pairs/restore-the-array-from-adjacent-pairs.py
@@ -1,5 +1,15 @@
 from collections import defaultdict as dict 
+'''
 
+we create a map  -> number :neib1, neib2
+
+where neib 1 and 2 are neibrs on either side of the number
+
+after creating  this it is all about finding  either start or end of the sequence(which is that number which has only 1 neighbour), then start creating the sequence , with the help of the map ,
+
+also we add the already added numbers into the sequence , to a visited set
+
+'''
 class Solution:
     def restoreArray(self, adjacentPairs: List[List[int]]) -> List[int]:
 
@@ -7,6 +17,7 @@ class Solution:
         adj = dict(list)
 
         n = len(adjacentPairs)+1
+        #creating the neib list
         for p in adjacentPairs:
 
             a,b = p[0],p[1]
@@ -26,6 +37,7 @@ class Solution:
             vis.add(start)
 
             for adjacent in adj[start]:
+                #choose that neib of the number, which is not already added to the sequence
                 if adjacent not in vis:
                     start = adjacent
  
