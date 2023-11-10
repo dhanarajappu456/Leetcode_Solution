9c0099f9a5b11a859720f8875f2354233d7978cc
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-11-10 10:14:55 +0530

    Time: 147 ms (28.87%) | Memory: 17.4 MB (30.99%) - LeetSync

--- 1885-count-number-of-homogenous-substrings/count-number-of-homogenous-substrings.py
+++ 1885-count-number-of-homogenous-substrings/count-number-of-homogenous-substrings.py
@@ -6,14 +6,16 @@ class Solution:
         mod  = 10**9+7
         n  = len(s)
         ans= 0 
-        i=0 
-        while(i<n): 
-            j=i
-            while(j<n and s[i]==s[j]):
-                j+=1
-            length = j-i
-            ans += length *(length+1)//2
-            i= j
-        return ans % mod
+        for i in range(n):
             
+            if i>0 and s[i]==s[i-1]:
+                length+=1
+            
+            else:
+                length = 1 
+            
+
+            ans+= length
+        
+        return ans%mod
         
