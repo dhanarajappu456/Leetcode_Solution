77834d7e057a1fbf5c129b646f324ce9fae79790
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-11-12 14:43:35 +0530

    Time: 66 ms (11.20%) | Memory: 16.3 MB (36.93%) - LeetSync

--- 2080-check-if-move-is-legal/check-if-move-is-legal.py
+++ 2080-check-if-move-is-legal/check-if-move-is-legal.py
@@ -1,9 +1,14 @@
+'''
+question asks for whether any direction have good line , with rmove, cmove,  as the end point 
+'''
+
 class Solution:
     def checkMove(self, board: List[List[str]], rMove: int, cMove: int, color: str) -> bool:
 
         def helper():
          
             dir  =[(0,1),(1,0),(0,-1),(-1,0),(1,1),(1,-1),(-1,1),(-1,-1)]
+            
             for d in dir:
                 cnt = 0 
                 x,y = rMove, cMove
@@ -13,11 +18,15 @@ class Solution:
                         break
                     if (d[0],d[1]) ==(0,-1):
                             print(x,y)
-      
+                    #if the cell is empty cell , we can to further in current direction
                     if board[x][y]==".":
                         break
+                    #if color is opposite of the given color, just increment the count 
                     elif board[x][y]!= color:
                         cnt+=1
+                    #if the cell is same color as one end(given cell rMove and Cmove)
+                    #then it  means,  we have a good line  ,  or we don't have , if there
+                    #is not any opposite color of given "color"  in b/w
                     else:
                         if cnt>=1 :
                             return True 
