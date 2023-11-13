175b1ed091da360084cc787e6e7f095c45342d4c
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-11-13 13:01:18 +0530

    Time: 573 ms (16.40%) | Memory: 18.7 MB (87.70%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.py
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.py
@@ -1,11 +1,12 @@
 from collections import defaultdict as dict 
-
+from sortedcontainers import SortedSet
 #approach2 - suing counting sort
 
 class Solution:
     def sortVowels(self, s: str) -> str:
         #this is traversed to give the next available character to be filled in current position of result
-        sortedVowels="AEIOUaeiou"
+        sortedVowels= SortedSet("AEIOUaeiou")
+        print(sortedVowels)
         ptr = 0
         #keeps track of avilable characters at any point of time in the string 
         vowCount =dict(int)
