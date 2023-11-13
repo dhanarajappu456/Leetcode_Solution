3c615dfe252179b638ae2151dd8a7a452e8ff1b1
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-11-13 19:44:55 +0530

    Time: 559 ms (90.01%) | Memory: 23.9 MB (55.09%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.py
+++ 820-find-eventual-safe-states/find-eventual-safe-states.py
@@ -6,17 +6,18 @@ class Solution:
         safe = dict()
         def dfs(node):
             
-
+            #base case - if cycle or already processed node, then return value False and True 
             if node in safe:
                 return safe[node]
-
+            #intially until all the path is known to lead to tail node, mark it as unsafe 
             safe[node] = False
             for neib in graph[node]:
-
+                
                 if dfs(neib)==False:
                     return False
                 
             safe[node] = True
+            #now the node is known to be safe, so true , is stored,
             return True
 
         n = len(graph)
