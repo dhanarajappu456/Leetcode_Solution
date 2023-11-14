696ac4f7116f91473e89fa82d557ed0af2eb6253
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-11-14 14:31:18 +0530

    Time: 123 ms (85.42%) | Memory: 23.5 MB (16.42%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -1,3 +1,36 @@
+'''
+
+1)approach1 - construct the grapah from wordlist, with key as each word
+
+remember the length of each word in wordlist == length of startword == length of endword
+create the graph ,where each node is neighbours to each other, if they diff in only 1 char 
+run the bfs on them to find shortest number of transformation to reach endword
+
+
+t create adj list (n^2*m ) + bfs (v+e = n+ n^2)
+s (v+e = n + n^2)
+
+2)approach2 
+
+same logic as first but creating graph with key as the pattern , we create a pattern
+eg: *ot  which maps , all words that is of this from , meaning it has any character at first place , but, 
+"ot" after that
+
+then do bfs using this adj list, 
+doing this way , reduce, the time complexity 
+from n^2m to nm^2
+
+
+
+t create adj list (n*m^2 ) + bfs (v+e = n+ n^2)
+s (v+e = n + n^2)
+
+
+
+'''
+
+#approach 2 
+
 from collections import defaultdict as dict, deque as dq
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
