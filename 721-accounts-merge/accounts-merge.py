40116f45f07d273cecce7ea9a0cefaa645fb2a63
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-11-15 15:02:21 +0530

    Time: 193 ms (74.87%) | Memory: 20.5 MB (83.59%) - LeetSync

--- 721-accounts-merge/accounts-merge.py
+++ 721-accounts-merge/accounts-merge.py
@@ -1,71 +1,55 @@
-from collections import defaultdict as dict
-'''
+from collections import defaultdict as dict 
+class DisjointSet:
+    def __init__(self,V):
+        self.rank = [1 for i in range(V)]
+        self.parent = [i  for i in range(V)]
+    #o(4*alpha) =O(1)
+    def union(self,u,v):
+        pu = self.findParent(u)
+        pv = self.findParent(v)
+        
+        if self.rank[pu] <self.rank[pv]:
+            self.parent[pu] =pv    
+        elif self.rank[pu] >self.rank[pv]:
+            self.parent[pv] =pu
+        else:
+            self.parent[pv]=pu
+            self.rank[pu]+=1
+            
+    
+    def findParent(self,u):
+        
+        if self.parent[u]==u:
+            return u
+        #path compressed
+        self.parent[u] = self.findParent(self.parent[u])
+        return self.parent[u]
+        
 
-using the traversal on each of the connected components
-
-the idea is simple. 
-we create a component , where all the nodes are the mails belonging to the same person - this adj list created
-by the create graph function , thus if 2 accounts have common mail , it would be automatically added to the same component 
-
-
-then for each account we check if the first mail(if first mail is processed all of the mail in the same account and other mails of the same person  would have been already visitd ), is processed, if not
-we visit all the mails of that component and add it as mails of a new person 
-
-
-'''
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
-    #create adj list mail mapped to anmother mail(if it belongs to same person )
-    #so a component , will be containing all the mail of a same person
-          
-        #keeps track of visited mail so , that same component(mails of same person ) not visited again
-        processed_mails  =set()
-        #to traverse a component and get all the mails associated with the same person 
-        def dfs(node):
-         
-            if node in processed_mails:
-                return []
-            
-            mails  =[]
-            mails.append(node)
-            processed_mails.add(node)
-            for neib in adj[node]:
-                #we can neglect the extend list time complexity
-                mails.extend(dfs(neib))
-            
         
-            return mails
-
-        adj = dict(set)
-
-        #create adjlist , a component is set of mails belonging to a particular person 
-        def createGraph(mails):
-            n = len(mails )
-            for i in range(n):
-
-                if i>0:
-                    adj[mails[i]].add(mails[i-1])
-                    adj[mails[i-1]].add(mails[i])
-            
-            
-        #creating the adjlist
-        for acc in accounts:
-            createGraph(acc[1:])
+        ds = DisjointSet(len(accounts))
+        emailToAccount = dict(int)
+        for i in range(len(accounts)):
+            acc = accounts[i]
+            for email  in acc[1:]:
+                if email in emailToAccount:
+                    #merge to the ultimate parent of the account for which this email was first seen 
+                    ds.union(i,emailToAccount[email])
+                else:
+                    emailToAccount[email] = i
+        accToMails  = dict(list)         
+        for email in emailToAccount :
+            acc = emailToAccount[email]
+            parAcc = ds.findParent(acc)
+            accToMails[parAcc].append(email)
         
-        res =[]
-        for acc in accounts:
-            #if the mail is not processed process that component ,of mails, for a person
-            if acc[1] not in processed_mails:
-              
-                ans= dfs(acc[1])
-                ans.sort()
-                res.append([acc[0]]+ans)
-                
-
+        res =[ ]
+        for acc in accToMails:
+  
+            res.append([accounts[acc][0]]+sorted(accToMails[acc]))
         return res
-        '''
-        t  creating adj list n*m (accounts array length , and each account length) = 10^5 + dfs(at worst will travel all mails , once = nm)
-        s n +n^2
-
-        '''
-
+                    
+            
+        
