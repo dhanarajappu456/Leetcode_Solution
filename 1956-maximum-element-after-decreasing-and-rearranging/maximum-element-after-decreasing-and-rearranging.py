788897b97415fddd3bc684e6eed5a94e1b4a9222
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-11-15 15:27:36 +0530

    Time: 450 ms (18.23%) | Memory: 26.5 MB (6.08%) - LeetSync

--- 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.py
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.py
@@ -1,8 +1,13 @@
+# sort the array, 
+# make first element as zero as the elements in the array always>=1
+# then starting from 1st index, if curr element ==  prev element  leave it as such , else 
+# might need to reduce it to a number just 1 greater than prev element(as absof adj element cant be more than 1)
+
 class Solution:
     def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
 
         arr.sort()
-
+        ans  =1 
         arr[0]=1
         n = len(arr)
         for i in range(1,n):
@@ -10,6 +15,14 @@ class Solution:
                 continue
             else:
                 arr[i]= arr[i-1]+1
-        print(arr)
-        return max(arr)
+            ans  = max(ans, arr[i])
+
+        return ans
+
+
+    '''
+    t n
+    s 1
+
+    '''
         
