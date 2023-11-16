59ae0953f654ae79c7eaa01e31c2bd64a0e43036
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-11-16 15:45:40 +0530

    Time: 44 ms (46.90%) | Memory: 16.3 MB (42.98%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.py
+++ 2107-find-unique-binary-string/find-unique-binary-string.py
@@ -1,3 +1,17 @@
+#approach 1
+#naive approach , starting from all numbers from 0 to 2**n-1 we check whose bin representation  is   not in the list
+#t 2**n
+#s 1 
+
+#approach 2 
+# we try to create a number that differ from all the numbers in the list , 
+# wrt to n position in the number 
+# at the end we get a number that is not in the list
+# t n 
+# s 1
+
+
+#approach 2
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
 
