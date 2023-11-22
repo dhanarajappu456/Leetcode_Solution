8c29bccfcb2463a485057dc9029aa1fd45fb48d2
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-11-22 23:54:24 +0530

    Time: 900 ms (82.81%) | Memory: 31 MB (39.72%) - LeetSync

--- 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.py
+++ 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.py
@@ -9,20 +9,26 @@ class Solution:
 
         n  = len(nums)
         def can_be_min_diff(diff):
-            i=1 
+            i=0
             pairs = 0 
-            while(i<n):
-                #theremay be adjacent values that are having neighbour differnce<= diff
-                #this groups all such number and find the number pairs possible from them
-               # which is len(this set)//2
-                contributing_index =set()
-                while(i<n and (nums[i]-nums[i-1])<=diff):
-                    contributing_index.add(i)
-                    contributing_index.add(i-1)
+            while(i<n-1):
+                #follow greedily , if a num at i  can pair with next one i+1  always pair it , 
+                #it is better than pairing the i+1, with next number at i+2 even if its diff is very short
+                # not following early index pairing,  cause to  miss some better opportunities
+                #eg:
+                #p =2 
+                #diff=5
+                #nums[1 , 3, 4, 5, 20,30 ]
+                #if 3 can pair with 1 pair it with 1, don't pair with 4, since doing so makes a pair (4,5) unavialable 
+                #and we get 2 pair with that diff cannot be made, but if we had paired (1,3) , then we can pair (4,5 ) which gives 
+                #2 pair
+                
+                if nums[i+1] - nums[i]<= diff:
+                    pairs+=1
+                    i+=2
+                else:
+
                     i+=1
-                    break
-                pairs += len(contributing_index)//2
-                i+=1
             #check if   p or more  of such number of pairs with difffernce<=diff  can be formed
             return pairs>=p
             for i in range(1,n):
