7eb901b3e930ac96e4fa36130591d91708ed5cd3
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-11-22 10:18:36 +0530

    Time: 967 ms (6.04%) | Memory: 44.4 MB (11.64%) - LeetSync

---
+++ 1539-diagonal-traverse-ii/diagonal-traverse-ii.py
@@ -0,0 +1,30 @@
+from collections import deque as deq
+
+class Solution:
+    def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
+        q = deq([(0,0)])
+        vis = set()
+        vis.add((0,0))
+        ans =[]
+        def out(coord):
+            i,j = coord[0],coord[1]
+            return i<0 or i>=len(nums) or j<0 or j>= len(nums[i])
+
+        while(q):
+
+            i,j = q.popleft()
+
+            ans.append(nums[i][j])
+            down,right = (i+1,j), (i,j+1)
+
+            if( not out(down)) and( down not in vis):
+                vis.add(down)
+                q.append(down)
+            
+            if (not out(right)) and (right not in vis):
+                vis.add(right)
+                q.append(right)
+        return ans
+        
+
+        
