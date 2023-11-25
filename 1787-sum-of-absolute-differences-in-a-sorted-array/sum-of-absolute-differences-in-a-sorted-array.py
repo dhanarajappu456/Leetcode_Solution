895ba67ddcaa07586271fb647dc292c5547e4ca3
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-11-25 12:25:11 +0530

    Time: 779 ms (18.91%) | Memory: 32 MB (20.51%) - LeetSync

---
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.py
@@ -0,0 +1,50 @@
+'''
+   
+   solution 1 
+        brute force - for each number calculate the diff, by iterating on the n eleements
+        t n^2
+        s 1(excludng the final ans array)
+        n = len(nums)
+    solution 2 -optimised
+    make use of sorted order
+
+    for each number which are less than nums[i]
+    
+    abs diff with elements left of  it is 
+    where nums[i] is current element 
+    this nums[i] - nums[j] where j is index of elements to left of it  
+    this can be formalised to the equation prefsum till 
+    
+    left =  num[i]- (i+1)*nums[i]
+
+    similarly for right part of i, which are all greater than nums[i]
+
+    equation is  
+    
+    right = (pref[n-1]-pref[i]) - num*(n-i-i) 
+
+    thus total ans  = left + right
+    
+'''
+class Solution:
+    
+    def getSumAbsoluteDifferences(self, nums: List[int]) -> List[int]:
+
+        n = len(nums)
+        pref =[0 for i in range(n)]
+        pref[0] = nums[0]
+    
+        for i in range(n):
+            num = nums[i]
+            pref[i] = pref[i-1]+nums[i]
+        print(pref)
+
+        ans =[]
+        for i in range(n):
+            num = nums[i]
+            left = (i+1)*num - pref[i]
+            right = (pref[n-1]-pref[i]) - num*(n-1-i) 
+
+            ans.append(left+right) 
+        return ans
+        
