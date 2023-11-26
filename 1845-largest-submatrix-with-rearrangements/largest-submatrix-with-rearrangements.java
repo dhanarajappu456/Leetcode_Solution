100abc0d79525433d7642d409919b84b3bf1141f
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-11-26 19:37:46 +0530

    Time: 28 ms (19.28%) | Memory: 70.6 MB (24.10%) - LeetSync

--- 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.java
+++ 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.java
@@ -1,36 +1,39 @@
 class Solution {
     public int largestSubmatrix(int[][] matrix) {
-        
-
-         int m = matrix.length;
+        int m = matrix.length;
         int n = matrix[0].length;
-        int[] heights = new int[n];
-        int maxArea = 0;
+        List<Pair<Integer, Integer>> prevHeights = new ArrayList<>();
+        int result = 0;
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    heights[j] = 0;
-                } else {
-                    heights[j] += 1;
+        for (int row = 0; row < m; row++) {
+            List<Pair<Integer, Integer>> heights = new ArrayList<>();
+            boolean[] seen = new boolean[n];
+
+            for (Pair<Integer, Integer> entry : prevHeights) {
+                int height = entry.getKey();
+                int col = entry.getValue();
+                if (matrix[row][col] == 1) {
+                    heights.add(new Pair<>(height + 1, col));
+                    seen[col] = true;
                 }
             }
 
-            int[] sortedHeights = Arrays.copyOf(heights, n);
-            Arrays.sort(sortedHeights);
-
-            for (int j = 0; j < n; j++) {
-                int h = sortedHeights[j];
-                int area = h * (n - j);
-                maxArea = Math.max(area, maxArea);
+            for (int col = 0; col < n; col++) {
+                if (!seen[col] && matrix[row][col] == 1) {
+                    heights.add(new Pair<>(1, col));
+                }
             }
+
+            for (int i = 0; i < heights.size(); i++) {
+                int base = i + 1;
+                int height = heights.get(i).getKey();
+                result = Math.max(result, base * height);
+            }
+
+            prevHeights = heights;
         }
 
-        return maxArea;
-
-  
+        return result;
     }
+    
 }
-
-
-
