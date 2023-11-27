6791d3e85409508fe14f30d45d42c04593cd2bfc
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-11-27 15:00:35 +0530

    Time: 9759 ms (5.16%) | Memory: 48.7 MB (26.18%) - LeetSync

--- 972-knight-dialer/knight-dialer.py
+++ 972-knight-dialer/knight-dialer.py
@@ -1,3 +1,36 @@
+'''
+
+
+solution 1 - 3state
+
+constructing the board and performing the recusrive solution with each cell as the starting cell
+
+ note that a movement (ie, consisting of the 3 jump and in the shape of L as said in q) cannot end at non numeric cell  but out of the three jump intermediate cell can be  non numeric
+
+
+
+ we can cahe the resut with state - i,j,n , when n is remaining number of jumps
+
+t 3*4 *5000 *(8*3)  for each call we have inner loop runs for 8 direction (movements) and each direct has a jump of length 3 
+
+so tot time approx = 10^5
+ 
+s n(5000) 
+
+
+solution 2- better optimised  and clean solution - without board- 2 state
+
+instead if board we keep a map for each value mapped to possible values it can move , then we make use of this map 
+
+
+t state reduce to to - (val,n) n =remaining number of jump  
+time = 10 * 5000= 10^4 
+s (n) = 5000
+
+
+
+'''
+#solution 1
 class Solution:
 
 
