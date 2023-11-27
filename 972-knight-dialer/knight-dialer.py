9a0d67b5654ba50bf519574fdcf73e7f37b51ab6
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-11-27 15:10:16 +0530

    Time: 1904 ms (38.02%) | Memory: 42.8 MB (40.53%) - LeetSync

--- 972-knight-dialer/knight-dialer.py
+++ 972-knight-dialer/knight-dialer.py
@@ -30,94 +30,57 @@ s (n) = 5000
 
 
 '''
-#solution 1
+#solution 2
 class Solution:
 
 
     def knightDialer(self, n: int) -> int:
-        board =[[1,2,3],[4,5,6],[7,8,9],[-1,0,-1]]
+        mod = 10**9+7
 
-        mod = 10**9+7    
-        memo  ={}
-        def invalidCell(i,j):
-            return board[i][j]==-1
+        map_   = {
+             0:[4,6], 
+            1:[6,8],
+            2:[7,9],
+            3:[4,8],
+            4:[0,3,9],
+            5:[],
+            6:[0,1,7],
+            7: [2,6],
+            8:[1,3],
+            9:[2, 4]}
+        memo ={ }
+        #current value of cell at and n is rem number of jumps
+        def rec(val,n):
 
-        def out_bound(i,j):
-
-            return i<0 or j<0 or i>=a or j>=b 
-        def rec(i,j,n):
-
-            
-            
-            #we can't stand at any non numerical cell out of each of the n-1 movementd
-            if invalidCell(i,j):
-                return 0
-
-            # if we reach this line this will be a valid cell (ie, it is numerical cell )
-            if n==0 :
-                  
+            if n==0:
                 return 1
-      
-
-
-            #for all possbilities check
-
-            if (i,j,n ) in memo:
-                return memo[(i,j,n)]
-            ans = 0 
             
-            for d in dire:
-         
-                x,y =i,j
-                valid_cell =True
-                for m in d:
 
-                    x += m[0] 
-                    y += m[1]
-                    
-                    if (not out_bound(x,y)):
-                      
-                        continue
-                    else:
-                      
-                        valid_cell =False
-                        break
-                if valid_cell:
-                   
-                    ans  = (ans%mod + rec(x,y,n-1)%mod)%mod
-            memo[(i,j,n) ] = ans%mod
+            if (val,n) in memo:
+                return memo[(val , n)]
+            
+            ans = 0 
+            for v in map_[val]:
+
+                ans = ( ans%mod +  rec(v,n-1)%mod)%mod
+            memo[(val,n)] = ans
             return ans
 
+
+        ans  = 0 
+        for val in range(0,10):
             
+            ans  = (ans%mod +  rec(val,n-1)%mod)%mod 
+        return ans 
+
+        return ans
+            
+
+
+
 
 
                     
-
-    
-
-
-        #for each number as the starting number:
-        dire  = [
-            
-            [(-1,0),(-1,0),(0,-1)],[(-1,0),(-1,0),(0,1)],
-        [(0,1),(0,1),(-1,0)],[(0,1),(0,1),(1,0)],
-        [(1,0),(1,0),(0,-1)],[(1,0),(1,0),(0,1)],
-        [(0,-1),(0,-1),(-1,0)],[(0,-1),(0,-1),(1,0)]]
-        a,b = 4,3
-        ans= 0 
-        for i in range(a):
-            for j in range(b):
-
-                if board[i][j] !=-1:
-                   
-                    ans  = (ans%mod + rec(i,j,n-1)%mod)%mod
-                 
-
-        return ans
-
-
-           
-            
             
 
             
