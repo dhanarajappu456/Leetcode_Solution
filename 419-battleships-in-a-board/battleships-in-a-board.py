5900df2cae83e34e633984ce524012778993a156
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-11-29 19:20:47 +0530

    Time: 79 ms (67.40%) | Memory: 16.8 MB (66.01%) - LeetSync

--- 419-battleships-in-a-board/battleships-in-a-board.py
+++ 419-battleships-in-a-board/battleships-in-a-board.py
@@ -1,28 +1,44 @@
+'''
+solution 1 
+basically the a battle ship will be part of either part of continuous battleship along row or column not both
+
+so , it is basically finding the number of these component  in the graph
+
+
+t   mn
+s   m + n(stack)
+
+solution 2- o(1) space
+
+we count the head of each battleship
+
+that is   each battle ship head is characterised by the fact that ,
+
+
+1:it is battle ship and
+        and 
+2: left of it  empty space(.) or no cell,  
+         and 
+2:  top of it empty space(.) or no cell
+
+
+
+
+t mn
+s 1
+
+'''
 class Solution:
     def countBattleships(self, board: List[List[str]]) -> int:
-        vis  = set()
+       
         m,n  = len(board), len(board[0])
-        def dfs(i,j):
-
-            
-
-            if ((i,j)  in vis )or  i<0 or j<0 or i>=m or j>=n or board[i][j]==".":
-
-                return 
-            vis.add((i,j))
-
-            dfs(i+1,j)
-            dfs(i,j+1)
-            dfs(i-1, j)
-            dfs(i,j-1)
-
-
-        cnt= 0
+        cnt = 0 
         for i in range(m):
             for j in range( n):
-                if board[i][j] =="X" and (i,j) not in vis: 
+                if board[i][j] =="X" and (j==0 or board[i][j-1]==".")  and (i==0 or board[i-1][j]=="."):
                     cnt+=1
-                    dfs(i,j)
+             
    
         return cnt
+
 
