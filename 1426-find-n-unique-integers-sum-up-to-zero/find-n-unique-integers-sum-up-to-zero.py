8155e54d384781650adc2c90d2e3fdbbf5974d59
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-12-02 22:42:21 +0530

    Time: 40 ms (56.60%) | Memory: 16.4 MB (20.79%) - LeetSync

--- 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.py
+++ 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.py
@@ -1,6 +1,15 @@
 class Solution:
     def sumZero(self, n: int) -> List[int]:
+        '''
+        one strategy is to append  number from 1 as 
+        1, -1 , 2 , -2 , 3, -3 and so on 
 
+        if n was odd, then we will at end add 0 to the list
+
+        t n 
+        s 1 (excluding the result array space)
+
+        '''
       
         ans = [ ]
         val = 1
@@ -13,6 +22,8 @@ class Solution:
         if n:
             ans.append(0)
         return ans
+
+
 
 
 
