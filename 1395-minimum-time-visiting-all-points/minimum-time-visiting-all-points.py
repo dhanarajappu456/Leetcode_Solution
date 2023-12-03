27142d04e62224154e8c046d464be5f1ffd9a2b1
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-12-03 11:16:20 +0530

    Time: 60 ms (82.74%) | Memory: 16.4 MB (30.54%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.py
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.py
@@ -1,6 +1,13 @@
 class Solution:
     def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+        '''
 
+        since can go through x y and diag direction with 1 unit of time per step ,
+
+
+        to reach from p1 to p2 , we need to travel max distance wchich is either the diff in x coord of p1 and p2 or 
+        diff in y coord of p1 and p2
+        '''
 
         time = 0
         n  =len(points)
