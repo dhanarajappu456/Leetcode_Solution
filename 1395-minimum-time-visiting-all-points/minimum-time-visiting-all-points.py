4e59ea85af2a84e85fa14666a37faa67f1987334
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-12-03 11:23:23 +0530

    Time: 65 ms (51.79%) | Memory: 16.4 MB (30.54%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.py
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.py
@@ -5,7 +5,15 @@ class Solution:
         since can go through x y and diag direction with 1 unit of time per step ,
 
 
-        to reach from p1 to p2 , we need to travel max distance wchich is either the diff in x coord of p1 and p2 or 
+        to reach from p1 to p2 in shortest time ,
+        
+        if is always preferable to travel :
+            1)first along  diagonal uptil x and y dont exceed given p2 
+            2) then we will be either on horizontal or vertical line connecting the current point we are at and p2 
+            now it is matter of traversing that distance 
+
+        as a whole, in summary , the  tot time needed to get from p1 to p2  can be easily obtained as:
+            we need to travel max distance wchich is either the diff in x coord of p1 and p2 or 
         diff in y coord of p1 and p2
 
         t n 
