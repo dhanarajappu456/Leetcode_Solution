b5db12a0e0fde03a5c0754bb91c9391ca09bca65
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-12-03 12:10:21 +0530

    Time: 1877 ms (31.29%) | Memory: 311.9 MB (26.41%) - LeetSync

---
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
@@ -0,0 +1,29 @@
+from collections import defaultdict as dict 
+
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        
+
+        s,d = source, destination 
+
+        adj = dict(list)
+        vis  =set()
+        for e in edges: 
+
+            adj[e[0]].append(e[1])
+            adj[e[1]].append (e[0])
+        def rec(s):
+            if s in vis:
+                return False
+            if s == d: 
+                return True
+            vis.add(s)
+          
+            for neib in adj[s]:
+                if rec(neib):
+                    return True
+            return False
+            
+        return rec(s)
+
+        
