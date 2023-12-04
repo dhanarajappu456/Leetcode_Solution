9b6a9d08806039bbe1b5d5e78d488900bcd83e35
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-12-04 23:21:48 +0530

    Time: 50 ms (13.82%) | Memory: 16.4 MB (9.84%) - LeetSync

--- 1976-splitting-a-string-into-descending-consecutive-values/splitting-a-string-into-descending-consecutive-values.py
+++ 1976-splitting-a-string-into-descending-consecutive-values/splitting-a-string-into-descending-consecutive-values.py
@@ -1,19 +1,53 @@
+
+
+'''
+Example - 1
+
+the solution is simple , that , we try the bruteforce backtracking way of 
+
+partitioning the  word 
+
+
+t:
+
+
+one way to code this is take or not take pattern wher we need 
+
+
+curr ind, prev val and prev part index, as the paramter passed
+
+where we make a partition at current index,  or not make partition at current index, 
+
+so time = 2^n  = 2^20 = 10^6
+
+s = n 
+
+'''
+
+
 class Solution:
     def splitString(self, s: str) -> bool:
-        
-        n  = len(s)
-        def rec(ind, prev_val , parts):
+        @lru_cache(None)
+        def helper(prev, s):
+            if not s:
+                return True
+            
+            for i in range(1, len(s)+1):
+                x = int(s[:i])
 
-            if ind  == n : 
-                return parts>=2
-
-            for i in range(ind,n):
-                curr = int(s[ind:i+1]) 
-                if parts ==0 or  curr == prev_val-1:
-                    if rec(i+1, curr, parts+1):
+                if prev - x == 1:
+                    if helper(x, s[i:]):
                         return True
+            
             return False
-        return rec(0,-1, 0)
+        
+        for i in range(1, len(s)):
+            if helper(int(s[:i]), s[i:]):
+                return True
+        
+        return False
+
+
 
 
 
