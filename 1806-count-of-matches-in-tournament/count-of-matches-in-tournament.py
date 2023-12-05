288d90a285d3e7ccfe85888fbf184f57fb79e1c9
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-12-05 10:16:04 +0530

    Time: 33 ms (83.83%) | Memory: 16.2 MB (71.13%) - LeetSync

--- 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.py
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.py
@@ -7,34 +7,48 @@ t = logn
 s= 1
 
 
-'''
-
-class Solution:
-    def numberOfMatches(self, n: int) -> int:
-        matches = 0
-        while(n>1 ):
 
 
-            if n%2 ==0:
-                matches +=n//2
-                n = n//2
-            else:
-                matches+= (n -1 )//2 
-                n  = (n -1 )//2 +1
-
-        return matches
-        
-'''
 #solution 2 
 
 observation 
 
 
-n = 1 => ans 
-n = 1 => ans 
-n = 1 => ans 
+n = 1 => ans = 0 
+n = 2 => ans = 1  
+n = 3 => ans = 2  
+n = 4 => ans = 3  
+
+
+so for n ans = n-1
 t = 1
 s= 1
 
 
+
+
+
+
 '''
+#solution 1 
+# class Solution:
+#     def numberOfMatches(self, n: int) -> int:
+#         matches = 0
+#         while(n>1 ):
+
+
+#             if n%2 ==0:
+#                 matches +=n//2
+#                 n = n//2
+#             else:
+#                 matches+= (n -1 )//2 
+#                 n  = (n -1 )//2 +1
+
+#         return matches
+        
+
+
+#solution 2 
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        return n-1
