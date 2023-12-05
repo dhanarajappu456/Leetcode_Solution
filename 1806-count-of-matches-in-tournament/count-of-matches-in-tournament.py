1fd4c6229b42b500e4f97e110e0de24a7722c059
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-12-05 10:14:36 +0530

    Time: 30 ms (93.22%) | Memory: 16.3 MB (5.91%) - LeetSync

--- 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.py
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.py
@@ -1,3 +1,14 @@
+#solution1 
+
+'''
+simulation 
+
+t = logn
+s= 1
+
+
+'''
+
 class Solution:
     def numberOfMatches(self, n: int) -> int:
         matches = 0
@@ -13,3 +24,17 @@ class Solution:
 
         return matches
         
+'''
+#solution 2 
+
+observation 
+
+
+n = 1 => ans 
+n = 1 => ans 
+n = 1 => ans 
+t = 1
+s= 1
+
+
+'''
