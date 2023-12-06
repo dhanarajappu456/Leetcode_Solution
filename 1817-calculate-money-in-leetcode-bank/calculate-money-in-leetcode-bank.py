a595a738fd2549434c7c40e7924ceb5a95a3b519
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-12-06 10:19:56 +0530

    Time: 41 ms (48.88%) | Memory: 16.3 MB (40.24%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.py
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.py
@@ -1,26 +1,28 @@
+'''
+solution1 - simultion
+
+'''
+
 class Solution:
     def totalMoney(self, n: int) -> int:
-        week = 1
+    
         ans =0 
-        d = 1
-        start =1 
+        start =0
         while(n):
             
             if n>=7:
-                
-
-
-                for i in range(start, start+7):
-                    ans+=i
+                end = start+7
+   
+                ans+= (end*(end+1)//2 - start*(start+1)//2)
                 start+=1
-                n = n-7
-          
-     
-            if n<7:
-              
-                for i in range(start, start+n):
-                    ans+= i
+                n-=7 
 
+            if n<7:
+
+            
+                for i in range(start+1, start+1+n):
+                   
+                    ans+= i
             
                 n = 0
            
