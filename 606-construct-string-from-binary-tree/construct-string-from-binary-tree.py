30a957cafa07701f7396228ef36eded66f64ca91
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-12-08 09:22:15 +0530

    Time: 50 ms (73.06%) | Memory: 18.8 MB (72.67%) - LeetSync

--- 606-construct-string-from-binary-tree/construct-string-from-binary-tree.py
+++ 606-construct-string-from-binary-tree/construct-string-from-binary-tree.py
@@ -1,9 +1,28 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+'''
+When such question is got , 
+
+remeber we need to distinuguish b/w  
+t1: 2 null 4  
+t2: 2 4 null
+and the expected ans is 
+for t1 : 2()(4)
+for t2 : 2(4)
+
+that is any children of node is assumed to be left then right , so left need to be always present 
+in the string format 
+
+that is if left subtree is empty  and right subtree exist for a node, left is represented by a ()
+
+
+rest is just a recursion 
+
+t n 
+s n
+
+
+
+
+'''
 class Solution:
     def tree2str(self, root: Optional[TreeNode]) -> str:
 
@@ -14,11 +33,12 @@ class Solution:
             
             if root == None:
                 return ""
-
+           
             l =rec(root.left)
             r= rec(root.right)
             if l =="" and r!="":
               l = "()"
+          
             return "(" +str(root.val)+ l+r +")"
 
         return rec(root)[1:-1]
