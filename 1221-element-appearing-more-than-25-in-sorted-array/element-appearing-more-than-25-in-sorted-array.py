3f3c1e8339f81e9881e0e9a8a1853f736954b12d
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-12-11 14:59:16 +0530

    Time: 82 ms (91.34%) | Memory: 17.7 MB (48.07%) - LeetSync

--- 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.py
+++ 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.py
@@ -1,55 +1,30 @@
 '''
-solution3 
+solution3  - using builtin binsearch 
 Binary search solution 
 The idea  is that candidate element whose freq >25% would have its element in either of the parition for sure, 
-thus we need to see all nums at index, n//4 , 2n//4 , 3n//
-[0 - n//4], [n//4+1 ,2n//4] , [2n//4+1 , n]
+thus we need to see all nums at index, n//4 , (2*n)//4 , (3*n)//4
+
+t logn
+s 1
+
 '''
 
 class Solution:
     def findSpecialInteger(self, arr: List[int]) -> int:
 
         n = len(arr)
-        def bin_left_most(i):
-            curr_num = arr[i]
-            l= 0
-            h=  i
-
-            ans  =i
-
-            while(l<=h):
-                m =(l+h)//2
-                if arr[m] ==  arr[i]:
-                    ans = m
-                    h= m-1
-                else:
-                    l = m+1
-            return ans
-        def bin_right_most(i):
-
-            curr_num = arr[i]
-            l= i
-            h=  n-1
-            ans = i
-            while(l<=h):
-                m =(l+h)//2
-                if arr[m] ==  arr[i]:
-                    ans = m
-                    l = m+1
-                else:
-                    h = m-1
-            return ans
-        
-        for i in {n//4 , 2*n//4 , 3*n//4}:
-            
+       
+        candidates = {n//4 , 2*n//4 , 3*n//4}
+        for i in candidates:
+            print(i)
             if i < n :
-
+                
                 curr_num = arr[ i]  
                
-                l = bin_left_most(i)
-                r = bin_right_most(i)
-              
-                if r-l+1 > (n//4):
+                l = bisect.bisect_left(arr,arr[i])
+                r = bisect.bisect_right(arr,arr[i])
+             
+                if r-l > (n//4):
                     return curr_num
 
             
