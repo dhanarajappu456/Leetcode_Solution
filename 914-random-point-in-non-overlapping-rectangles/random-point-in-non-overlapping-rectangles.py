1e0e44fca8f664046c04001e5b7153a6d3eaf63c
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-12-12 21:15:58 +0530

    Time: 166 ms (32.03%) | Memory: 20.6 MB (35.16%) - LeetSync

--- 914-random-point-in-non-overlapping-rectangles/random-point-in-non-overlapping-rectangles.py
+++ 914-random-point-in-non-overlapping-rectangles/random-point-in-non-overlapping-rectangles.py
@@ -37,6 +37,20 @@ import random
 
 #solution 2 
 
+'''
+using cumulative freq and bin search 
+
+since we need the probavility to be eqully likely, we  have more probaility to find a point in a reactangle that has 
+greater number of points
+
+so we store the , the cumulative freq based on the number of points in each rectangle, 
+then we find a random number b/w 1 and entire number of points in the entire rectangle. 
+based on which we find the rectangle in which this point fall in 
+
+and return it 
+
+
+'''
 import random
 import bisect
 
@@ -45,21 +59,25 @@ class Solution:
     def __init__(self, rects: List[List[int]]):
 
         self.rects = rects
-        self.points=[]
-
+        #each index denote the rectangles index and  cumulative freq(number of points ) ,upto the rectangle
+        self.cum_freq=[]
+        
         self.totPoints= 0 
 
+        #generate the cumulative freq , using number of points 
         for rect in rects:
 
             bx,by,tx,ty = rect
 
             self.totPoints +=  (tx-bx+1)*(ty-by+1)
 
-            self.points.append(self.totPoints)
+            self.cum_freq.append(self.totPoints)
 
     def pick(self) -> List[int]:
+        #generate random number 
         rand_val = random.randint(1,self.totPoints)
-        ind  = bisect.bisect_left(self.points , rand_val)
+        #find the  index of rectanfgle to which this freq fall to 
+        ind  = bisect.bisect_left(self.cum_freq , rand_val)
         rect = self.rects[ind]
         x,y = random.randint(rect[0],rect[2]) , random.randint(rect[1],rect[3])
         return [x,y]
