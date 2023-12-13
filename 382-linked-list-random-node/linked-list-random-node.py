3086e900b083f9d6841af70bee5dc90c59b026c3
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-12-13 21:56:33 +0530

    Time: 80 ms (32.34%) | Memory: 19.7 MB (14.29%) - LeetSync

--- 382-linked-list-random-node/linked-list-random-node.py
+++ 382-linked-list-random-node/linked-list-random-node.py
@@ -1,16 +1,24 @@
 #solution 1  - hack  create a list and choose a ranodm index
 
+#solution 2- using random sampling- this is a special case of 
+#where k = 1 (ie length of reservior)
+
+
+
 class Solution:
 
     def __init__(self, head: Optional[ListNode]):
-        self.li =[]
-        ptr = head
-        while(ptr):
-  
-            self.li.append(ptr.val)
-            ptr = ptr.next
+        self.head  = head
     def getRandom(self) -> int:
-        ind = random.randint(0,len(self.li)-1)
-        return self.li[ind]
+        res = 0
+        count =1 
+        temp = self.head
+        while(temp!=None):
+            if( random.random() < 1/count ):
+                res = temp.val
+          
+            temp = temp.next
+            count+=1
+        return res
 
 
