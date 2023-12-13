94082738fdf5d424a95df2e47a7e632c46d33e68
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-12-13 09:53:57 +0530

    Time: 150 ms (77.37%) | Memory: 16.8 MB (14.98%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.py
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.py
@@ -1,5 +1,29 @@
+'''
+solution 1 
+
+Naive solution 
+for each 1 cell check if it is safe by iterating on its row and col
+
+t mn*(m+n)= 10^6
+s 1 
+
+#solution 2 - using map 
+
+create 2 map , on for row and other for col 
+where we map 
+
+row ind -> list of cols(where 1 present)
+and col_ind -> list of rows(where 1 present)
+
+t mn 
+s mn 
+
+'''
+#solution 1
 class Solution:
     def numSpecial(self, mat: List[List[int]]) -> int:
+
+       
         m,n  = len(mat),len(mat[0])
         def check_special(i,j):
             for r in range(m):
