8efc3ef552f5c49c5992305ae840b60e2c564e19
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-12-18 12:03:47 +0530

    Time: 157 ms (54.96%) | Memory: 17.8 MB (8.58%) - LeetSync

--- 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.py
+++ 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.py
@@ -4,13 +4,41 @@ sort and choose the 2 top element and 2 small element and find diff
 t nlogn
 s 1 
 
+
+
+#Solution 2
+itearate the array , parallely update max1,max2 , min1 , min2 
+t n
+s 1 
+
 '''
 
-
+#solution 2 
 class Solution:
     def maxProductDifference(self, nums: List[int]) -> int:
 
+        
 
-        nums.sort()
+        min_1, min_2  = float("inf") , float("inf")
 
-        return (nums[-1]*nums[-2])- (nums[0]*nums[1])
+        max_1, max_2  = -1, -1
+
+        n = len(nums)
+
+        for i in range(n):
+
+            if nums[i]<= min_1 :
+                min_2  = min_1 
+                min_1 =  nums[i]
+
+            elif nums[i]<=min_2:
+             
+                min_2 =  nums[i]
+
+            if nums[i]>= max_1:
+                max_2 = max_1 
+                max_1 = nums[i]
+            elif nums[i] >= max_2 :
+                max_2 = nums[i]
+           
+        return  (max_1*max_2) - (min_1*min_2)
