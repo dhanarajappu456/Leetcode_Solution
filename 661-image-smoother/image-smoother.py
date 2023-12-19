b3d385455beac9cae58feda79965bb96f265053c
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-12-19 11:16:06 +0530

    Time: 558 ms (18.21%) | Memory: 17.1 MB (37.10%) - LeetSync

--- 661-image-smoother/image-smoother.py
+++ 661-image-smoother/image-smoother.py
@@ -6,7 +6,12 @@ t mn*8
 s mn
 
 
-#solution 2 - without additional space
+#solution 2 - without additional space- using bits(inplace)
+
+when we are at a cell 
+we store the value in the cell such that , 8 lsb(since img[i][j] can at max be 255 ,) for the value that was in the cell before
+and the next msbs to store the actual ans , 
+thus each cell stores both the final ans and the value that was at the location earlier
 
 t mn*8
 s 1
@@ -30,11 +35,19 @@ class Solution:
                 for k in range(8):
                     x,y = i+d[k][0],j+d[k][1]
                     if (0<=x<m) and (0<=y<n):
-                        tot += img[x][y]
+                        #retrive the earlier value that was at img[i][j]
+                        #which is last 8bits
+                        tot += img[x][y]%256
                         count+=1
-                res[i][j] = math.floor(tot/count)
+                #store the result  followed by the actual value that was  at the img[i][j]
+                img[i][j] = ( (math.floor(tot/count) <<8) | img[i][j] )
 
-        return res
+        #retireive the result 
+        for i in range(m):
+            for j in range(n):
+                #tha ans is represented by next bits after removeing  8lsb 
+                img[i][j] = img[i][j]>>8
+        return img
 
 
                                
