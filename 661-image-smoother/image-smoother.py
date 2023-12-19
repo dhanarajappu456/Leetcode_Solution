5b278d1053e3285de8876dea9cb4c0d82db96a6a
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-12-19 11:08:16 +0530

    Time: 544 ms (24.45%) | Memory: 16.9 MB (84.82%) - LeetSync

--- 661-image-smoother/image-smoother.py
+++ 661-image-smoother/image-smoother.py
@@ -1,8 +1,22 @@
+'''
+solution 1 - with additional space
+
+for each cell find all neib's sum  and count and find ans
+t mn*8
+s mn
+
+
+#solution 2 - without additional space
+
+t mn*8
+s 1
+
+'''
+
+
+
+
 #Solution 1 
-
-#solution 2 
-
-
 class Solution:
     def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
         m,n  = len(img) , len(img[0])
@@ -21,7 +35,6 @@ class Solution:
                 res[i][j] = math.floor(tot/count)
 
         return res
-
 
 
                                
