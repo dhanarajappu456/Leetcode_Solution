7523ba739001c3a76af2101e585a0e65ab10382f
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-12-19 11:06:32 +0530

    Time: 545 ms (23.95%) | Memory: 17.1 MB (59.70%) - LeetSync

--- 661-image-smoother/image-smoother.py
+++ 661-image-smoother/image-smoother.py
@@ -1,25 +1,26 @@
+#Solution 1 
+
+#solution 2 
+
+
 class Solution:
     def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
         m,n  = len(img) , len(img[0])
-        sum_, count = [[img[i][j] for j in range(n)] for i in range(m)], [[1 for j in range(n)] for i in range(m)]
+        res =  [[ 0 for j in range(n)] for i in range(m)]
 
         d = [(-1,0),(1,0),(0,-1), (0,1), (-1,1),(1,-1),(-1,-1),(1,1)]
         for i in range(m):
             for j in range(n):
+                tot = img[i][j]
+                count =1 
                 for k in range(8):
                     x,y = i+d[k][0],j+d[k][1]
                     if (0<=x<m) and (0<=y<n):
-                        sum_[i][j] += img[x][y]
-                        count[x][y]+=1 
-    
-        
+                        tot += img[x][y]
+                        count+=1
+                res[i][j] = math.floor(tot/count)
 
-        for i in range(m):
-            for j in range(n):
-
-                sum_[i][j] = math.floor(sum_[i][j]/count[i][j])
-
-        return sum_
+        return res
 
 
 
