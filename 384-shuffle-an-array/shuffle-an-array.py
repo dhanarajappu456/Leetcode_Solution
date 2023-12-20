dc760eea75842f5c042fac609815deaa331ef64e
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-12-20 12:18:06 +0530

    Time: 149 ms (29.15%) | Memory: 20.6 MB (9.83%) - LeetSync

--- 384-shuffle-an-array/shuffle-an-array.py
+++ 384-shuffle-an-array/shuffle-an-array.py
@@ -1,8 +1,13 @@
 '''
 #solution 1 -bruteforc
+
+t n^2
+s n(temp used to restore the array)
 #solution2 - fisher yates - algo
 
 '''
+#solution 2
+
 class Solution:
 
     def __init__(self, nums: List[int]):
@@ -24,9 +29,8 @@ class Solution:
         n = len(self.nums)
         arr = self.nums[:]
         for i in range(n):
-            rand_ind  = random.randint(0,len(arr)-1)
-            self.nums[i] = arr[rand_ind]
-            arr.pop(rand_ind)
+            rand_ind  = random.randint(i,n-1)
+            self.nums[i],self.nums[rand_ind] = self.nums[rand_ind],self.nums[i]
         return self.nums
 
 
