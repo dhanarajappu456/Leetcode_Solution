bfbd217d1120c9b2dc43136c779d53d5b2dc0469
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-12-26 11:52:54 +0530

    Time: 500 ms (31.54%) | Memory: 18 MB (47.86%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.py
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.py
@@ -20,7 +20,24 @@ class Solution:
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
 
         mod = 10**9+7
-        
+        # @lru_cache(None)
+        # def rec(n,tar):
+            
+            
+        #     if n ==  0:
+        #         return int(tar==0)
+        #     if tar<0:
+        #         return 0
+        #     ans = 0
+        #     for i in range(1,k+1):
+        #         ans = (ans%mod + rec(n-1, tar-i)%mod)%mod
+        #     return ans
+
+        # return rec(n,target)
+
+        #solution2 
+
+        #dp 
     
     
         dp =[[0 for i in range(target+1)] for  j in range(n+1)]
@@ -30,8 +47,6 @@ class Solution:
                 ans = 0
                 for f  in range(1,k+1):
                     if j-f>=0:
-                        if i==2 and j==2:
-                            print(i-1,j-f,dp[i-1][j-f])
                         ans = (ans%mod + dp[i-1][j-f]%mod)%mod
                 dp[i][j]= ans 
 
