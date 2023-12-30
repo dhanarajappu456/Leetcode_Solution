5eefbc729dbaf2277f0096f38decaa8aae8e2059
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-12-30 10:27:58 +0530

    Time: 110 ms (5.08%) | Memory: 17.5 MB (9.52%) - LeetSync

--- 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.py
+++ 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.py
@@ -3,14 +3,14 @@ from collections import defaultdict as dict
 class Solution:
     def makeEqual(self, words: List[str]) -> bool:
 
-        chars_count =[0 for i in range(26)]
+        mp =dict(int)
         n  = len(words)
         for word  in words:
             for c in word:
 
-                chars_count[ord(c)-97]+=1 
+                mp[c]+=1 
     
-        for count in chars_count:
-            if count%n!=0:
+        for c in mp:
+            if mp[c]%n!=0:
                 return False
         return True
