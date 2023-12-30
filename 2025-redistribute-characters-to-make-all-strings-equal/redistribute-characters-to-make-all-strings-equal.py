11e1fb00d3d93d9120b89c5b6debbc9afcb346cd
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2023-12-30 10:29:50 +0530

    Time: 95 ms (5.08%) | Memory: 17.4 MB (9.52%) - LeetSync

--- 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.py
+++ 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.py
@@ -1,3 +1,15 @@
+'''
+
+Idea is simple since we can move any char from any position to any position 
+
+the thing is count of any char across all words must be multiple of number of words, 
+then only we can atleast make them equal 
+
+t  w*c = 100*100
+s  charmap  = 26
+
+'''
+
 from collections import defaultdict as dict 
 
 class Solution:
