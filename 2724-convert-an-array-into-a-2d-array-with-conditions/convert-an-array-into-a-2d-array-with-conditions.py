16ae62caec6c7f6b2b290448a28fba97d1ea2f62
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-01-02 12:06:48 +0530

    Time: 102 ms (5.01%) | Memory: 17.5 MB (5.17%) - LeetSync

---
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findMatrix(self, nums: List[int]) -> List[List[int]]:
+
+        mp ={}
+        for num in nums:
+            mp[num] = mp.get(num,0)+1
+        ans= []
+        while mp:
+            li =[]
+            keys = mp.keys()
+            for num in mp.copy():
+                li.append(num)
+                mp[num]-=1
+                if mp[num] ==0:
+                    mp.pop(num)
+
+            ans.append(li)
+    
+        return ans 
+
+        
