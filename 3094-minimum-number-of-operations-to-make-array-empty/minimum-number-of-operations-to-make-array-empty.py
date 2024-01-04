e158b6561795701430c918e75c5e145619ccd9ef
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-01-04 11:41:16 +0530

    Time: 530 ms (98.79%) | Memory: 32.1 MB (9.09%) - LeetSync

--- 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.py
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.py
@@ -1,45 +1,16 @@
-
-'''
-
-#soltuion 1 dp/memo
-
-once we create the count of each number , we used memo recursion to find the min operation to reduce a count of occurence of  a number to 0 
-
-time  n+ x
-space n(aux) + x(dict)
-
-n = max number of element in the array(might be the case same number is ocuuring in the array)
-x = distinct numbers in the array
-'''
-
-from functools import lru_cache
+#solution 2 - using greedy 
 
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
         
-
-        @lru_cache(None)
-        def dfs(count):
-            if count ==0:
-                return 0
-            if count ==1:
-                return float("inf")
-            elif count ==2:
-                return 1
-
-            
-            else:
-                return 1+min(dfs(count-2), dfs(count-3))
-            
         m =Counter(nums)
-        ans  =0 
-        for num in m:
-            min_op = dfs(m[num])
-            if min_op == float("inf"):
+        ans = 0 
+        for num in m: 
+            if m[num]==1:
                 return -1
-            ans+= min_op
+            else:
+                ans += math.ceil(m[num]/3)
         return ans
-        
        
     
 
