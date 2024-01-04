b18187d71a9993b6605252097c4b6bc66c4d1928
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-01-04 11:47:52 +0530

    Time: 21 ms (50.94%) | Memory: 63.3 MB (6.04%) - LeetSync

--- 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.java
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.java
@@ -2,12 +2,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Solution {
-    private Map<Integer, Integer>  memo = new HashMap<>();
-    private Map<Integer, Integer> m = new HashMap<>();
-
     public int minOperations(int[] nums) {
-       
-        
+        Map<Integer, Integer> m = new HashMap<>();
         int ans = 0;
 
         for (int num : nums) {
@@ -15,32 +11,13 @@ public class Solution {
         }
 
         for (int num : m.keySet()) {
-            int minOp = dfs(m.get(num));
-            if (minOp == Integer.MAX_VALUE) {
+            if (m.get(num) == 1) {
                 return -1;
+            } else {
+                ans += Math.ceil( m.get(num) / 3.0);
             }
-            ans += minOp;
         }
 
         return ans;
-    }
-
-    private int dfs(int count) {
-        if (count == 0) {
-            return 0;
-        }
-        if (count == 1) {
-            return Integer.MAX_VALUE;
-        } else if (count == 2) {
-            return 1;
-        } else {
-            if (memo.containsKey(count)) {
-                return memo.get(count);
-            }
-
-            int result = 1 + Math.min(dfs(count - 2), dfs(count - 3));
-            memo.put(count, result);
-            return result;
-        }
     }
 }
