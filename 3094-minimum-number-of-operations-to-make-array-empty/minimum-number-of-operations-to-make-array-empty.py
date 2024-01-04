f222442ca451f85390d6fa7ccdb2720bccf53b11
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-01-04 11:12:33 +0530

    Time: 511 ms (100.00%) | Memory: 32.1 MB (8.79%) - LeetSync

---
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        
+        m =Counter(nums)
+        ans = 0 
+        for num in m: 
+            if m[num]==1:
+                return -1
+            else:
+                ans += math.ceil(m[num]/3)
+        return ans
+       
+    
+
+            
+        
