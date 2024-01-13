18a177db864163e88965eebfbedbf098b82bf7fb
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-01-13 18:49:15 +0530

    Time: 162 ms (59.77%) | Memory: 17.9 MB (21.41%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -1,3 +1,16 @@
+'''
+greedy using map 
+
+to find the min number of chars to replace in s, 
+we need to find the max nuber of chars in s that already match with that of the chars in t
+
+for missing chars in s , we incremenet the ans , finally giving the required ans
+
+t n 
+s  n
+
+'''
+
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
         
