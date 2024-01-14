32a7a7cdc3e8cfc93b1b1708d6e36c9e3c75bc82
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-01-14 22:57:22 +0530

    Time: 176 ms (50.49%) | Memory: 18.2 MB (46.64%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
@@ -1,3 +1,14 @@
+'''
+solution 1 
+if at all possible then the set of chars in both should be same
+and 
+also both words should have same set of frequencies  in the word1 and word2
+
+t n 
+s n 
+
+'''
+
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
         mp1 = {}
@@ -12,10 +23,15 @@ class Solution:
         
         for  c in word2:
             mp2[c] = mp2.get(c,0)+1
+
+        #collecting all the freq of letter in word1 
         for c in mp1:
             freq1.append(mp1[c])
+        #collecting all the freq of letters in the word2
         for c in mp2:
             freq2.append(mp2[c])
         freq1.sort()
         freq2.sort()
+
+        
         return freq1 == freq2  and mp1.keys() == mp2.keys()
