2454cd01eb3f9a0abc4760d98e95223a7e5cc1fc
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-01-22 12:47:33 +0530

    Time: 168 ms (62.24%) | Memory: 17.8 MB (94.40%) - LeetSync

--- 645-set-mismatch/set-mismatch.py
+++ 645-set-mismatch/set-mismatch.py
@@ -1,18 +1,17 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        set_=set()
-        result=[]
-        xor_=0
-        miss,dup=0,0
-        for i in nums:
-            set_.add(i)
-            
-        for i in range(len(nums)):
-            xor_^=(i+1)^nums[i]
-            
-        for i in range(1,len(nums)+1):
-            if(i not in set_):
-                miss=i
-        dup=miss^xor_    
-        return([dup,miss])
-            
+        ans1 = 0
+        ans2 = 0
+        n = len(nums)
+        for i in range(n):
+            num = nums[i]
+            #m-d
+            ans1 +=  i+1 - num
+            #m^2 - d^2
+            ans2 += (i+1)**2- num**2
+
+        ans3 = ans2//ans1
+
+        miss = (ans1+ans3)//2
+        dup = miss-ans1
+        return [dup,miss]
