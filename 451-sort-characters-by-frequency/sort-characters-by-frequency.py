bda12d3381ce116c42be192b7d02421aa323ab83
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-02-07 22:34:21 +0530

    Time: 46 ms (65.01%) | Memory: 17.8 MB (71.88%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.py
@@ -1,15 +1,30 @@
+'''#SOLUTION1 
+USING DICT AND ARR WITH SORTING
+
+
+SOLUTION 2 
+
+USING DICT MAXHEAP
+
+T N 
+S  NLOGN
+
+'''
+import heapq as h 
 class Solution:
     def frequencySort(self, s: str) -> str:
         d =dict()
+        heap =[]
         for c in s:
             d[c]= d.get(c,0)+1
-        cnts =[]
+        ans  = []
+
         for c in d:
             cnt = d[c]
-            cnts.append((cnt,c))
-        cnts.sort()
-        ans = []
-        for cnt,char in cnts[::-1]:
-            ans.append(char*cnt)
+            h.heappush(heap,(-cnt,c))
+        while heap:
+            cnt,char  = h.heappop(heap)
+            ans.append(char*(-cnt))
+        
         return "".join(ans)
 
