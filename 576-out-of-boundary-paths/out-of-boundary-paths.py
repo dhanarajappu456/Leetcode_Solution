c3a0969d2c76ed98c968f7aacd65a059279038c1
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-02-11 18:23:12 +0530

    Time: 99 ms (41.04%) | Memory: 22.8 MB (9.37%) - LeetSync

--- 576-out-of-boundary-paths/out-of-boundary-paths.py
+++ 576-out-of-boundary-paths/out-of-boundary-paths.py
@@ -1,5 +1,5 @@
 '''
-
+#solution 1 
 classic dp problem , where the i,j indicate the cell address and the rem denote the number of remaining moves
 
 the state rec(i,j, rem) denote the number  ways we can move out from the cell i,j with rem number of
