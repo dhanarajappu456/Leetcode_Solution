08ac7c19ef39c039d58e0d6ad680e7b7c4c6f733
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-02-11 18:23:03 +0530

    Time: 99 ms (41.04%) | Memory: 22.8 MB (9.37%) - LeetSync

--- 576-out-of-boundary-paths/out-of-boundary-paths.py
+++ 576-out-of-boundary-paths/out-of-boundary-paths.py
@@ -1,3 +1,16 @@
+'''
+
+classic dp problem , where the i,j indicate the cell address and the rem denote the number of remaining moves
+
+the state rec(i,j, rem) denote the number  ways we can move out from the cell i,j with rem number of
+moves  left
+
+t mn*maxMoves
+s mn*MaxMoves + aux
+
+'''
+
+
 class Solution:
     def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
         dir =[(0,1),(1,0),(-1,0),(0,-1)]
