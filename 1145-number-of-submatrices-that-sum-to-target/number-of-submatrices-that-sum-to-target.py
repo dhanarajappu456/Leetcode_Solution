661e6c2a35b5102e835b6eb0e1e5bb7396eb7d0d
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-02-16 00:01:55 +0530

    Time: 412 ms (83.66%) | Memory: 17.5 MB (82.42%) - LeetSync

--- 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.py
+++ 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.py
@@ -1,19 +1,35 @@
 class Solution:
     def numSubmatrixSumTarget(self, matrix: List[List[int]], target: int) -> int:
-        n=len(matrix)
-        m=len(matrix[0])
-        ans=0
-        for r in matrix:
-            for i in range(1,len(r)):
-                r[i]+=r[i-1]
-        for start in range(m):
-            for end in range(start,m):
-                lookup=defaultdict(int)
-                cumsum=0
-                lookup[0]=1
-                for k in range(n):
-                    cumsum+=matrix[k][end] - (matrix[k][start-1] if start !=0 else 0)
-                    if cumsum -target in lookup:
-                        ans+=lookup[cumsum-target]
-                    lookup[cumsum]+=1
-        return ans     
+        m=len(matrix)
+        n=len(matrix[0])
+        pref =[[0 for i in range(n)] for j in range(m)]
+        ans =0 
+        for i in range(m):
+            prefix_sum =0 
+            for j in range(n):
+                prefix_sum+= matrix[i][j]
+                
+                pref[i][j] = prefix_sum
+        
+    
+        
+        for c1 in range(n):
+            for c2 in range(c1,n):
+                mp = {0:1}
+                cum_sum = 0 
+                for r in range(m):
+                    if c1==0:
+                        cum_sum+= pref[r][c2]
+                    else:
+                        cum_sum += pref[r][c2] - pref[r][c1-1]
+                        
+                    ans+= mp.get(cum_sum - target,0)
+                    
+                    mp[cum_sum] = mp.get(cum_sum,0)+1
+        return ans
+                        
+                        
+                    
+                    
+                    
+        
