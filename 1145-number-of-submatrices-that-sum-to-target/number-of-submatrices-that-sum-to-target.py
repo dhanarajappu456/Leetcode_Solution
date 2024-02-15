c2aa46e8fcdc6b3db3ddef4377c1e4e60053d070
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-02-16 00:08:23 +0530

    Time: 443 ms (60.11%) | Memory: 17.5 MB (65.88%) - LeetSync

--- 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.py
+++ 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.py
@@ -1,3 +1,21 @@
+'''
+
+map and  prefix sum 
+
+this is problem is similar to the problem , to find the subarray with target k in 1d array 
+
+
+we store the pref sum for each row. 
+
+for each pair of  start column  c1 and end column  c2  we find each sub matrix starting from the starting row. during this process we find if we can get the sub matrix with c1 as starting col and c2 as ending column  where map is used to store the cumsum of matrices starting from 0th row to current row with c1 and c2 as the ending column
+
+
+t n^2*m where  ,m,n are the row and column
+s mn  - pref array
+
+'''
+
+
 class Solution:
     def numSubmatrixSumTarget(self, matrix: List[List[int]], target: int) -> int:
         m=len(matrix)
