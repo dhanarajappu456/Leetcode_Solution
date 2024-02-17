5ec2a54c267de212c1e2e52d57a1a4df57c58de3
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-02-17 15:10:29 +0530

    Time: 431 ms (47.83%) | Memory: 31 MB (73.99%) - LeetSync

--- 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.py
+++ 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.py
@@ -1,3 +1,35 @@
+'''
+
+solution 1 using 3d dp
+
+ind,rembrick, remladd
+
+make choices at each index
+
+t n*b*l
+s n*b*l
+
+
+solution 2 using maxHeap
+
+idea is to use the ladder as super power to jump very large heights
+and use brick when making small jumps.
+
+
+
+but we dont know which is larger jump, where we need to use the ladder 
+
+so , idea is we start using brick at all possible situaion and , when we 
+no more can use the rem brick for current jump we see in past we have used a max number of bricks
+that >= current cost of jumping , in which case we use ladder in that past postion and 
+collect those bricks to make current jump 
+
+
+t nlogb b = brick , n =len o f heights 
+s b(brick)
+'''
+
+
 import heapq as h
 
 class Solution:
