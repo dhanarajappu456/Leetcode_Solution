412416c05ef0f562d238d24d82e7ed70e372bb0f
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-02-19 15:02:27 +0530

    Time: 1295 ms (33.96%) | Memory: 62.8 MB (68.49%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.py
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.py
@@ -6,7 +6,7 @@ class Solution:
         available  =[i for i in range(n)]
         h.heapify(available)
         meetings.sort(key = lambda x: (x[0],x[1]))
-        #print(meetings)
+       
         curr_meeting = []
         for m in meetings:
             
@@ -19,15 +19,12 @@ class Solution:
                 h.heappush(curr_meeting,(new_end,ind))
                 rooms[ind]+=1
             else:
-                # while(curr_meeting and curr_meeting[0][0]<=m[0]):
-                #     end, ind = h.heappop(curr_meeting)
-                #     available.add(ind)
+           
                     ind = h.heappop(available)
                     end = m[1]
                     h.heappush(curr_meeting,(end,ind))
                     rooms[ind]+=1
-            #print(curr_meeting)
-        #print(rooms)
+         
         max_ind = None
         count= 0
         for ind,cnt  in enumerate(rooms) :
