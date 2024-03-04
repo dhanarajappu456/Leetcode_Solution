1a5b2e1745d9bb10476d83e76a31ccad716fc5d4
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-03-04 12:06:43 +0530

    Time: 53 ms (59.76%) | Memory: 16.7 MB (64.63%) - LeetSync

--- 985-bag-of-tokens/bag-of-tokens.py
+++ 985-bag-of-tokens/bag-of-tokens.py
@@ -1,3 +1,19 @@
+'''
+solution - sort and two pointer(greedy)
+
+We know we get a score of 1 for each token , no matter what order we collect , the coin 
+so it is ideal to 
+
+1) perform the face up -  collect all the token  sum of whose value is < current power we have.
+2) then we need to increase the  power - by face down , in this case it is ideal to get the token that can give max power 
+that is to take a token with max power
+note when performing the facedown , we need to have a score of atleast 1, to payoff for the operation.
+
+All in all the token must be sorted with the 2 pointers at each end.
+
+t n 
+s 1 
+'''
 class Solution:
     def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
 
