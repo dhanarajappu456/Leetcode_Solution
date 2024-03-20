2046d999630a15ef6a40cb4c03a28e29d57fd45b
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-03-20 12:08:33 +0530

    Time: 182 ms (89.59%) | Memory: 21.1 MB (31.60%) - LeetSync

--- 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.py
+++ 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.py
@@ -1,8 +1,12 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+'''The idea is simple 
+
+find the prev node of node a as s   and next node of node b as e 
+then connect s to head of list2 and tail of list2 to e
+
+t n 
+s 1
+
+'''
 class Solution:
     def mergeInBetween(self, list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode:
         #creating dummy node
@@ -16,7 +20,7 @@ class Solution:
 
 
         #fiding the prev node of node at a and and next  node of b 
-        #strored in s and e 
+    
         p1= list1 
         prev = d
         curr = list1
@@ -24,15 +28,17 @@ class Solution:
         s,e = None,None
         while(pos!=b):
             if pos==a:
-                print(pos,a)
                 s = prev
-         
             prev = prev.next
             curr =curr.next
             pos+=1
+        
+        # handling edge case case when a and b are same
         if s == None:
             s = prev
         e =  curr.next
+        # joining the prev node of a in list1  to start head of list2 
+        # and joining the tail of list2  to next node of node b in list1 
         s.next = list2
         l2_end.next = e
         return d.next
