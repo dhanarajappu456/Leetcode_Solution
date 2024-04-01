6d571471070eb80ac4c23ef25699b3d7103f5006
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-04-01 17:40:30 +0530

    Time: 706 ms (65.46%) | Memory: 30.7 MB (88.02%) - LeetSync

--- 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.py
+++ 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.py
@@ -1,19 +1,22 @@
 class Solution:
     def countSubarrays(self, nums: List[int], mink: int, maxk: int) -> int:
         res = 0
+        #keeps track of bad , min ,max index
         bad_index, min_index, max_index = -1,-1,-1
         
         for i, num in enumerate(nums) :
+            #bad index is the index of recent value that is not in range[mink,maxk]
             if num<mink or maxk<num:
                 bad_index = i
-
+            #update the min index
             if num == mink:
                 min_index = i
-
+            #update the max index
             if num == maxk:
                 max_index = i
+            # this is left most index among indices where recent min and max element  lies
             left_index = min(min_index, max_index)
-          
+            #counts the valid subarray
             res += max(0,left_index - bad_index)
 
         return res
