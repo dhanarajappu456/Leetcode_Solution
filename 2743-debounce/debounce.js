970a1983b7ced9dfb5921b5081c8c75ea7f75822
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-04-03 22:04:51 +0530

    Time: 60 ms (50.94%) | Memory: 49.4 MB (40.87%) - LeetSync

--- 2743-debounce/debounce.js
+++ 2743-debounce/debounce.js
@@ -6,6 +6,9 @@
 var debounce = function(fn, t) {
     let id; 
     return function(...args) {
+        //if current function call is before execution of previous function call which was scheduled
+        //in another word there is no atleast a gap of time b/w two successive calls , cancel previous 
+        //scheduled call , and schedule current call to be executed after t 
         clearTimeout(id)
         id  = setTimeout(()=>fn(...args),t)
     }
