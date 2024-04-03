068e55e7ecbc9a61cab21bab49eee6fb270409a2
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-04-04 00:48:48 +0530

    Time: 56 ms (22.46%) | Memory: 48.5 MB (68.77%) - LeetSync

--- 2734-array-prototype-last/array-prototype-last.js
+++ 2734-array-prototype-last/array-prototype-last.js
@@ -1,6 +1,12 @@
 /**
  * @return {null|boolean|number|string|Array|Object}
  */
+//  1)if you call like this ->Array.prototype.last()  
+//   "this " refer to the Array.prototype object
+
+//2.if u call like this -> arr = [12,4]
+//arr.last() , "this" refer to the current array object
+
 Array.prototype.last = function() {
     console.log("hai",this)
     return this.length  === 0 ? -1 : this[this.length-1]
