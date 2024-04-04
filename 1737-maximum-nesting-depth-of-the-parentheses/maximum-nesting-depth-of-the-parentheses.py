5d1bb506fc91a10d4a9e26d3e51d62f793a120e1
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-04-04 11:55:13 +0530

    Time: 34 ms (66.43%) | Memory: 16.5 MB (38.30%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.py
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.py
@@ -1,19 +1,16 @@
 class Solution:
     def maxDepth(self, s: str) -> int:
-
-        stk  =[]
-     
+        #keeps track of open pars at any time
+        open = 0
         ans  = 0 
         for c in s:
             if c  =="(":
-        
-                stk.append(c)
+                open+=1
             #it is is said s is vps , so the parenthesis will be such that they match and 
             #is balanced, so for closing bracket an open bracket will be there for sure
             elif c ==")":
-  
-                stk.pop(-1)
-            ans = max(ans , len(stk))
+                open-=1
+            ans  = max(ans, open)
             
         return ans 
         
