4281d776e0d321778393a65dd81ab3a1ae76401d
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-04-05 00:12:07 +0530

    Time: 71 ms (6.79%) | Memory: 51.7 MB (6.45%) - LeetSync

---
+++ 2797-event-emitter/event-emitter.js
@@ -0,0 +1,45 @@
+class EventEmitter {
+    
+    /**
+     * @param {string} eventName
+     * @param {Function} callback
+     * @return {Object}
+     */
+    // this is  new way of declaring the instance variable in ES 6
+    eventMap  = {}
+    subscribe(eventName, callback) {
+        if(!(eventName in this.eventMap))
+            this.eventMap[eventName] = new Set();
+        this.eventMap[eventName].add(callback);
+        console.log(this.eventMap)
+        return {
+            unsubscribe: () => {
+                this.eventMap[eventName].delete(callback);
+            }
+        };
+    }
+    
+    /**
+     * @param {string} eventName
+     * @param {Array} args
+     * @return {Array}
+     */
+    emit(eventName, args = []) {
+        const res = [];
+        //coalescing operator , you can also use || operator 
+        (this.eventMap[eventName] ?? []).forEach((cb)=> res.push(cb(...args)));
+        return res;
+    }
+}
+
+/**
+ * const emitter = new EventEmitter();
+ *
+ * // Subscribe to the onClick event with onClickCallback
+ * function onClickCallback() { return 99 }
+ * const sub = emitter.subscribe('onClick', onClickCallback);
+ *
+ * emitter.emit('onClick'); // [99]
+ * sub.unsubscribe(); // undefined
+ * emitter.emit('onClick'); // []
+ */
