846c701028bc625fdadbd64f4c02e964d38beee5
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-04-05 01:11:43 +0530

    Time: 98 ms (64.87%) | Memory: 68.2 MB (72.65%) - LeetSync

--- 2759-flatten-deeply-nested-array/flatten-deeply-nested-array.js
+++ 2759-flatten-deeply-nested-array/flatten-deeply-nested-array.js
@@ -3,6 +3,16 @@
  * @param {number} depth
  * @return {Array}
  */
+
+/*
+recursion - 
+if current depth is <=n 
+    if current element is number just append to res
+    if current element is array , we need to further rec calls 
+else
+    just append the current array to res
+
+ */
 var flat = function (arr, n) {
 
 
@@ -13,14 +23,16 @@ var flat = function (arr, n) {
         if(depth<=n){
 
             for(const item of arr){
-                  
+                //call recursion for the array
                 if (item instanceof Array)
                     rec(item,depth+1);    
+                //append the element to res
                 else
                     res.push(item);
             
             } 
         }
+        // just push current array to res
         else
             res.push(arr);
        
