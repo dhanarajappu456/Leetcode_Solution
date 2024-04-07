418b1f0b277c1cfa5d8ecd4fffe46ab6f8d4630f
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-04-07 14:56:28 +0530

    Time: 36 ms (54.61%) | Memory: 16.6 MB (21.36%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.py
+++ 678-valid-parenthesis-string/valid-parenthesis-string.py
@@ -1,61 +1,42 @@
-#solution 1 - with 2 stack , greedey 
-from collections import deque 
+#solution 3 - with 2 iteration from front and end
 
 class Solution:
     def checkValidString(self, s: str) -> bool:
       
         '''
-        we use 2 stack , one for pushing the  parenth
-        other for aster
+        1)iterating from start and we sum up total opens , 
+        with aster being taken as open 
+        during this , if open becomes <0 return False
+        essentially we check , if there are sufficient opens for closed pars
 
-        when we find there is not mathching open par in stack for a closed par
-        we check if aster stack has one, which can be assumed as open par
-
-        at end of loop , the par stack has only open pars, so 
-        we need to check if asterisk stack has aserisks, which can be assumed as closed pars
-
+        2)iterating from end  and we sum up total closed , 
+        with aster being taken as closed 
+        during this , if closed becomes <0 return False
+        essentially we check , if there are sufficient closed pars for open pars
         t n 
-        s n
+        s 1
 
         '''
-        aster_stk= []
-        par_stk = []
-        for i,c in enumerate(s) :
-            if c == "(":
-                #index is needed when matching open pars in stack to asterisk as closed pars
-                #at the end of the for loop, where asterisk can be taken as closed pars, iff it comes after the open 
-                #pars
-                par_stk.append((i,"("))
-            elif c == ")":
-                #direct pop if there is opening bracket seen so far
-                if par_stk and par_stk[-1][1] == "(":
-                    par_stk.pop(-1)
-                #assuming * as (
-                elif aster_stk:
-                    aster_stk.pop(-1)
-                else:
-    
-                    return False
-            #add the *  to deq
+        close,open  = 0,0
+        for c in s :
+            if c ==")": 
+                open-=1
             else:
-                aster_stk.append(i)
-        # now the stack will only have (
-        # so we need to match it with * assuming as )
-        #where we need to pop asterisk from aster stack which can be assumed as 
-        #) , if it comes after this open par 
-
-        while(par_stk):
-            #check if the * comes after the (, then only it can be matched
-            if  par_stk and  aster_stk and aster_stk[-1]>par_stk[-1][0]:
-                par_stk.pop(-1)
-                aster_stk.pop(-1)
-     
-            else:
+                open+=1
+            
+            if open<0:
                 return False
-           
-        
-        return True
 
+        
+        for c in s[::-1]:
+            if c =="(": 
+                close-=1
+            else:
+                close+=1
+            if close<0:
+                return False
+        return True
     #t n 
-    # s n 
+    # s n1
+
 
