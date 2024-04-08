7ff88b976ce4dbc61fcd29a60319487910d72b15
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-04-08 14:37:33 +0530

    Time: 45 ms (23.85%) | Memory: 16.6 MB (59.90%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
@@ -1,3 +1,5 @@
+#for this problem the sandwich stack given is reveresed stack , 
+#that is top is on left , not on right
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
 
@@ -8,6 +10,11 @@ class Solution:
         #check if we can consume it ( possible iff there is atlest one student of that type, else ,cant so return
         #remainnig number of students)
         #if possible eat we decrement cnt of that type of students
+
+        #eg test , students  = [1,1,1]
+        #sandwich = [0,1,1] 
+        #ans =3 (as no students of type 0, is remaining, if at all we can try deq and enq them so that 
+        #they match the top of stack)
 
         stud_0, stud_1 = students.count(0), students.count(1)
         stud_cnt = [stud_0, stud_1]
