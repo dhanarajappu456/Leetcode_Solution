fcc6f8ee8bbce9e594a09b482a4b9be0a3889ab2
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-04-08 23:29:45 +0530

    Time: 1107 ms (5.16%) | Memory: 21.3 MB (6.99%) - LeetSync

--- 902-minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.py
+++ 902-minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.py
@@ -20,12 +20,14 @@ class Solution:
         
         for i in range(1,n+1):
             for j in range(1,i+1):
-
+                #not choose the ith pump
                 dp[i][j] = dp[i-1][j]
+                #choose the ith pump
                 if dp[i-1][j-1]>=stations[i-1][0]:
                     dp[i][j]  = max(dp[i-1][j], dp[i-1][j-1]+stations[i-1][1])
 
-     
+        #last row is the max dist can go , considering all n pumps
+        #so ,just go through this row to get first col which has value maxdist >=target
         for j in range(n+1):
             if dp[n][j]>=target:
                 return j
