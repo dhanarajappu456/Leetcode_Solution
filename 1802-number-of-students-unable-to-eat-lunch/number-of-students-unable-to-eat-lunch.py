28afaada75611c5b499eac9851fb09bceec894b1
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-04-08 14:28:49 +0530

    Time: 31 ms (95.68%) | Memory: 16.5 MB (95.17%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
@@ -2,30 +2,17 @@ class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
 
         #solution 1  - simulation
-        students = deque(students)
+        stud_0, stud_1 = students.count(0), students.count(1)
+        stud_cnt = [stud_0, stud_1]
+       
         i=0
-        while(sandwiches and students):
-
-            tries =0 
+        while(i<len(sandwiches)):
           
-            #we need to try removing from frontof queue and ut at end of it 
-            # if top of stack cant be consumed by current student
-            while(tries<len(students) and students[0]!=sandwiches[i]):
-                tries+=1
-                students.append(students.popleft())
-            #this means without removing and adding all the students
-            #the loop ended, means we have a match , in which case consume top stack
-            
-            if tries<len(students):
-                students.popleft()
-                #updating the index of sandwich , to next sandwich
-                i+=1
-            
-            #tried all students , no match so return remaining number of students
-            else:
-                
-                return len(students)
-            
+            if stud_cnt[sandwiches[i]] ==0:
+                return sum(stud_cnt)
+            stud_cnt[sandwiches[i]]-=1    
+            i+=1
+        
         return 0 
 
 
