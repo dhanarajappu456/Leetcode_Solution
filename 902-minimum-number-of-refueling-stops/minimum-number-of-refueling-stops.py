e604cae6b1b94beb788cc2b845c11b02fbd683b1
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-04-08 23:25:14 +0530

    Time: 1099 ms (5.16%) | Memory: 21.7 MB (6.16%) - LeetSync

---
+++ 902-minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.py
@@ -0,0 +1,31 @@
+import heapq as h
+
+class Solution:
+    def minRefuelStops(self, target: int, startFuel: int, stations: List[List[int]]) -> int:
+
+        n = len(stations)
+        dp  =[[0 for j in range(n+1)] for i in range(n+1)]
+
+        for i in range(n+1):
+            dp[i][0] = startFuel
+
+        
+        for i in range(1,n+1):
+            for j in range(1,i+1):
+
+                dp[i][j] = dp[i-1][j]
+                if dp[i-1][j-1]>=stations[i-1][0]:
+                    dp[i][j]  = max(dp[i-1][j], dp[i-1][j-1]+stations[i-1][1])
+
+     
+        for j in range(n+1):
+            if dp[n][j]>=target:
+                return j
+        return -1
+
+            
+
+
+
+
+
