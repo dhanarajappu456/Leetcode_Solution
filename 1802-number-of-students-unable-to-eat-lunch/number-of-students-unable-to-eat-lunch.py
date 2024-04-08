08e712e6f03a7d8ee70f9144c3da9e6904b585c0
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-04-08 14:38:22 +0530

    Time: 35 ms (84.79%) | Memory: 16.5 MB (59.90%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
@@ -3,31 +3,35 @@
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
 
-        #solution 1  - simulation
-        students = deque(students)
-        i=0
-        while(sandwiches and students):
+        #solution 2 using variable
+        #keep track of cnt of students consuming each type
+        #now we kinda simulate the solution1 
+        #such that , we take top of sandwich stack denoted by ith variable,
+        #check if we can consume it ( possible iff there is atlest one student of that type, else ,cant so return
+        #remainnig number of students)
+        #if possible eat we decrement cnt of that type of students
 
-            tries =0 
-          
-            #we need to try removing from frontof queue and ut at end of it 
-            # if top of stack cant be consumed by current student
-            while(tries<len(students) and students[0]!=sandwiches[i]):
-                tries+=1
-                students.append(students.popleft())
-            #this means without removing and adding all the students
-            #the loop ended, means we have a match , in which case consume top stack
+        #eg test , students  = [1,1,1]
+        #sandwich = [0,1,1] 
+        #ans =3 (as no students of type 0, is remaining, if at all we can try deq and enq them so that 
+        #they match the top of stack)
+
+        stud_0, stud_1 = students.count(0), students.count(1)
+        stud_cnt = [stud_0, stud_1]
+       
+        i=0
+        while(i<len(sandwiches)):
+            #if we cant consume curretn item in the sandwich stack(ie, no such type pf student remaining =)
+            #return the rem number of students
+       
+            if stud_cnt[sandwiches[i]] ==0:
+                return sum(stud_cnt)
+            # student of type of current item takes the current item
             
-            if tries<len(students):
-                students.popleft()
-                #updating the index of sandwich , to next sandwich
-                i+=1
-            
-            #tried all students , no match so return remaining number of students
-            else:
-                
-                return len(students)
-            
+            stud_cnt[sandwiches[i]]-=1   
+            #assumption of pop 
+            i+=1
+        
         return 0 
 
 
