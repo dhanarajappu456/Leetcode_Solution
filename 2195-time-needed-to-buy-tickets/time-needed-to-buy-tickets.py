5abdbd6c85eaefcb3e52d20bfd4c7bb283ee2f60
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-04-09 11:33:09 +0530

    Time: 53 ms (26.26%) | Memory: 16.5 MB (51.83%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
@@ -2,37 +2,20 @@ class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
 
 
-        # front,back  =Counter( tickets[:k]), Counter(tikets[k+1:])
-        # needed = tickets[k]
-        # ans = 0 
-        # while(needed):
-
-        #     ans += len(front),len(back)
-        #     for k in front:
-        #         front[k]-=1
-        #         if front[k] ==0:
-        #             front.pop(k)
-
-        #     for k in back:
-        #         back[k]-=1
-        #         if back[k] ==0:
-        #             back.pop(k)
-        #     needed-=1
-        # return ans
+        #simulation 1 
+        #t - kn
+        #s   n 
         t  =deque([(i,num) for i,num in enumerate(tickets)])
         need = tickets[k]
         ans = 0 
         while(need):
-           
             ind, val  =t.popleft()
             val-=1
             if val!=0:
                 t.append((ind, val))
-          
             if ind ==k:
                 need-=1
             ans+=1
-            
 
         return ans
         
