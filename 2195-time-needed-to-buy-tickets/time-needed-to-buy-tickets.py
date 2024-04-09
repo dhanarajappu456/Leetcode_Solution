c7f177c21513f110f2b1e85fad820840badf4444
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-04-09 11:58:48 +0530

    Time: 35 ms (92.79%) | Memory: 16.7 MB (16.09%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
@@ -8,9 +8,11 @@ class Solution:
         #s 1
         ans = 0 
         for i,num in enumerate(tickets):
+            #values to left of k
             if i<=k:
 
                 ans += min(num ,  tickets[k])
+            #values to right of k 
             else:
                 ans += min(num, tickets[k]-1)
         return ans 
