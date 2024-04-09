9711a0353c654beb35a4ca8d41d3a93f30aed4d6
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-04-09 16:12:22 +0530

    Time: 938 ms (13.66%) | Memory: 17.3 MB (56.63%) - LeetSync

--- 1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.py
+++ 1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.py
@@ -4,46 +4,41 @@ class Solution:
         #create the prefix sum for each row 
 
         #fixing the left side and right side for rectangle as c1 and c2 as the column 
-        #see the number of submatices with in this 
+        #see the number of submatices with in this left and right as boundary
 
 
         m,n  =len(mat), len(mat[0])
-        pref = [[0 for i in range(n)] for j in range(m)]
+        pref = [[0 for i in range(n+1)] for j in range(m+1)]
         ans = 0 
         for i in range(m):
             s = 0
             for j in range(n):
                 s+= mat[i][j]
-                pref[i][j] = s
-        # print(pref)
+                pref[i+1][j+1] = s
+       
         for c1 in range(n):
 
             for c2 in range(c1,n):
                 start =0 
                 
-                ones = pref[start][c2] - (pref[start][c1-1] if c1-1>= 0  else 0 )
+                ones = pref[start+1][c2+1] - pref[start+1][c1] 
                 #skip all rows that dont have the c2-c1+1 number of 1's
                 while(start<m):
                     while((start<m )and (ones!=(c2-c1+1))):
-                    
                         start+=1
                         if start<m :
-                            ones = pref[start][c2] - (pref[start][c1-1] if c1-1>= 0  else 0 )
-                        
+                            ones = pref[start+1][c2+1] - pref[start+1][c1] 
                     end  = start
                     #find the start and end row with c2-c1+1 number of 1's
                     if end<m: 
-                            ones = pref[end][c2] - (pref[end][c1-1] if c1-1>= 0  else 0 )
-                
+                            ones = pref[end+1][c2+1] - pref[end+1][c1] 
                     while((end < m) and( ones == (c2-c1+1))):
-                        #print("inner",c1,c2, start,end)
                         ans+= end-start+1
                         end+=1
                         if end<m: 
-                            ones = pref[end][c2] - (pref[end][c1-1] if c1-1>= 0  else 0 )
+                            ones = pref[end+1][c2+1] - pref[end+1][c1] 
                     start = end
                 
-                # print(c1,c2,ans)
         return ans
 
             
