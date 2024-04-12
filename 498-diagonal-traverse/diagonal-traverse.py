c0af7dc0f094b24558ec201aca3cc4cbbe2a2d64
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-04-12 16:44:27 +0530

    Time: 130 ms (98.95%) | Memory: 19.6 MB (63.49%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.py
+++ 498-diagonal-traverse/diagonal-traverse.py
@@ -10,21 +10,69 @@ understanding the fact theaty each diagonal is identified by
  s mn
 
 '''
+# class Solution:
+#     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+#         m,n = len(mat),len(mat[0])
+#         arr = [ deque([]) for i in range(m+n-1)]
+  
+#         for i in range(m):
+#             for j in range(n):
+#                 if (i+j)%2 !=0:
+#                     arr[i+j].append(mat[i][j])
+#                 else:
+#                     arr[i+j].appendleft(mat[i][j])
+     
+#         ans  = []
+#         for a in arr:
+           
+#             for v in a:
+#                 ans.append(v)
+#         return ans
+
+#solution - 2
+
 class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
         m,n = len(mat),len(mat[0])
-        arr = [ deque([]) for i in range(m+n-1)]
-  
-        for i in range(m):
-            for j in range(n):
-                if (i+j)%2 !=0:
-                    arr[i+j].append(mat[i][j])
-                else:
-                    arr[i+j].appendleft(mat[i][j])
-     
         ans  = []
-        for a in arr:
-           
-            for v in a:
-                ans.append(v)
+        upward  = True
+        i,j = 0,0
+        print()
+        while(i<m and j<n):
+            ans.append(mat[i][j])
+            #case1 - moving  up
+            if upward == True:
+                #when reaching upper edge
+                if i ==0 and j!=n-1:
+                    j+=1
+                    upward = False
+
+                #when reaching right edge
+                elif j == n-1:
+                    i+=1
+                    upward =False
+                #other case
+
+                else:
+                    i-=1
+                    j+=1
+
+            #case 2 - moving  down
+            else:
+                #when reaching bottom edge
+                if i ==m-1 and j!=n-1:
+                    j+=1
+                    upward = True
+
+                #when reaching left edge
+                elif j == 0:
+                    i+=1
+                    upward =True
+                #other case
+                else:
+                    i+=1
+                    j-=1
         return ans
+
+  
+        
