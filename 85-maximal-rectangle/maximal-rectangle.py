f1924bbe4156b0be4f0981a02e14da982b5a2218
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-04-13 17:18:52 +0530

    Time: 459 ms (19.29%) | Memory: 18 MB (34.16%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.py
+++ 85-maximal-rectangle/maximal-rectangle.py
@@ -2,30 +2,72 @@ class Solution:
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
 
 
+    
+       
         ans = 0 
-        m,n  = len(matrix) , len(matrix[0])
-        pref  =[[0 for j  in range(n+1)] for i in range(m+1)]
-        #storing pref sum for each row
-        for i in range(m):
-            pref_sum =0 
+        m,n   =len(matrix), len(matrix[0])
+        # maxArea = float("-inf")
+        # for i in range(len(heights)):
+        #     index = i
+        #     while(stk and stk[-1][1]>=heights[i]):
+        #         index,height = stk.pop()
+        #         width = i-index
+        #         area =  width * height
+        #         maxArea =max(maxArea , area)
+        #     stk.append((index,heights[i]))
+        # return maxArea
+        '''
+        ["0","1","1","0","1"],
+        ["1","1","0","1","0"],
+        ["0","1","1","1","0"],
+        ["1","1","1","1","0"],
+        ["1","1","1","1","1"],
+        ["0","0","0","0","0"]]
+        '''
+
+        heights =[0   for j in range(n) ]
+      
+        def helper(heights):
+            print(heights)
+            max_area  = 0 
+            stk = [(-1,-1)]
             for j in range(n):
-                pref_sum+= int(matrix[i][j])
-                pref[i+1][j+1]  = pref_sum
-        #fixing the left and right edge for the reactangle 
-        for c1 in range(n):
-            for c2 in range(c1,n):
-                area = 0
-                #iterate on each row assuming it as horizontal edge
-                for  r in range(m): 
-                    #checking if the horizontal cells are all 1s
-                    #in which case this part added to area
-                    if (pref[r+1][c2+1] - pref[r+1][c1]) == (c2-c1+1):
-                        area += (c2-c1+1)
-                    #else reinitialize the area 
-                    else:
-                        area =0
-                    ans  =max(ans, area)
+                ind = j
+                print(stk)
+                while(stk and stk[-1][1]>= heights[j]):
+                    #3,5,10, 1 , we should nto misse the rectangle formed by 3 towards right , when inserting 1 
+                    ind, height   = stk.pop()
+                    area  = height *(j-ind)
+                    max_area = max(area, max_area)
+                width = (j-ind+1)
+                area = width*heights[j]
+                print("ar",area,j,ind)
+                max_area =max(area, max_area)
+
+                stk.append([ind, heights[j]])
+            print('lo',max_area )
+            
+            for i in range(1,len(stk)):
+                print("m",max_area)
+                max_area  =max(max_area, (n - stk[i][0])*stk[i][1] )
+                
+            return max_area
+        
+        for i in range(m):
+            #creating the heights array
+            for j in range(n):
+
+                if matrix[i][j] =="1":
+                    heights[j] +=1
+                else:
+                    heights[j] = 0
+          
+            ans = max(ans , helper(heights))
+        
+            
         return ans
+
+
 
 
 
