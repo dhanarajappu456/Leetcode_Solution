59c9babbebee6ab6452aae06ca93d8e019899d89
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-04-14 08:15:04 +0530

    Time: 38 ms (45.81%) | Memory: 16.8 MB (8.50%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.py
+++ 404-sum-of-left-leaves/sum-of-left-leaves.py
@@ -7,13 +7,15 @@
 class Solution:
     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
 
-
+    
         def rec(root,is_left):
             if root == None:
                 return  0
+            #consider the root value iff it is a leaf node and is a left child
             elif root.left  == None and root.right == None and is_left:
                 return root.val
             
+            #in other case  just return value sum of l and r 
             l =   rec(root.left,1)
             r =   rec(root.right,0)
 
