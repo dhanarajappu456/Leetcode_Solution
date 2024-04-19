d82b2a7be82c5668e88eee54e6803d735792002c
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-04-19 16:05:34 +0530

    Time: 29 ms (87.26%) | Memory: 16.6 MB (46.70%) - LeetSync

--- 1194-path-in-zigzag-labelled-binary-tree/path-in-zigzag-labelled-binary-tree.py
+++ 1194-path-in-zigzag-labelled-binary-tree/path-in-zigzag-labelled-binary-tree.py
@@ -1,14 +1,27 @@
+'''
+the idea is, we need to find actual parent to which the current label migh have
+connected to , then find the posiiton of this parent in the level it is present , denoted by position_from_end 
+this value need to be added to value of  starting node of the level. this is repeated
+t logn
+s aux(logn)
+'''
 class Solution:
     def pathInZigZagTree(self, label: int) -> List[int]:
     
         ans =[]
         ans.append(label)
         while label>1:
+            #actual parent
             actual_par = label//2
+            #finding prev level (first level =0 )
             prev_level = int(math.log(actual_par,2))
+            #finding max value in the prev level
             max_in_prev_level = 2**(prev_level+1)-1
-            diff = max_in_prev_level - actual_par
-            res  = ( 2**prev_level)+diff
+            #finding the position of actual parent in the level(from right side)
+            position_from_end = max_in_prev_level - actual_par
+            #now this initial positio value to be added to the starting value of the level
+            #to get actual position
+            res  = ( 2**prev_level)+position_from_end
             ans.append(res)
             label =res
         return ans[::-1]
