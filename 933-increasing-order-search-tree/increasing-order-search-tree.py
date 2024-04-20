f6ef9743e187979405548940776cb386affdf790
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-04-20 20:07:30 +0530

    Time: 39 ms (33.74%) | Memory: 16.5 MB (47.07%) - LeetSync

--- 933-increasing-order-search-tree/increasing-order-search-tree.py
+++ 933-increasing-order-search-tree/increasing-order-search-tree.py
@@ -1,29 +1,26 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+#dfs 
+
+#t n
+#s n 
 class Solution:
     def increasingBST(self, root: TreeNode) -> TreeNode:
-        new_root = None
-        temp = root
-        while(temp):
-            new_root  = temp
-            temp = temp.left
-
-        
+       
         def reform(root):
             if root == None:
                 return None,None
-
-            
+            #left most node of right subtree , 
+            #and right most node of right subtree
             r_left,r_right = reform(root.right)
+            #left most node of left subtree and
+            #right most node of left subtree
             l_left,l_right = reform(root.left)
-      
+            #if connect the right most node in the left subtree
+            #to the root 
             if l_right:
                 l_right.right = root
                 root.left = None
+            #make root.right point to 
+            #left most  node of right subtree
             root.right = r_left
             return (l_left or root ),(r_right or root)
         l_left,r_right = reform(root)
