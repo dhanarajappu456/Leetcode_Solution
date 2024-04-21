2d00bab2bb195751b91fffc7f71316014610b759
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-04-22 01:01:35 +0530

    Time: 23 ms (98.69%) | Memory: 16.5 MB (38.42%) - LeetSync

--- 886-score-of-parentheses/score-of-parentheses.py
+++ 886-score-of-parentheses/score-of-parentheses.py
@@ -10,15 +10,14 @@ class Solution:
                 current_open_matched = False
                 item  = stk[-1]
 
-                # if top is (, just we would might need to push 1 to stack not immediately , keep it stored
-                #in num , beacuse next top will be a number in which it need to be added to num and push this num
+                # if top is( pop it ansd push 1
 
                 if  item=="(":
                     current_open_matched = True
                     num = 1
                     stk.pop()
-                #if top was a number double it 
-                
+                #if top was a number add all previosu number until ( 
+                #and double this sum and insert to stack
                 else:
         
                     while stk and stk[-1]!="(":
@@ -30,7 +29,7 @@ class Solution:
             
               
                 stk.append(num)
-      
+        #at end stack  will have only numbers that need to be added
         return sum(stk)
             
      
