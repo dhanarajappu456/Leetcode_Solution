46accd286faf13cf1aed1ba255677473cfaee8ce
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-04-22 00:59:55 +0530

    Time: 28 ms (89.73%) | Memory: 16.6 MB (38.42%) - LeetSync

--- 886-score-of-parentheses/score-of-parentheses.py
+++ 886-score-of-parentheses/score-of-parentheses.py
@@ -3,23 +3,35 @@ class Solution:
         stk = []
         
         for c in s:
-            
+            num = 0
             if c == "(":
                 stk.append(c)
             elif c==")":
-                item = stk.pop()
+                current_open_matched = False
+                item  = stk[-1]
+
+                # if top is (, just we would might need to push 1 to stack not immediately , keep it stored
+                #in num , beacuse next top will be a number in which it need to be added to num and push this num
+
                 if  item=="(":
+                    current_open_matched = True
                     num = 1
+                    stk.pop()
+                #if top was a number double it 
+                
                 else:
-                    num = item*2
-                if stk and stk[-1]!="(":
-                    item = stk.pop()
-                    num+=item
-                else:
-                    if stk:
-                        stk.pop()
+        
+                    while stk and stk[-1]!="(":
+                        item = stk.pop()
+                        num+=item
+                    num*=2
+
+                    stk.pop()
+            
+              
                 stk.append(num)
-        return stk[0]
+      
+        return sum(stk)
             
      
 
