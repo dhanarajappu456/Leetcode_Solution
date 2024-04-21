13128cc7d4663a35bce080a5d19f25e2f11a37b2
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-04-21 21:20:06 +0530

    Time: 38 ms (50.74%) | Memory: 16.5 MB (60.33%) - LeetSync

--- 1159-smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.py
+++ 1159-smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.py
@@ -1,12 +1,19 @@
+
+'''
+
+'''
+
 class Solution:
     def smallestSubsequence(self, s: str) -> str:
-
-
         stk  = []
         last_index = {c:i for i,c in enumerate(s)}
         for i, c in enumerate(s):
+            #skip curr char if already in the stk
+            #example s= bcabd, 
+            #when at second b we need to consider it so continue
             if c in stk:
                 continue
+            #if top char come after the curr char , then pop it 
             while(stk  and stk[-1] and stk[-1]>c and last_index[stk[-1]]>i):
                 stk.pop(-1)
             
