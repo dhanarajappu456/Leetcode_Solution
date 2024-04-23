17a1ab68c8c47e8470b53dde83a029ddfba9b8a1
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-04-23 23:51:30 +0530

    Time: 420 ms (46.42%) | Memory: 27.1 MB (77.90%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.py
+++ 310-minimum-height-trees/minimum-height-trees.py
@@ -1,3 +1,17 @@
+'''
+The observation is leaf node(indegree 1 ) cant be  the answers and 
+ also there are only at max  on or two nodes that gives the MHT 
+
+ what we do is delete each leaf node at current istance , until we have <=2 remainig nodes
+
+we use q here to process leaf nodes at each instatant at each instance
+infact doing like khans algo
+
+ t n
+ s n
+
+'''
+
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
 
@@ -5,6 +19,7 @@ class Solution:
         if len(edges)== 0:
             return [0]
         neib  = defaultdict(list)
+        #to identtify leaf node(indegree =1 )we use the indegree list
         indegree = [0 for i in range(n)]
         for a,b in edges:
             neib[a].append(b)
@@ -14,19 +29,17 @@ class Solution:
 
        
         q = deque([ i for i in range(n) if indegree[i] == 1 ])
-        
-        visited = set()
+        #perform removal of leaf node until the number of nodes>2
         while(n>2):
             
             size = len(q)
+            #we delete each leaf node at present from total nodes 
             n-=size
-            indeg_nodes =set()
             for i in range(size):
                 node = q.popleft()
                 for nb in neib[node]:
                     indegree[nb]-=1
                     if indegree[nb]==1 :
-                      
                         q.append(nb)
         
         return q
