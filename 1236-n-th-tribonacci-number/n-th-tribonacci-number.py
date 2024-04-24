613d728a58805ab93e55ebc01918bab0a9573bb9
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-04-24 17:38:07 +0530

    Time: 34 ms (59.23%) | Memory: 16.7 MB (31.32%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+
+        @lru_cache(None)
+        def rec(n):
+
+            if n==0 :
+                return 0 
+            if n==1 or n==2 :
+                return 1
+            
+            return rec(n-1) +  rec(n-2) + rec(n-3)
+        return rec(n)
