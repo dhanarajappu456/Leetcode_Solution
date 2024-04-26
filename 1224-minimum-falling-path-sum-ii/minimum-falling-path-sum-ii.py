5546dffaea733cc4989c0bcbe5b9463c5c01cd23
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-04-26 18:31:49 +0530

    Time: 5077 ms (19.47%) | Memory: 20.1 MB (34.96%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.py
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.py
@@ -1,18 +1,33 @@
+'''
+
+solution 2 - dp solution
+
+
+the dp solution fo solution 1 
+
+t n^3
+s n
+
+'''
+
 class Solution:
     def minFallingPathSum(self, grid: List[List[int]]) -> int:
         n = len(grid)
-        @lru_cache(None)
-        def rec(i,j):
-            m = float("inf")
+        dp = [[0 for i in range(n+1)] for i in range(n+1)]
+  
+        #state indicate the min sum when choosing element from ith row to 
+        #end row , with j-1 being the column chosen from the previous row
+        for i in range(n-1,-1,-1):
+            #     j-1 being the previous index chosen
+            for j in range(n+1):
+                m  = float("inf")
+                for c in range(n):
+                    if c!=j-1:
+                        m = min(m  ,grid[i][c]+ dp[i+1][c+1])
+                
+                dp[i][j]= m 
 
-            if i == n:
-                return 0
-            for c in range(n):
-                if c!=j:
-                    m = min(m  ,grid[i][c]+ rec(i+1,c))
-            
-            return m
-        return rec(0,-1)
+        return min(dp[0])
 
 
         
