f0032fb1392f16fc56ececc40c7533bb78024de3
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-04-30 22:57:52 +0530

    Time: 1367 ms (59.62%) | Memory: 17.4 MB (79.81%) - LeetSync

--- 2044-number-of-wonderful-substrings/number-of-wonderful-substrings.py
+++ 2044-number-of-wonderful-substrings/number-of-wonderful-substrings.py
@@ -1,3 +1,28 @@
+'''
+solution1 
+
+approach  subarray sum  = k
+we know the xor of a num even number times gives = 0 
+and odd number of times gives the same number
+we use this approach 
+
+each char a-j is mapped to vals  like 1,2,4,8....
+we find the subaray ending at current index, 
+a)which either has  all terms even number of times
+checked by :
+when at current index, we check if we have same culative xor found so far
+b)or either one of char from a to j is appears  odd number times
+checked by 
+wehen at current index , for each char a to j , if each of them appears exactly odd number of times
+this is done by xoring this char with cumulative xor , let  call this as "prefix_to_remove", which is some 
+thing that need to be cur from beginning
+
+
+
+
+t 10*n = n
+s  n(map)
+'''
 class Solution:
     def wonderfulSubstrings(self, word: str) -> int:
 
