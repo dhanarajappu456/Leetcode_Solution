b34bb352c657c9939bd5fb5a49a0b0eec6820a84
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-05-05 13:04:58 +0530

    Time: 31 ms (91.88%) | Memory: 16.7 MB (70.90%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.py
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.py
@@ -1,9 +1,6 @@
 
 class Solution:
     def deleteNode(self, node):
-        if node.next == None:
-            node = None
-        else:
-            node.val = node.next.val
-            node.next = node.next.next
+        node.val = node.next.val
+        node.next = node.next.next
         
