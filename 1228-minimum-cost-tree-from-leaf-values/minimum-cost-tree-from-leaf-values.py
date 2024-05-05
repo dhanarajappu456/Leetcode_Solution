e12610ebf79fdc0a97102fb54efee3d2a6a97f49
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-05-05 13:33:52 +0530

    Time: 195 ms (19.39%) | Memory: 17 MB (24.85%) - LeetSync

--- 1228-minimum-cost-tree-from-leaf-values/minimum-cost-tree-from-leaf-values.py
+++ 1228-minimum-cost-tree-from-leaf-values/minimum-cost-tree-from-leaf-values.py
@@ -1,9 +1,19 @@
+'''
+solution1 
+memo 
+
+the idea is rec(l,h) -> stores the minium ans for array ranging from l,h
+
+t n^3 
+s  n(aux) + n^2(memo ds )
+
+'''
+
 class Solution:
     def mctFromLeafValues(self, arr: List[int]) -> int:
 
         @lru_cache(None)
         def rec(l,h):
-
             if l>=h:
                 return 0
             ans = float("inf")
