21042337180d55543b3033768e64164240eb62ca
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-05-06 15:53:58 +0530

    Time: 458 ms (35.79%) | Memory: 52 MB (51.47%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.py
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.py
@@ -5,17 +5,18 @@
 #         self.next = next
 class Solution:
     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-       
-        def rec(root):
-            
-            if root  == None:
-
-                return  -1, None
-            max_,nxt = rec(root.next)
-            if root.val>=max_:
-                root.next = nxt
-                return root.val,root
-            else:
-                return max_,nxt
-
-        return rec(head)[1]
+        stk =[]
+        temp = head
+        while(temp!=None):
+            num = temp.val
+            while(stk and stk[-1].val<num):
+                stk.pop(-1)
+            stk.append(temp)
+            temp = temp.next
+        next_ = None
+        while(stk):
+            stk[-1].next = next_
+            next_= stk[-1]
+            stk.pop(-1)
+        return next_
+        
