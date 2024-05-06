c5b9d1afaf2f9887c9eb83dc6c5d28874d1def93
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-05-06 15:54:39 +0530

    Time: 447 ms (40.73%) | Memory: 51.1 MB (78.68%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.py
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.py
@@ -1,8 +1,8 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+'''solution2 - 
+monotonic stk  of  decreasing value
+t n 
+s n 
+'''
 class Solution:
     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
         stk =[]
