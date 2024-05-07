27d41ef0108c881478789069bd5491b962b5d8cb
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-05-07 11:00:40 +0530

    Time: 220 ms (84.98%) | Memory: 20.6 MB (17.06%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.py
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.py
@@ -1,8 +1,6 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+#solution 1 - recursion
+#t n 
+#s n(stk space)
 class Solution:
     def doubleIt(self, head: Optional[ListNode]) -> Optional[ListNode]:
         def rec(root):
