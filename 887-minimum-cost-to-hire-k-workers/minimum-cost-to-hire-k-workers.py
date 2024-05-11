f8bb3e26189ac20f05ab0a85d5fcee6165b6624d
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-05-11 22:14:44 +0530

    Time: 151 ms (74.48%) | Memory: 19 MB (22.85%) - LeetSync

--- 887-minimum-cost-to-hire-k-workers/minimum-cost-to-hire-k-workers.py
+++ 887-minimum-cost-to-hire-k-workers/minimum-cost-to-hire-k-workers.py
@@ -4,22 +4,22 @@ class Solution:
         n = len(quality)
         ratio = [0 for i in range(n)]
         min_heap_quality = []
+        #stores the sum k-1 least wage
         min_sum_k=0
-        # h.heapify(min_heap_quality)
         for i in range(n):
             q = quality[i]
-            ratio[i] =( wage[i]  / quality[i],quality[i],wage[i])
+            ratio[i] =( wage[i]  / quality[i],quality[i])
         ratio.sort()
         ans = float("inf")
         for i in range(n):
+            curr_ratio = ratio[i][0]
             curr_qual = ratio[i][1]
-            curr_wage = ratio[i][2]
             if len(min_heap_quality)>k-1:
                 max_qual  = h.heappop(min_heap_quality)
                 max_qual*=-1
                 min_sum_k-=max_qual
             if len(min_heap_quality)==k-1:
-                ans  = min(ans, ratio[i][0]*min_sum_k + curr_wage)
+                ans  = min(ans, ratio[i][0]*min_sum_k + curr_ratio*curr_qual)
             h.heappush(min_heap_quality,-curr_qual)
             min_sum_k += curr_qual
         return ans 
