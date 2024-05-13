8f798eb311bf876657c3c9b085383152360e5a0f
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-05-13 15:08:47 +0530

    Time: 39 ms (72.41%) | Memory: 16.6 MB (59.05%) - LeetSync

--- 891-score-after-flipping-matrix/score-after-flipping-matrix.py
+++ 891-score-after-flipping-matrix/score-after-flipping-matrix.py
@@ -3,6 +3,7 @@ class Solution:
         col_size=len(grid[0])
         row_size=len(grid)
         mask=(1<<col_size)-1
+        #flipping the row if possible
         for i in range(row_size):
             val=0
             for j in grid[i]:
@@ -11,24 +12,22 @@ class Solution:
             if(val^mask>val):
                 for j in range(col_size):
                     grid[i][j] = grid[i][j] ^ 1
-        #print(grid)            
+        #flipping the col if possible 
         for j in range(col_size):
-            count_zero=0
             count_one=0
-            for k in range(row_size):
-                if(grid[k][j]==0):
-                    count_zero+=1
-                else:
-                    count_one+=1
-            if(count_zero>count_one):
+            for i in range(row_size):
+                count_one += (grid[i][j] & 1 )
+            count_zero  = row_size - count_one
+            if( count_zero >count_one):
                 for i in range(row_size):
                     grid[i][j] = grid[i][j] ^ 1
+        #finding the sum
         sum_=0
         for i in grid:
-            cnt=1
+            ans = 0
             for j in i:
-                sum_+=(2**(col_size-cnt))*j
-                cnt+=1   
+                ans = (ans << 1)  + j
+            sum_ += ans
         return sum_             
                     
                     
