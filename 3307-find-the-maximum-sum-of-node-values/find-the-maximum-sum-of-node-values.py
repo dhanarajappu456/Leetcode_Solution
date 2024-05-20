7e798a10318fbdc6931b3401619779964653e2da
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-05-20 13:12:42 +0530

    Time: 938 ms (92.86%) | Memory: 28.1 MB (69.05%) - LeetSync

--- 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.py
+++ 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.py
@@ -1,44 +1,31 @@
 '''
-#solution 1 -  greedy
+#solution 2 - solution is same as the solution 1 
+here we initally store all the sum of nums in ans variable then we have a aray indiacating the profit or loss 
+if we xored each value in nums 
 
-the idea is any pair of number in the tree can be maximised if possible to maximise both , without
-affecting other numbers in the path b/w these number.
+then we sort this array in descending order and take only profitable pair(that is sum of thier profit>0) into consideration
+t nlogn
+s n 
 
-initially we find ideal(max sum) that can be obtained from all the numbers in the tres
-
-note: if number of numbers in the tree that can be maximised is odd ,  then one of the number  which is maximised 
-using xor with k , is need to be undone, since , there is no other number forming pair with this  .
- thus we subtract(undo xoring a number num with k) min_xor_diff from the ideal sum
-  
-  note:
-  min_xor_diff = the minimum diff b/w a number  and its xored value , among all of the xored values
 '''
-
 
 class Solution:
     def maximumValueSum(self, nums: List[int], k: int, edges: List[List[int]]) -> int:
-        #no of number which yield higher value on xoring with k 
-        count= 0
-        #minimum xored value
-        min_xor_diff = float("inf")
+        n = len(nums)
+        ans = 0
+        res =[0 for i in range(n)]
+        for i,num in enumerate(nums):
+            ans+= num
+            res[i] = (num^k) -num
+        res.sort(reverse =True)
+ 
+        for i in range(0,n-1,2):
+            profit_xor = res[i]+res[i+1]
+            if profit_xor>0:
+                ans+= profit_xor
+        return ans 
 
-        #stores ideal(max) sum that is obtained after performing xor with k (for each num )
-        ans = 0 
-        #finding ideal sum and count  number that can be maximised by  xoring with k
-        for num in nums:
-            #if xoring yields higher number , then xor it to get max(ideal) sum
-            if num^k> num:
-                count+=1 
 
-                ans += (num^k)
-            #if it doesn't yield a higher num, then consider the num as such
-            else:
-                ans += num
-            #stores the min diff b.w its value and xored value withk 
-            min_xor_diff = min(min_xor_diff,abs(num - ( num^k)))
-        #if no;of numbers that can yield hiher nums on xor with k , is odd
-        #then undo one such operation , by removing the mindiff from the ideal sum
-        if count%2==1:
-            return ans  - min_xor_diff
-        #if even then ideal sum  == final result
-        return ans
+
+
+        
