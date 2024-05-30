dd232fe4d24d983bc270f9b4a1a45901653ec919
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-05-30 19:14:53 +0530

    Time: 32 ms (82.32%) | Memory: 16.5 MB (93.91%) - LeetSync

--- 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
@@ -1,15 +1,32 @@
 class Solution:
     def numSteps(self, s: str) -> int:
+        n = len(s)
+        l = n-1
+        count =0 
+        carry = 0 
+        while(l>=0 ):
+            #reached value 1, so stop 
+            if( l == 0 )and (carry ==0):
+                break
+            if ((int(s[l]) +  carry ) == 0) and  (carry ==1):
+                count +=1
+                carry =1 
+            elif ((int(s[l]) +  carry ) == 0) and  (carry ==0):
+                carry = 0
+                count+=1
+            elif ((int(s[l]) +  carry ) == 1):
+                count+=2
+                carry =1 
+            elif (int(s[l]) +  carry ) == 2:
+                count+=1
+                carry  = 1
+            
+            
 
-        num = int(s,2)
-        ans  = 0
-        while(num!=1):
+      
+           
 
-            if num%2 ==1:
-                num+=1
-            else:
-                num//=2 
-     
-            ans+=1
-        return ans
+            l-=1
+   
+        return count
         
