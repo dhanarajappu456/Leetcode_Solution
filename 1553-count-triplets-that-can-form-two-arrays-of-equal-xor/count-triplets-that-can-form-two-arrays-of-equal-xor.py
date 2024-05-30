49475d2868aa5eb3349fb6f3650bb00bb573a628
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-05-30 13:09:02 +0530

    Time: 46 ms (69.54%) | Memory: 16.7 MB (14.37%) - LeetSync

--- 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/count-triplets-that-can-form-two-arrays-of-equal-xor.py
+++ 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/count-triplets-that-can-form-two-arrays-of-equal-xor.py
@@ -1,3 +1,24 @@
+'''
+
+solution -1 
+bruteforce for i,j,k
+
+t n^3
+s 1
+
+solution 2 - optimisation to n^2
+the questioncan be interpreted in another way 
+that is it asks to find subarry whose len>=2 and
+xor of all elements in it  =0 
+so we find possible subaray with i at left and k at right
+when we find such subarry with xor =0 with left end at i and right end at k , then 
+we could have k-i possiblities for j , which is added to result 
+
+t n^2
+s 1
+
+'''
+
 class Solution:
     def countTriplets(self, arr: List[int]) -> int:
 
@@ -5,10 +26,10 @@ class Solution:
         n =len(arr)
         for i in range(n-1):
             xor = arr[i]
-            for j in range(i+1,n):
-                xor^=arr[j]
+            for k in range(i+1,n):
+                xor^=arr[k]
                 if xor ==0 :
                   
-                    res+=  (j-i)
+                    res+=  (k-i)
         return res
         
