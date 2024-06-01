12a73e95c0655f2f3c30fd37c0b2ab367a4deddc
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-06-01 13:00:20 +0530

    Time: 58 ms (58.08%) | Memory: 18.5 MB (76.21%) - LeetSync

--- 260-single-number-iii/single-number-iii.py
+++ 260-single-number-iii/single-number-iii.py
@@ -14,26 +14,47 @@ t n
 s n 
 
 
+
+solution 3 using xor
+The idea is to seperate the number that occur once into a different grp and 
+finding the xor of individaul grp , ultimately will give the numbers
+The  way we can do is finding xor of all nums , which is essential the xor of the
+numbers that occur once.Then we can seperate them based on the first bit from right at
+which they differ.
+So that this ensures they would be in a different grp
+
+t n 
+
+s  1
+
+
 '''
 
 
 
-#solution 2 
+#solution 3
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
-
-
-        cnt  = defaultdict(int)
-        ans1, ans2   = -1,-1
+        #finding xor of all nums  = xor of nums occuring once(num1 and num2 )
+        xor  = 0
         for num in nums:
-            cnt[num]+=1
-        
-        for num in cnt:
-            if cnt[num] ==1 :
-                if ans1 ==-1:
-                    ans1=num
-                else:
-                    ans2  = num
-        return [ans1, ans2]
-            
+            xor^= num
+        bit  = 0 
+        val = 1 
+        #finding the first bit from right, at which the num1 and num2 have  different
+        #bits
+        while((val&xor) == 0):
+            bit+=1
+            val<<=1
+        #finding xor all nums belonging to grp1 and 
+        #grp2 , which is nothing but the num1 and num2 
+        grp1_xor = 0
+        grp2_xor = 0
+        for num in nums:
+            if num&val!=0:
+                grp1_xor^=num
+            else:
+                grp2_xor^=num
+        return [grp1_xor , grp2_xor]
+
         
