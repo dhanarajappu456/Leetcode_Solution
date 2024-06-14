475b8c98f1dda333cdec2f5c15bcc8345904489d
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-06-14 12:28:50 +0530

    Time: 648 ms (51.13%) | Memory: 30.7 MB (47.97%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.py
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.py
@@ -4,14 +4,14 @@ class Solution:
         ans =0
         nums.sort()
         mx = nums[0]
-        print(nums)
+
         for i in range(1,n):
             if nums[i]<=nums[i-1]:
-                print(nums[i],mx+1, abs(nums[i]-mx+1))
+   
                 ans+=abs(nums[i]-(mx+1))
                 nums[i] = mx+1
             mx = nums[i]
-        print(nums)
+  
         return ans
 
         
