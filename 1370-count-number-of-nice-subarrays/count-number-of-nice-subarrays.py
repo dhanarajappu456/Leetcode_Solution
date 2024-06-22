f370bc8853fd5cb71853ae05ea6eff4203d3f2a1
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-06-22 15:03:53 +0530

    Time: 584 ms (79.49%) | Memory: 23.6 MB (42.15%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
@@ -1,34 +1,53 @@
+'''
+solution 2
+sliding window 
+the sliding window cannot be applied as such 
 
+eg: 
+conside 2,2,1,1,1,2,2  and k = 3 
+in this example the when at ind = 5,6 the we can still form the subarray with 3 elements 
+with i at 0,1,2. So it is important that  when we shrink the i , of the window , 
+we need to store this information of how many elements at beginning is removed,
+so that the window still has k elements .
+Since these removed elements can form subarray with even elements coming afterwards 
+as discussed above. 
+t n
+s 1
 
 '''
-Solution 1 - 
-like subarray with sum == k 
 
-here the sum is number of odd numbers in a subarray
-pref pref sum which is the sum of count of odd numbers so far 
-
-the array can be imagined as :
-[1,1,2,1,1]----> [1,1,0,1,1]
-t n 
-s n 
-
-'''
 
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        d = defaultdict(int)
-        pref,ans   =0 ,0 
-        d[0]=1 
-        n   = len(nums)
-        for i,num in enumerate(nums):
-            if num%2 ==1 :
-                pref+=1 
-            if pref-k in d: 
-                ans  += d[ pref-k]
-            
-            d[pref] +=1 
-        return ans 
+ 
+        i,j = 0,0
+        n = len (nums)
+        #keeps number of odd numbers in current window /subarray
+        odds = 0
+        ans =0 
+        #this keeps the number of elements that can be removed from 
+        #beginning of current subarray so that still number of odds 
+        #in this subarray that end at j 
 
+        prevCount =  0
+        while(j<n):
+            #when current number if odd, we update the odds , prevCount
+            if nums[j]%2 ==1:
+                prevCount =  0
+                odds+=1
+            #find prevCount 
+            #ie,find the number of elements that can be removed from front so that 
+            #the subarray still have k number of odds in it
+            while(odds == k):
+                if nums[i]%2 ==1:
+                    odds-=1
+                prevCount+=1
+                i+=1
+            #add the prevCount(which is the  number of suabrray with k elemetns in it and end 
+            #at j )
+            ans+= prevCount
+            j+=1
+        return ans 
                 
             
             
