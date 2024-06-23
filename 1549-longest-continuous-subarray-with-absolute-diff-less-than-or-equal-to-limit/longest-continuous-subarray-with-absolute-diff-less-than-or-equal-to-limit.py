92013564e81bd2fa304b7b56d397db6b9ffc7c5a
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-06-23 17:10:07 +0530

    Time: 596 ms (24.75%) | Memory: 36.1 MB (10.55%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -5,6 +5,12 @@ class Solution:
         #identifies  the min and max of the current subarray
         min_heap,max_heap=[],[]
         n = len(nums)
+        #note , i is at -1 initally as i is index value upto which element is need to be removed
+        ##including the ith value 
+        #so length is j-i , we need i to be -1, to deal with edge case , where entire array can be a 
+        #valid ans 
+        #eg: [2,5,2] ,limit =9 
+        #
         i,j =-1, 0 
         ans = 0
 
@@ -13,6 +19,9 @@ class Solution:
             h.heappush(max_heap, (-nums[j],j))
     
             #the diff b/w min and max in the subarray need to be<limit
+            #else pop the max or min element (whichever comes first in the array) 
+            #that cause the diff to be>limit
+        
             while(min_heap and max_heap and ((-max_heap[0][0] - min_heap[0][0])>limit)):
                 
                 if min_heap[0][1] <= max_heap[0][1] :
@@ -30,6 +39,8 @@ class Solution:
             ans  = max(j-i, ans )
             j+=1 
         return ans 
+
+
 
 
 
