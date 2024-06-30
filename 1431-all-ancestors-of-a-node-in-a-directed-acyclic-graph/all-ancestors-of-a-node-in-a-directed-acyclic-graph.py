202f3b0abdf3a953556ac0ac01c7f99076df189d
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-06-30 23:56:35 +0530

    Time: 463 ms (69.44%) | Memory: 48.3 MB (34.19%) - LeetSync

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
@@ -3,25 +3,28 @@ class Solution:
 
 
         adj =  defaultdict(list)
-
         ans =   defaultdict(set)
         indeg= [0 for i in range(n)]
 
-
+        # t: e
         for a,b in edges:
             adj[a].append(b)
             indeg[b]+=1
         q=deque([ i for i in range(n) if indeg[i] ==0 ])
-        while( q):
+        while( q): # t: n+e
 
             node= q.popleft()
 
-            for nb in adj[node]:
+            for nb in adj[node]:  # t :n*e , combining all the n iteration,the n from inner set union
+            
+
                 ans[nb] = ans[nb].union(ans[node])
                 ans[nb].add(node)
                 indeg[nb]-=1
                 if indeg[nb] == 0:
                     q.append(nb)
+        # n^2logn worst upper bound , not that much really 
+        # as all  node cant have n number of  ancestor nodes
         res =[ 0 for i in range(n)]
         for i in range(n):
 
