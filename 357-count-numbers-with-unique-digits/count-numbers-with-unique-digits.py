8225754416e2e3bb7c479843ec851092490aaee9
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-07-02 00:14:48 +0530

    Time: 33 ms (69.89%) | Memory: 16.5 MB (53.41%) - LeetSync

--- 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.py
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.py
@@ -7,10 +7,26 @@ class Solution:
         
   
         res = 0
+        '''
+        i denote the number of digit in the number 
+        '''
         for i in range(1,n+1):
             if i ==1:
                 res+= 10
+            
+           
             else:
+
+                '''
+                for nums with number of digit >1 ,
+                that is like 2 digit ,  digit number 
+                the first msb position cant have  0
+                rest position can have 0, 
+                so we calcualte the computatation starting from the 2nd position from msb
+                and at end of loop we multiply the ans by  9 , 
+                that comes from the count of possible  number that can be placed at the 
+                first msb position  
+                '''
               
                 temp = i-1
                 val = 9
