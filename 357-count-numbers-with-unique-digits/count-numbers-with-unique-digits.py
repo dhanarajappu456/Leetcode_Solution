b64d917aaf9f73e4f0c4ee5641f2e3a19cf5610e
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-07-02 13:04:47 +0530

    Time: 39 ms (31.05%) | Memory: 16.6 MB (13.69%) - LeetSync

--- 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.py
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.py
@@ -1,41 +1,19 @@
 class Solution:
     def countNumbersWithUniqueDigits(self, n: int) -> int:
-        if n == 0:
-            return 1
-
         
         
-  
-        res = 0
-        '''
-        i denote the number of digit in the number 
-        '''
-        for i in range(1,n+1):
-            if i ==1:
-                res+= 10
-            
-           
-            else:
-
-                '''
-                for nums with number of digit >1 ,
-                that is like 2 digit ,  digit number 
-                the first msb position cant have  0
-                rest position can have 0, 
-                so we calcualte the computatation starting from the 2nd position from msb
-                and at end of loop we multiply the ans by  9 , 
-                that comes from the count of possible  number that can be placed at the 
-                first msb position  
-                '''
-              
-                temp = i-1
-                val = 9
-                ans = 1 
-                while(temp):
-                 
-                    ans *=  val
-                    val-=1
-                    temp-=1 
-                res +=  ans*9
-
-        return res 
+        dp =[1,10]
+        i=0
+        for i in range(2,n+1):
+            ans = dp[i-1]
+            temp = i-1
+            val= 9 
+            cnt =1 
+            while(temp):
+                cnt*= val
+                val-=1
+                temp-=1
+            cnt*=9
+            ans+=cnt
+            dp.append(ans )
+        return dp[n]
