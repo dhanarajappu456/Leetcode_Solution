f4083b5637d6ccbcc9462e3f80c3b7fdca4f7e44
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-07-05 13:21:44 +0530

    Time: 414 ms (11.03%) | Memory: 44.6 MB (14.45%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
@@ -5,28 +5,24 @@
 #         self.next = next
 class Solution:
     def nodesBetweenCriticalPoints(self, head: Optional[ListNode]) -> List[int]:
-    
         ptr = head
         prev = None
         mn_l, mn_r, mx_l,mx_r = None ,None ,None ,None 
         min_,max_ = float("inf"),-float("inf")
         i=0
         while(ptr):
-            if prev and ptr.next and prev.val<ptr.val and ptr.val>ptr.next.val:
+            if ( 
+            prev and ptr.next and prev.val<ptr.val and ptr.val>ptr.next.val
+            ) or ( 
+            prev and ptr.next and prev.val>ptr.val and ptr.val<ptr.next.val
+            ):
                 if mx_l == None:
                     mx_l = i
                 else:
                     mx_r  = i
                 mn_l, mn_r =mn_r, i
-            if prev and ptr.next and prev.val>ptr.val and ptr.val<ptr.next.val:
-                if mx_l == None:
-                    mx_l = i
-                else:
-                    mx_r = i 
-                mn_l, mn_r =mn_r, i
             if mn_l!= None and mn_r!=None :
                 min_  = min(min_, mn_r- mn_l)
-            
             if mx_l!= None and mx_r!=None :
                 max_  = max(max_, mx_r- mx_l)
             prev = ptr 
