f7f5657f2ac3f655a73dc5ae05cc678899012ede
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-07-07 14:56:03 +0530

    Time: 37 ms (38.61%) | Memory: 16.5 MB (79.17%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+
+        ans  = numBottles
+        while( numBottles>= numExchange):
+            newFree=numBottles//numExchange
+            ans+= newFree
+            numBottles = numBottles%numExchange + newFree
+        return ans
