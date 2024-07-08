f6566b4da4ddc9bdc6ae0af212e642fb2f20f0fe
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-07-08 17:41:39 +0530

    Time: 36 ms (73.87%) | Memory: 16.6 MB (49.47%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -4,24 +4,9 @@ class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
 
 
-
+        ind = 0
+        for i in range(2,n+1):
+            ind =  ind = (ind+k) % i
+    
+        return ind+1
         
-        def rec(n,k):
-            '''
-            when single element , which is winner , index of it is 0
-            '''
-            if n==1:
-                return 0
-            
-            ind = rec(n-1,k)
-            '''
-            index of winner among n element is, k more  than index of it 
-            when among n-1 elements
-            '''
-            ind = (ind+k) % n
-
-            return ind
-        '''
-        at end the value of ans is 1 more than index at which it stays
-        '''
-        return 1+rec(n,k)
