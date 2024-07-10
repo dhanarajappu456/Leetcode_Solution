6673be1a2cb5c6e2f2ff5a29ae147a52bcc8a6fa
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-07-10 12:52:31 +0530

    Time: 48 ms (63.33%) | Memory: 16.6 MB (82.14%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+
+        stk = []
+        for op in logs:
+            if op =="../":
+                if stk:
+                    stk.pop()
+            elif op!="./":
+                stk.append(op)
+ 
+        return len(stk)
+
+        
