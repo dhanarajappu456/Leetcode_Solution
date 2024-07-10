e295f8110a91486b2f1a3112b77d054bf3395bbb
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-07-10 13:01:44 +0530

    Time: 932 ms (12.56%) | Memory: 17.8 MB (72.95%) - LeetSync

--- 2657-minimum-time-to-complete-all-tasks/minimum-time-to-complete-all-tasks.py
+++ 2657-minimum-time-to-complete-all-tasks/minimum-time-to-complete-all-tasks.py
@@ -2,29 +2,23 @@ class Solution:
     def findMinimumTime(self, tasks: List[List[int]]) -> int:
 
         tasks.sort(key = lambda x:x[1]) 
-        global_set =set() 
+        taken =set() 
         for inter in tasks:
-
             s,e,d = inter
-
-            taken_curr = set() 
             rem = d 
             for j in range(s,e+1):
-                if j in  global_set:
-                    taken_curr.add(j)
-
+                if j in  taken:
+                    taken.add(j)
                     rem-=1 
-
                 if rem==0:
                     break
             j = e 
             while(j>=s and rem):
-                if j not in taken_curr:
-                    taken_curr.add(j)
-                    global_set.add(j)
+                if j not in taken:
+                    taken.add(j)
                     rem-=1
                 j-=1 
            
-        return len(global_set)
+        return len( taken)
 
         
