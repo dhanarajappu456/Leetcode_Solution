9af49476f069cad7de86acaa4f72cc743d9922f0
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-07-16 20:57:26 +0530

    Time: 307 ms (56.04%) | Memory: 54.7 MB (26.84%) - LeetSync

--- 2217-step-by-step-directions-from-a-binary-tree-node-to-another/step-by-step-directions-from-a-binary-tree-node-to-another.py
+++ 2217-step-by-step-directions-from-a-binary-tree-node-to-another/step-by-step-directions-from-a-binary-tree-node-to-another.py
@@ -7,44 +7,66 @@
 class Solution:
     def getDirections(self, root: Optional[TreeNode], startValue: int, destValue: int) -> str:
 
-        parent = {}
-        start, end   = None ,None
-        def dfs(root,par):
-            if root == None:
-                return 
-            nonlocal start,end
+        start , end  = None, None
+        def lca_find(root):
+
+
+            if root == None :
+                return None 
+            nonlocal start, end
+            l= lca_find(root.left)
+            r = lca_find(root.right)
+            root_ = None
+            if root.val == startValue or root.val == destValue or (l and r ):
+                root_ = root 
+                if  root.val == startValue:
+                    start  = root
+                if root.val   == destValue:
+                    end   = root
+
+
             
-            parent[root] = par
-            if root.val == startValue:
-                start = root
-            if root.val  == destValue:
-                end  = root
-            dfs(root.left,root)
-            dfs(root.right,root)
-
-        
-        dfs(root,None)
+            return root_ or l or r 
+                
 
 
-        q = deque([(start,"")])
-
-        vis  = set()
-        vis.add(start.val)
-        
-        while(q):
-            node,path  = q.popleft()
-            if node == end:
-                return path
-            if node.left and node.left.val not in vis:
-                q.append((node.left,path+"L"))
-                vis.add(node.left.val)
-            if node.right and node.right.val not in vis:
-                q.append((node.right,path+"R"))
-                vis.add(node.right.val)
-            if parent[node] and  parent[node].val not in vis:
-                q.append((parent[node],path+"U"))
-                vis.add(parent[node].val)
+      
+        def path_find(s, d,path):
+            if s  == None:
+                return False
+            if s == d:
+                return True
+            path.append("L")
+            if path_find(s.left, d,path):
+                return True
+            path.pop()
+            path.append("R")
+            if path_find(s.right, d, path):
+                return True
+            path.pop()
             
+
+
+
+
+        lca = lca_find(root)
+        lca_to_src = []
+        lca_to_dest =[]
+
+        path_find(lca,start,lca_to_src)
+        path_find(lca,end,lca_to_dest)
+
+        ans = ""
+        print(lca_to_src, lca_to_dest)
+
+        for c in lca_to_src:
+            ans+="U"
+
+        ans+= "".join(lca_to_dest)
+        
+
+        return ans
+
 
 
 
