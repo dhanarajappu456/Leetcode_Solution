00857a885e93b775e96f6078bcdf15660fe52329
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-07-18 21:48:16 +0530

    Time: 134 ms (38.79%) | Memory: 17 MB (38.59%) - LeetSync

--- 1653-number-of-good-leaf-nodes-pairs/number-of-good-leaf-nodes-pairs.py
+++ 1653-number-of-good-leaf-nodes-pairs/number-of-good-leaf-nodes-pairs.py
@@ -6,74 +6,28 @@
 #         self.right = right
 class Solution:
     def countPairs(self, root: TreeNode, distance: int) -> int:
+        self.ans   =  0
+        def rec(root):
 
 
-        '''
-            
-            1.create the undirected greaph 
-            2.Find and store  leaf node
-            3.for each leaf node find the pair by starting bfs
-        
-        '''
-        
-
-        
-        adj = defaultdict(list)
-
-        leafs  = set()
-
-        '''
-        create undir graph
-        '''
-        def create_g(root,prev):
             if root == None:
-                return None 
-            if root.left  == None and root.right == None:
-                leafs.add(root)
-            if prev!= None:
-                adj[root].append(prev)
-                adj[prev].append(root)
-            create_g(root.left,root)
-            create_g(root.right,root)
-        create_g(root,None)
-
-        '''
-        to keep track of processed leafs, means all pair with this leaf is obtained
-        '''
-        processed_leaf  =  set()
-        ans = 0
-
-        '''
-        processing each leaf , 
-        by starting bfs from this leaf  till d dist frontier , 
-        so as  to find all the valid pair formed by this leaf
-        '''
-        for leaf in leafs:
-            d=1 
-            q= deque([])
-            if leaf not in processed_leaf:
-                q = deque([leaf])
+                return [ ] 
             
-            vis = set()
+            if root.left == None and root.right == None:
+                return [1]
+            left_leaf_depths = rec (root.left)
+            right_leaf_depths = rec (root.right)
+
+            for d1 in left_leaf_depths:
+                for d2 in right_leaf_depths:
             
-            vis.add(leaf)
-            while(d<=distance):
-                size = len(q )
-                for i in range(size):
-                    node = q.popleft( )
-                    for neib in adj[node]:
-                        
-                        if neib not in vis:
-                            q.append( neib )
-                            vis.add( neib )
-                            '''
-                            got other node of the valid pair with leaf
-                            '''
-                            if neib not in processed_leaf  and neib.left  == None and neib.right == None:
-                                ans+=1
-                d+=1
-            processed_leaf.add(leaf)
-        return ans
+                    if d1+d2  <= distance:
+
+                        self.ans+=1
+            all_depths = left_leaf_depths + right_leaf_depths
+            return [d+1 for d in all_depths]
+        rec(root)
+        return self.ans
 
 
 
