109217ec8a1b5bb299bb12256c31a4f18eb517dd
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-07-20 14:48:04 +0530

    Time: 608 ms (14.08%) | Memory: 21.8 MB (36.82%) - LeetSync

--- 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.py
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.py
@@ -2,16 +2,22 @@ class Solution:
     def restoreMatrix(self, rowSum,colSum):
         m, n  = len(rowSum), len(colSum)
         ans  = [[0 for j in range(n)] for i in range(m)]
-     
+        #sufficing the row 
         for i in range(m):
             ans[i][0] = rowSum[i]
+        
+        #cheking if the cols_sum is satisified , if not move the values to the next row
         for j in range(n):
             col_sm = 0 
             for i in range(m):
                 col_sm+= ans[i][j]
                 diff =  max(0 , col_sm - colSum[j])
-                if diff>0:
-                    ans[i][j+1] = diff
+                #if there is an excess than the given colSum for the col, then move the excess
+                #to the cell in the next col of the same row .
+                if diff>0: 
+                    #removing the excess value 
                     ans[i][j]-=diff
+                    #moving the excess to the cell of next col and same row 
+                    ans[i][j+1] = diff
                 col_sm -=diff
         return ans
