4bb0fbeebf6f4f248b8790b3f5a2bf0d14d5702a
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-07-20 15:04:34 +0530

    Time: 555 ms (64.98%) | Memory: 22 MB (14.08%) - LeetSync

--- 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.py
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.py
@@ -1,23 +1,11 @@
 class Solution:
     def restoreMatrix(self, rowSum,colSum):
         m, n  = len(rowSum), len(colSum)
-        ans  = [[0 for j in range(n)] for i in range(m)]
-        #sufficing the row 
+        ans  = [[0 for j in range(n)] for i in range(m)] 
         for i in range(m):
-            ans[i][0] = rowSum[i]
-        
-        #cheking if the cols_sum is satisified , if not move the values to the next row
-        for j in range(n):
-            col_sm = 0 
-            for i in range(m):
-                col_sm+= ans[i][j]
-                diff =  max(0 , col_sm - colSum[j])
-                #if there is an excess than the given colSum for the col, then move the excess
-                #to the cell in the next col of the same row .
-                if diff>0: 
-                    #removing the excess value 
-                    ans[i][j]-=diff
-                    #moving the excess to the cell of next col and same row 
-                    ans[i][j+1] = diff
-                col_sm -=diff
+            for j in range(n) :
+                val  = min(rowSum[i],colSum[j])
+                ans[i][j] = val
+                rowSum[i]-= val
+                colSum[j]-=val
         return ans
