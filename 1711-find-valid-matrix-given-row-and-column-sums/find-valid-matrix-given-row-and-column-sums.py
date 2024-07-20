aba1d4bc20ff60c40c80c79203859a58c0afb28d
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-07-20 15:07:49 +0530

    Time: 579 ms (43.32%) | Memory: 22.2 MB (7.22%) - LeetSync

--- 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.py
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.py
@@ -4,8 +4,10 @@ class Solution:
         ans  = [[0 for j in range(n)] for i in range(m)] 
         for i in range(m):
             for j in range(n) :
+                #update the cell with min value of either the colSum or rowSum for the cell
                 val  = min(rowSum[i],colSum[j])
                 ans[i][j] = val
+                #update the rowSum and colSum 
                 rowSum[i]-= val
                 colSum[j]-=val
         return ans
