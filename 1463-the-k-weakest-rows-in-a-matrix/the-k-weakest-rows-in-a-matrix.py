a2ff66b01af2fa9e6e25bee85eaae1090ddcf66a
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-07-23 01:13:21 +0530

    Time: 101 ms (19.28%) | Memory: 16.9 MB (24.85%) - LeetSync

--- 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.py
+++ 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.py
@@ -1,15 +1,16 @@
 class Solution:
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
-
-
-        m,n  = len(mat), len(mat[0])
-
-        arr =[]
-        for i in range(m): 
-            arr.append((mat[i].count(1),i))
-        print(arr)
-        arr.sort()
-        return [ind  for count  ,ind in arr[:k]] 
-
-
         
+        cnt =  [ (sum(m) , i) for i,m in enumerate(mat)]
+        n = len(mat)
+        cnt.sort()
+        
+        prev_val = -1
+        
+        ans = []
+        i= 0
+        while(i<k and prev_val<=cnt[i][0] ):
+            ans.append(cnt[i][1])
+            prev_val = cnt[i][0]
+            i+=1
+        return ans 
