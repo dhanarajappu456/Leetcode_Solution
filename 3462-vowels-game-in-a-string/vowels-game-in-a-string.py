351e521b31be48a65e061323af821ecdb4886c9f
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-07-23 20:05:14 +0530

    Time: 69 ms (82.72%) | Memory: 17.5 MB (55.98%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.py
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.py
@@ -1,10 +1,8 @@
 class Solution:
     def doesAliceWin(self, s: str) -> bool:
-
         vow  = set(['a','e','i','o','u'])
         cnt  = 0
-        for c in s:
-            if c in vow:
-                cnt+= 1
+        vows = [1 for c in s  if c in vow]
+        cnt = sum(vows)
         return cnt>=1
         
