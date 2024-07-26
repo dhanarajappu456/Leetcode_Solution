d29cec2bdb62c498db2aaa7b546513c7f09d86bb
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-07-26 21:13:31 +0530

    Time: 9100 ms (5.00%) | Memory: 17.6 MB (96.97%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -1,45 +1,37 @@
-import heapq as h 
+
 
 class Solution:
     def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
         
-        adj = defaultdict(list)
-        for a,b,w in edges:
+        
 
-            adj[a].append((b,w))
-            adj[b].append((a,w))
-
-        def djikstra(i):
+        def bellman(i):
 
             d =[float("inf") for i  in range(n)]
             d[i] =0
-            min_heap = [(i,0)]
-            h.heapify(min_heap )
-            #stores all nodes reachable from the source node
-            #note , it is a set , since same nodes can be oushed to heap sometimes
-            # ex: conider source 1
-            # 1->(4,10) , (2,1)
-            # 2->(4,1)
-            #threshold :100
-            # 4 may be pushed twice , here , but we need to have it only once
-            nodes  =set()
-            while(min_heap):
-                node,w = h.heappop(min_heap)
-                for nb,nb_wt in adj[node]:
-                    #when the path len<=threshold and is reaching the nb with short path , update the valeu for nb 
-                    #and include it to the st of reachable node
-                    if (w+nb_wt<= distanceThreshold) and (d[nb]>w+ nb_wt):
-                        h.heappush(min_heap , (nb,nb_wt+w))
-                        d[nb] = w + nb_wt 
-                        nodes.add(nb)
+            nodes = set()
+            for _ in range(n-1):
+                #relax edges in both direction, as is undir
+                for a,b, w  in edges:
+
+                    d[b] = min(d[b], d[a]+w)
+                    d[a] = min(d[a], d[b]+w)
+                 
+            #dont consider the source node itself , when counting  the rechable node
+            for v in range(n):
+                if i!=v and d[v]<=distanceThreshold:
+                    nodes.add(v)
             return nodes
+            
+            
+
 
         ans  = -1 
         min_cnt  = float("inf")
-        #for each node as source find all possible dest that can be reached
         for i in range(n):
             
-            nodes  =  djikstra(i)
+            nodes  =  bellman(i)
+
             if len(nodes) <= min_cnt:
                 min_cnt = len(nodes)
                 ans = i
