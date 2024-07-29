0a1a519428643431dbe9ced9e4dad69986b28974
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-07-29 13:33:41 +0530

    Time: 6227 ms (5.04%) | Memory: 18 MB (6.25%) - LeetSync

--- 1511-count-number-of-teams/count-number-of-teams.py
+++ 1511-count-number-of-teams/count-number-of-teams.py
@@ -11,15 +11,15 @@ class Solution:
             n1 = rating[i]
        
             for j in range(i+1,n):
-                n1,n2= rating[i],rating[j]
-                #in this case , see how many number we have which<n1, 
-                #which can be found from arr1
-                if n1<n2:
-                    ans += arr1[n1-1]
-                #in this case , see how many number we have which > n1, 
-                #which can be found from arr2
-                if n1>n2:
-                    ans+= arr2[n1+1]
+                n2,n3= rating[i],rating[j]
+                #in this case , see how many number we have which<n2, 
+                #which can act as n1, this can be found from arr1
+                if n2<n3:
+                    ans += arr1[n2-1]
+                #in this case , see how many number we have which>n2, 
+                #which can act as n1, this can be found from arr1
+                if n2>n3:
+                    ans+= arr2[n2+1]
             #updating the arr1 and arr2 info
             for i in range(n1,len(arr1)):
                 arr1[i]+=1
