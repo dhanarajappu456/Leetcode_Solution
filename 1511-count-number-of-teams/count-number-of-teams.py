89c02b31e263f986444ed75b1678e0738ebc2edb
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-07-29 13:30:29 +0530

    Time: 6318 ms (5.04%) | Memory: 18.1 MB (6.25%) - LeetSync

--- 1511-count-number-of-teams/count-number-of-teams.py
+++ 1511-count-number-of-teams/count-number-of-teams.py
@@ -1,6 +1,7 @@
 class Solution:
     def numTeams(self, rating: List[int]) -> int:
 
+        #stores at any index i the total numbers less<=i and total numbers>=i
 
         arr1 =[ 0 for i in range(max(rating)+ 2)]
         arr2 =[ 0 for i in range(max(rating)+ 2)]
@@ -11,12 +12,15 @@ class Solution:
        
             for j in range(i+1,n):
                 n1,n2= rating[i],rating[j]
-
+                #in this case , see how many number we have which<n1, 
+                #which can be found from arr1
                 if n1<n2:
                     ans += arr1[n1-1]
-  
+                #in this case , see how many number we have which > n1, 
+                #which can be found from arr2
                 if n1>n2:
                     ans+= arr2[n1+1]
+            #updating the arr1 and arr2 info
             for i in range(n1,len(arr1)):
                 arr1[i]+=1
             for i in range(1,n1+1):
