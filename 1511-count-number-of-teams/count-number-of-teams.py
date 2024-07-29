aed370f75baf88777e3dcc5f4af3858a3334ad69
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-07-29 14:24:58 +0530

    Time: 491 ms (56.43%) | Memory: 16.7 MB (66.67%) - LeetSync

--- 1511-count-number-of-teams/count-number-of-teams.py
+++ 1511-count-number-of-teams/count-number-of-teams.py
@@ -1,31 +1,33 @@
 class Solution:
     def numTeams(self, rating: List[int]) -> int:
 
-        #stores at any index i the total numbers less<=i and total numbers>=i
-
-        arr1 =[ 0 for i in range(max(rating)+ 2)]
-        arr2 =[ 0 for i in range(max(rating)+ 2)]
-        ans  = 0 
+      
+        ans =0 
         n  = len(rating)
-        for i in range(n-1):
-            n1 = rating[i]
-       
-            for j in range(i+1,n):
-                n2,n3= rating[i],rating[j]
-                #in this case , see how many number we have which<n2, 
-                #which can act as n1, this can be found from arr1
-                if n2<n3:
-                    ans += arr1[n2-1]
-                #in this case , see how many number we have which>n2, 
-                #which can act as n1, this can be found from arr1
-                if n2>n3:
-                    ans+= arr2[n2+1]
-            #updating the arr1 and arr2 info
-            for i in range(n1,len(arr1)):
-                arr1[i]+=1
-            for i in range(1,n1+1):
-                arr2[i]+=1
+        for j in range(1,n-1):
             
+            left_smalls,left_greats,right_smalls,right_greats =0,0,0,0
+            n2 = rating[j]
 
+            #finding cnt of nums on left side, < n2  (middle element)
+            #and also cnt of nums on left side> n2 
+            for i in range(j):
+                if rating[i]<n2:
+                    left_smalls+=1
+                if rating[i]>n2:
+                    left_greats+=1
+            
+            #finding cnt of nums on right side, < n2  (middle element)
+            #and also cnt of nums on right side> n2 
+            for k in range(j+1,n):
+                if rating[k]<n2:
+                    right_smalls+=1
+                if rating[k]>n2:
+                    right_greats+=1
+            #the final ans is 
+            #combination 1  - smalls on left , n2 , large on right
+            #combination 2 -  large on right, n2, small on right
+            ans += left_smalls *right_greats  + left_greats * right_smalls
+            
         return ans
         
