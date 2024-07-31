3acf0d2148d1155e5fda7f81b4e14f268b656e23
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-07-31 16:32:05 +0530

    Time: 53 ms (48.02%) | Memory: 17.2 MB (41.24%) - LeetSync

--- 1196-filling-bookcase-shelves/filling-bookcase-shelves.py
+++ 1196-filling-bookcase-shelves/filling-bookcase-shelves.py
@@ -21,9 +21,8 @@ class Solution:
             #you can keep the book at same shelf as prev book
             if rem_width_prev>=w:
                 take_current = rec(i+1,rem_width_prev-w, max(h,max_height_prev))
-            #can skip the prev_shelf
+            #can skip the prev_shelf , when skipping, we need add the prev_shelf_height , as height increase now
             skip_current = max_height_prev + rec(i+1,shelfWidth-w, h)
-
             memo[(i,rem_width_prev)]=  min(take_current,skip_current )
             return memo[(i,rem_width_prev)]
 
