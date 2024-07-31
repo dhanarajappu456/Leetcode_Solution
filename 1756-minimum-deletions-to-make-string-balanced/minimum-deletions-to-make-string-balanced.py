e2eee713f7b8190dc1f69221f267c761f8245d43
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-07-31 21:01:53 +0530

    Time: 447 ms (39.34%) | Memory: 17.9 MB (52.79%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.py
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.py
@@ -2,21 +2,17 @@ class Solution:
     def minimumDeletions(self, s: str) -> int:
         
         n  = len(s)
-        a_count = [0 for i in range(n)]
-        a_cnt =  0 
-        for i in range(n-1,-1,-1):
-           
-            a_count[i] = a_cnt
-            if s[i] == "a":
-                a_cnt+=1
-        b_left = 0 
+        a_right ,b_left= 0 ,0
+        #a_cnt initially is total number of 'a' in the array
+        a_right  = s.count('a')
         ans  = float("inf")
         for i,c in enumerate(s):
-            a_right = a_count[i]
+            #dynamically get the number of a to right
+            if c =='a':
+                a_right-=1
             ans = min( ans , b_left+a_right)
             if c=="b":
                 b_left+=1
-            
         return ans
 
             
