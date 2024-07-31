f631d7017ec12cd163e25add17f50c870c6445a0
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-07-31 13:13:46 +0530

    Time: 59 ms (20.06%) | Memory: 17.1 MB (48.87%) - LeetSync

---
+++ 1196-filling-bookcase-shelves/filling-bookcase-shelves.py
@@ -0,0 +1,30 @@
+from typing import List
+
+class Solution:
+    def minHeightShelves(self, books: List[List[int]], shelfWidth: int) -> int:
+        n = len(books)
+        # Memoization dictionary to store intermediate results
+        memo = {}
+        
+        def rec(i):
+            if i == n:
+                return 0
+            if i in memo:
+                return memo[i]
+            
+            width = 0
+            height = 0
+            min_height = float('inf')
+            
+            # Try placing as many books as possible on the current shelf
+            for j in range(i, n):
+                if width + books[j][0] > shelfWidth:
+                    break
+                width += books[j][0]
+                height = max(height, books[j][1])
+                min_height = min(min_height, height + rec(j + 1))
+            
+            memo[i] = min_height
+            return min_height
+        
+        return rec(0)
