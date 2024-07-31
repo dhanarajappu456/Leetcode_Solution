8baec9dfb4c4ae0948860f258bf0eb5b10956bcc
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-07-31 16:27:22 +0530

    Time: 56 ms (28.81%) | Memory: 17.2 MB (41.24%) - LeetSync

--- 1196-filling-bookcase-shelves/filling-bookcase-shelves.py
+++ 1196-filling-bookcase-shelves/filling-bookcase-shelves.py
@@ -5,26 +5,26 @@ class Solution:
         n = len(books)
         # Memoization dictionary to store intermediate results
         memo = {}
-        #function computes minheight to keep all books form i to n
-        def rec(i):
+        #this keeps minheight to keep books from i to n with rem_widht_prev as the widht of shelf in which prev
+        #book is kept  and max_heigt of the  same
+        def rec(i,rem_width_prev,max_height_prev ):
             if i == n:
-                return 0
-            if i in memo:
-                return memo[i]
-            
-            width = 0
-            height = 0
-            min_height = float('inf')
-            
-            # Try placing as many books as possible on the current shelf
-            for j in range(i, n):
-                if width + books[j][0] > shelfWidth:
-                    break
-                width += books[j][0]
-                height = max(height, books[j][1])
-                min_height = min(min_height, height + rec(j + 1))
-            
-            memo[i] = min_height
-            return min_height
-        
-        return rec(0)
+                return max_height_prev
+            #note we need to cache only i and rem_width_prev, becaue when
+            #i and rem_width_prev is same , the max_height_prev also will be same
+            #as is the max_heihg of prev_shelf, and also the books are kept in order,
+            #and same i  and rem_width_prev means same set of books in the prev shelf no matter what
+            if (i,rem_width_prev) in memo:
+                return memo[(i,rem_width_prev)]
+            w,h = books[i]
+            take_current = float("inf")
+            #you can keep the book at same shelf as prev book
+            if rem_width_prev>=w:
+                take_current = rec(i+1,rem_width_prev-w, max(h,max_height_prev))
+            #can skip the prev_shelf
+            skip_current = max_height_prev + rec(i+1,shelfWidth-w, h)
+
+            memo[(i,rem_width_prev)]=  min(take_current,skip_current )
+            return memo[(i,rem_width_prev)]
+
+        return rec(0,shelfWidth,0)
