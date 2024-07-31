42744fa5bdbc88906ea8417c19cf264d933bbdf6
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-07-31 20:56:24 +0530

    Time: 616 ms (21.97%) | Memory: 20.3 MB (23.28%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.py
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.py
@@ -1,16 +1,22 @@
 class Solution:
     def minimumDeletions(self, s: str) -> int:
         
-        #'"bbbababaa"'
-        stk = []
-        cnt=0
-        for c in s:
-            if stk and stk[-1]=="b" and c=="a":
-                stk.pop(-1)
-                cnt+=1
-                continue
-            stk.append(c)
-
-        return cnt
+        n  = len(s)
+        a_count = [0 for i in range(n)]
+        a_cnt =  0 
+        for i in range(n-1,-1,-1):
+           
+            a_count[i] = a_cnt
+            if s[i] == "a":
+                a_cnt+=1
+        b_left = 0 
+        ans  = float("inf")
+        for i,c in enumerate(s):
+            a_right = a_count[i]
+            ans = min( ans , b_left+a_right)
+            if c=="b":
+                b_left+=1
+            
+        return ans
 
             
