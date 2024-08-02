1999bf021cefabcbb4dc1f8ef91f80ee4611d78e
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-08-02 15:12:52 +0530

    Time: 694 ms (67.12%) | Memory: 20.5 MB (45.50%) - LeetSync

--- 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.py
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.py
@@ -1,46 +1,7 @@
 class Solution:
     def minSwaps(self, nums: List[int]) -> int:
-        '''
-            Input
-            nums =
-            [1,1,1,0,0,1,0,1,1,0]
+    
 
-            Use Testcase
-            Stdout
-            [3, 2, 1, 0, 0, 1, 0, 2, 1, 0]
-            Output
-            3
-            Expected
-            1
-        '''
-        n = len(nums)
-        tot_one, tot_zero = 0,0
-        # one = [ 0 for i in range(n)]
-        # cnt = 0 
-        # for i in range(n-1,-1,-1):
-        #     if nums[i] ==0:
-        #         cnt = 0 
-        #         tot_zero+=1
-        #         continue
-        #     cnt+=1
-        #     tot_one+=1
-        #     one[i] = cnt
-        # i= n-1
-        # while(i>=0 and nums[i] ==1 ):
-        #     i-=1
-        
-        # if i+1<n:
-        #     #for edge case with just one element and is one ,ie  [1]
-        #     if i+1!=0:
-        #         one[i+1]+= one[0]
-        # print(one)
-        # ans  = float("inf")
-        # for i in range(n):
-        #     ans = min(ans, tot_one - one[i])
-        # return  ans 
-
-        if nums.count(1) ==1 :
-            return 0
         i,j = 0,nums.count(1)-1
         cnt_one_wind = nums[i:j+1].count(1)
         tot_ones =nums.count(1)
@@ -63,7 +24,6 @@ class Solution:
             if nums[j] ==1:
                 cnt_one_wind+=1
         
-
         return ans 
             
 
