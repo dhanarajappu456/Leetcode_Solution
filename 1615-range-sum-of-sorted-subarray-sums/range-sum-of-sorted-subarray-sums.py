c12d77c99581ebddf006ef1bc93eb4948d501cdc
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-08-04 15:20:48 +0530

    Time: 264 ms (31.86%) | Memory: 16.7 MB (97.79%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.py
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.py
@@ -1,38 +1,26 @@
+import heapq as h 
 class Solution:
     def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
         mod  = 10**9+7 
-        sm = [0 for i in range(max(nums)*n+1)]
-        for i in range(n):
-            s=0
-            for j in range(i,n):
-                s+= nums[j]
-                sm[s]+=1
-        i = 0 
-        m = len(sm)
+        min_heap  = [ ]
+
+        for i, num in enumerate(nums):
+            h.heappush(min_heap , (num,i) )
+        
         ans = 0 
-        #total terms to be summed
-        needed  = right-left+1
-        #skip these many terms , which comes before the index,  left
-        # in the hypothetical sorted array 
-        to_skip =left-1
-        while(i<m and needed!=0):
+        index = 1
+        while min_heap :
+            sm , i  = h.heappop(min_heap)
+            if left<=index<=right:
+                ans = (ans%mod + sm%mod )%mod
+            if i+1<n:
+                h.heappush(min_heap, ( sm + nums[i+1],i+1))
+            index+=1
+            #optimisation - early break out 
+            if index>right:
+                break
             
-            if sm[i]!=0:
-                #rem stores the usable occurence of sum sm[i]
-                rem  = sm[i]
-                #we can use only those many occurence from cnt of terms with sum = sm[i],
-                #after skipping "to_skip" terms and if at all anything remains
-                if to_skip:
-                    temp_rem = rem
-                    rem = sm[i] - min(to_skip, rem)
-                    to_skip  -=min(to_skip, temp_rem)
-                #finding the ans
-                ans = (ans%mod  + ( i*min(needed, rem)) %mod)%mod
-                #now we need only these many terms to be summed
-                needed -= min(needed, rem)
-            i+=1
-        return ans
-                
+        return ans 
                 
              
 
