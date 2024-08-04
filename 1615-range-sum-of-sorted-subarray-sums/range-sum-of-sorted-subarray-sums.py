9d626118dd4b535f76545153b5cf3aab77faba72
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-08-04 11:33:05 +0530

    Time: 133 ms (94.69%) | Memory: 17.3 MB (84.07%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.py
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.py
@@ -10,17 +10,25 @@ class Solution:
         i = 0 
         m = len(sm)
         ans = 0 
+        #total terms to be summed
         needed  = right-left+1
+        #skip these many terms , which comes before the index,  left
+        # in the hypothetical sorted array 
         to_skip =left-1
         while(i<m and needed!=0):
             
             if sm[i]!=0:
+                #rem stores the usable occurence of sum sm[i]
                 rem  = sm[i]
+                #we can use only those many occurence from cnt of terms with sum = sm[i],
+                #after skipping "to_skip" terms and if at all anything remains
                 if to_skip:
                     temp_rem = rem
                     rem = sm[i] - min(to_skip, rem)
                     to_skip  -=min(to_skip, temp_rem)
+                #finding the ans
                 ans = (ans%mod  + ( i*min(needed, rem)) %mod)%mod
+                #now we need only these many terms to be summed
                 needed -= min(needed, rem)
             i+=1
         return ans
