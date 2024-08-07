26fa383982fa1ddcdc33ffef8afa6c257dc17fe2
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-08-07 12:37:10 +0530

    Time: 155 ms (56.83%) | Memory: 17.8 MB (8.15%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
@@ -6,18 +6,19 @@ class Solution:
         letter_cnt   = Counter(word)
         for c in letter_cnt:
             h.heappush(max_heap,(-letter_cnt[c],c))
-        min_heap  =[ (0,i) for i in range(2,10)]
-        h.heapify(min_heap)
+    
         ans = 0 
-        mapped  = {}
+        #increased once per each unique character, 
+        #which denote position among all unique letters in the word
         position = 0
         while(max_heap):
-    
             occur , c  = h.heappop(max_heap)
             occur *= -1
+            #this is the position among the places mapped to a particular character
+            #which is nothing  but rounds, the alog first fills first position of all numbers 
+            #then second and so on 
             pos = position//8 +1 
             ans  += pos*occur
-
             position+=1
            
         return ans
