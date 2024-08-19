0420551aeb04afd7132da858a744e04102312206
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-08-19 13:49:15 +0530

    Time: 863 ms (5.01%) | Memory: 16.5 MB (61.96%) - LeetSync

---
+++ 650-2-keys-keyboard/2-keys-keyboard.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minSteps(self, n: int) -> int:
+
+        def rec (current,clipboard):
+
+            if current  == n:
+                return 0 
+            if current>n:
+                return float("inf")
+
+
+            res1 = 1+rec(current+clipboard , clipboard) 
+
+            res2  = 2+ rec( 2* current  , current )
+
+            return min(res1 , res2)
+        if n ==1 : 
+            return 0 
+        return 1+ rec(1,1)
+
+        
