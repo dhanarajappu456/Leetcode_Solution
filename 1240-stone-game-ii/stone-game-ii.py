6127a1d8f89d10d926aaf71f74bf80d03f902d5b
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-08-20 14:05:03 +0530

    Time: 741 ms (22.45%) | Memory: 20.7 MB (17.35%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.py
+++ 1240-stone-game-ii/stone-game-ii.py
@@ -1,21 +1,27 @@
 
-
 class Solution:
     def stoneGameII(self, piles: List[int]) -> int:
         
+        #we focus to maximise alices points , given that both of them play optimallly
+        #alice will  try out all the possibilites of choosing the 2m stones  and choose the max she could get, 
+        #bob will try all the 2m possibilities and choose the path that make allice get the minimum point
+
         n = len(piles)
         memo  = {}
       
-        def rec(m,ind,alice):
+
+      # the state m,, ind , player indicate , at the index from ind to end , with first,  2m possibility of choosing the piles
+      #if a particular player , plays , then what is the max codt alice can get 
+        def rec(m,ind,player):    #timw m*ind*player = logn * n * 2 = 12*(10^4)*2 = 10^5
             
             if ind>=n:
                 return 0
             
-            if(m,ind, alice ) in memo:
-                return memo[(m,ind, alice)]
+            if(m,ind, player ) in memo:
+                return memo[(m,ind, player)]
             
        
-            if alice:
+            if player:
                 
                 
                 points = 0
@@ -31,11 +37,9 @@ class Solution:
                 
                     points  = min(points , rec(max(i+1,m), ind+i+1 , 1))
                 
-            memo[(m,ind, alice)] = points
-            return  memo[(m,ind, alice)]
+            memo[(m,ind, player)] = points
+            return  memo[(m,ind, player)]
         
         return rec(1,0,1)
-                
-                
-                
         
+                
