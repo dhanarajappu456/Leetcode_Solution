4d1a1dc9089d5abecd4e5780867fbbb47a7a3ebd
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-09-28 04:55:28 +0530

    Time: 265 ms (73.97%) | Memory: 17.5 MB (46.25%) - LeetSync

---
+++ 731-my-calendar-ii/my-calendar-ii.py
@@ -0,0 +1,34 @@
+class MyCalendarTwo:
+
+    def __init__(self):
+        '''
+        keeps two array 
+        overlap: keeps only the over lapping section b/w two intervals
+        non-ovelap: one keeps all the current interval
+        
+        so when we are looking for current interval , we can immediately 
+        see the overlap , array if there is section(which is already ovelapping b/w two interval) , that overlap with current interval
+        
+        
+        '''
+        self.overlap = []
+        self.non_overlap = []
+        
+
+    def book(self, start: int, end: int) -> bool:
+        
+        for inter in self.overlap:
+            # checks if there is a overlap 
+            if start< inter[1]  and end > inter[0]:
+                return False
+        # this insert the ovelapping section b/w two interval to the overlap array
+        for inter in self.non_overlap:
+            if start< inter[1]  and end > inter[0]:
+                self.overlap.append(( max(start, inter[0]) , min(end, inter[1])))
+        #always keep all intervals encountered so far in this list
+        self.non_overlap.append((start, end))
+        return True
+
+# Your MyCalendarTwo object will be instantiated and called as such:
+# obj = MyCalendarTwo()
+# param_1 = obj.book(start,end)
