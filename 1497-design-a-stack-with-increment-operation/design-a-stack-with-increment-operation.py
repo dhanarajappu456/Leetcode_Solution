c265def979a2c1d268123a3696a185245439a8e2
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-10-01 05:56:55 +0530

    Time: 75 ms (81.85%) | Memory: 17.5 MB (87.39%) - LeetSync

--- 1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.py
+++ 1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.py
@@ -1,4 +1,14 @@
 '''
+the idea is to se a array called inc , where value at index idx is 
+the value to be increased for all elements from bottom of stack , 
+till idx+1 numebr of items .
+
+thus when a pop happens it checks if it need to be incremeneted based 
+on its position in stack and the same index position in inc stack where the amount by which to be incremented is placed.
+
+so this is kinda lazy update .
+
+so that the inc operateion can be performed in o(1), as compared to brutre which is k 
 '''
 class CustomStack:
     def __init__(self, maxSize: int):
