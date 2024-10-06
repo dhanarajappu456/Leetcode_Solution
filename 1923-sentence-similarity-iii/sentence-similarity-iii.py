be25b6dd8494984850371cb231a029483cde44d9
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-10-06 18:57:06 +0530

    Time: 38 ms (36.65%) | Memory: 16.6 MB (13.04%) - LeetSync

--- 1923-sentence-similarity-iii/sentence-similarity-iii.py
+++ 1923-sentence-similarity-iii/sentence-similarity-iii.py
@@ -5,15 +5,18 @@ class Solution:
         m, n  = len(a1) , len(a2)
 
         k, l   = m-1 , n-1
+        #matching the pref words in s1 and s2
         while(i<m and j<n and ( a1[i] == a2[j]) ):
             i+=1
             j+=1
-        
+        #matching the suff words
         while(k>=i and l>=j and ( a1[k] == a2[l]) ):
             k-=1
             l-=1
-        
+        #if there is subarray portion in both string taht can't be matched
+        #in above steps, then it can't be made similar
         if i<=k and j<=l :
             return False
-
+        #one can be made similar to another
+        
         return True
