fb04d8341c160302759ae9020b6b47dab07bc3f6
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-10-07 21:01:59 +0530

    Time: 44 ms (57.39%) | Memory: 16.5 MB (41.77%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.py
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.py
@@ -1,15 +1,19 @@
 class Solution:
     def minLength(self, s: str) -> int:
-
-        stk =[]
-
+        stk  =[]
         for c in s :
-
-            if stk and stk[-1] =="C" and c=="D":
-                stk.pop(-1)
-            elif stk and stk[-1] =="A" and c=="B":
-                stk.pop(-1)
+            if c == "B":
+                if stk and stk[-1] == "A":
+                    stk.pop(-1)
+                else:
+                    stk.append(c)
+            elif c == "D":
+                if stk and stk[-1] == "C":
+                    stk.pop(-1) 
+                else:
+                    stk.append(c)
             else:
                 stk.append(c)
+         
         return len(stk)
-        
+
