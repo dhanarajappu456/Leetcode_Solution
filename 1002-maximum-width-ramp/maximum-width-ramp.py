d18255077f8d265622ea6abe7e43f0536f6148bf
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-10-11 21:57:12 +0530

    Time: 414 ms (14.18%) | Memory: 27.7 MB (6.18%) - LeetSync

--- 1002-maximum-width-ramp/maximum-width-ramp.py
+++ 1002-maximum-width-ramp/maximum-width-ramp.py
@@ -12,6 +12,21 @@ this paves a stack or ds of decresing order to be formed ,  which therefore acn 
 to find the element with left most element in the stk with height<= current element
 t nlogn 
 s  n(stk)
+
+solution3- optimised solution - greedy 
+
+we sort the array based in value also we will have their old index saved
+thus as we iterate on this sorted array ,which is the right leg
+of the ramp , we need to get the min index among all the values 
+to the left of this value in array , 
+
+therefore we also keep track of the min index in the values 
+seen so far , so that we can find the max ramp dist 
+that current element can from with it beign right leg
+
+
+t nlogn 
+s  n(stk)
 '''
 
 
