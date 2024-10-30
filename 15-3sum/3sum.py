1f9884b624a7bc55032be488e5173ad598b01e33
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-10-30 15:42:09 +0000

    Time: 1865 ms (6.21%) | Memory: 19.8 MB (92.29%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -3,16 +3,16 @@ class Solution:
         ans  = set()
         n  = len(nums)
         nums.sort()
-        print(nums)
+
         z_set = set()
         for i in range(n-1,0, -1):
            
             for j in range(i-1,-1,-1):
                 y,x  = nums[i],nums[j]
                 if -(x+y) in z_set:
-                    ans.add((x,y,-(x+y))) 
-                  
+                    ans.add((x,y,-(x+y)))   
             z_set.add(nums[i])
+          
         return list(ans) 
 
 
