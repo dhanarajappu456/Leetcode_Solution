cc00a7ab240b334bf9b6e39fe31cae90d9ee724c
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-11-01 16:12:38 +0000

    Time: 1213 ms (60.00%) | Memory: 16.8 MB (46.13%) - LeetSync

---
+++ 2130-maximum-product-of-the-length-of-two-palindromic-subsequences/maximum-product-of-the-length-of-two-palindromic-subsequences.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxProduct(self, s: str) -> int:
+        n= len(s)
+        di =dict()
+        for i in range(1<<n):
+            st =""
+            #constructing the string , based on 
+            #chars chosen 
+            
+            for j in range(n):
+                if i&(1<<j)!=0:
+                    st = s[-(j+1)]+st
+            #store the subsequence 
+            #to dict , if is pali          
+            if st[::-1]==st:
+                di[i] =  len(st)
+                
+        max_ = -1*float("inf")
+ 
+        for i in di:
+            for j in di:
+                if j&i == 0:
+                    max_ = max(max_,di[i]*di[j])
+        return max_
