75d946e868db251eab6b9a6752238c713ff0006f
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-11-05 12:22:31 +0000

    Time: 21 ms (55.41%) | Memory: 16.8 MB (89.02%) - LeetSync

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.py
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.py
@@ -1,31 +1,23 @@
 class Solution:
-    def minChanges(self, s: str) -> int:
+    #the idea is that , a valdi partiton can be broken to 
+    #furhter small even partion at the end giving a parition with 
+    #two elements
 
-        i =0 
+    #so it is matter of checking for block of two elements like 
+    #what are the changes that we need 
+    def minChanges(self, s: str) -> int:
+        i =1
         n= len(s)
         count = 0
         ans  = 0 
         while(i<n):
-            #mismatch from same set of chars seen so far
-            if i>=1 and (s[i]!=s[i-1]):
-                #combine current char to the grp 
-                if count %2!=0:
-                    ans+=1
-                    count = 0
-                #keeep curr char for next subarry grp 
-                #form subarry with those chars in count
-                #which are already even
-                else:
-                    count = 1
-            
-            else:
-                count+=1
-            i+=1
+            #if current block two elemetn has , contrasting 
+            #elements then we need to change either one of it
+             
+            if s[i]!=s[i-1]:
+                ans+=1
+            i+=2
         return ans
-                
 
-            
-        return ans
-        
 
         
