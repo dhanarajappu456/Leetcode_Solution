afdae2d4e19735a517fe03e26869c4d45dcd1398
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-11-05 12:18:02 +0000

    Time: 54 ms (6.76%) | Memory: 16.9 MB (89.02%) - LeetSync

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.py
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.py
@@ -6,12 +6,15 @@ class Solution:
         count = 0
         ans  = 0 
         while(i<n):
-
+            #mismatch from same set of chars seen so far
             if i>=1 and (s[i]!=s[i-1]):
-
+                #combine current char to the grp 
                 if count %2!=0:
                     ans+=1
                     count = 0
+                #keeep curr char for next subarry grp 
+                #form subarry with those chars in count
+                #which are already even
                 else:
                     count = 1
             
