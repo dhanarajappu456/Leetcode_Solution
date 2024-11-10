1acdd9f666c55d20906ea169ee651c452ab291fc
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-11-10 23:34:44 +0000

    Time: 3084 ms (11.11%) | Memory: 35.9 MB (67.69%) - LeetSync

--- 3380-shortest-subarray-with-or-at-least-k-ii/shortest-subarray-with-or-at-least-k-ii.py
+++ 3380-shortest-subarray-with-or-at-least-k-ii/shortest-subarray-with-or-at-least-k-ii.py
@@ -5,23 +5,14 @@ class Solution:
         ors = 0 
         i  = 0
         j= 0 
-        #[5,3,2,0,0.........32 position]
         bits=[0 for i in range(32)]
-        #when the num removed from the subarray
-        #the 1's from those bits position are decremented
-        def remove(bits,num):
+
+        def update(bits,num,val):
             for i in range(32):
                 if (num&(1<<i))!=0 :
-                    bits[i]-=1
+                    bits[i]+= val
              
-        #when the num is add to the subarray
-        #the 1's from those bits position are incremented
-        def add(bits,num):
-            for i in range(32):
-                if (num&(1<<i))!=0 :
-                    bits[i]+=1
-        #get the decimal value obtained from the 
-        #bits array  
+  
         def get_decimal(bits):
             ans =  0 
             for i in range(32):
@@ -29,14 +20,14 @@ class Solution:
                     ans|=(1<<i)
             return ans
         while(j<n ):
-            add(bits,nums[j])
+            update(bits,nums[j],1)
             if get_decimal(bits)<k:
                 j+=1
                 continue
             while(i<=j and get_decimal(bits)>=k):
            
                 ans = min(ans,j-i+1)
-                remove(bits,nums[i])
+                update(bits,nums[i],-1)
                 i+=1
             j+=1
         return -1 if ans == float("inf") else ans
