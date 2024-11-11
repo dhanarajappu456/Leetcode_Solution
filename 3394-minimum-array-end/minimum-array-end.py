f550f3c8f089d83af2fd213930224990a61470f0
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-11-11 01:37:42 +0000

    Time: 0 ms (100.00%) | Memory: 16.7 MB (23.57%) - LeetSync

--- 3394-minimum-array-end/minimum-array-end.py
+++ 3394-minimum-array-end/minimum-array-end.py
@@ -7,8 +7,18 @@ class Solution:
             #fill the bit in the places wehre 
             #there is 0 in the x
             if i_x & x == 0 :
+                #fill the current bit in the value n-1 , pointer by i_n
+                #to the slot indicated by i_x on the x  
+                #also note that if it 0 pointed by i_n 
+                #jsut ignore it , as  it is already 0 
+                #in the x, just move to the next poistion in x , ie 
+                #move the i_x to next bit posotion  
                 if (n-1 ) & i_n !=0:
                     res |= i_x
+                #moving the pointer i_n , on the bits of value 
+                #represneted by n-1 
                 i_n = i_n<<1
+            #moving the pointer on bits of x ,
+            #to dfind the next empty slot on x 
             i_x  = i_x<<1
         return res
