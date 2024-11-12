c1835bc0e282ceda952647001a31bb60c002f0b5
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-11-12 10:55:27 +0000

    Time: 355 ms (6.10%) | Memory: 63.2 MB (86.02%) - LeetSync

--- 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.py
+++ 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.py
@@ -1,40 +1,39 @@
+from typing import List
+
 class Solution:
+    def customBinarySearch(self, items: List[List[int]], queryPrice: int) -> int:
+        l = 0
+        r = len(items) - 1
+        maxBeauty = 0
+
+        while l <= r:
+            mid = l + (r - l) // 2
+
+            if items[mid][0] > queryPrice:
+                r = mid - 1  # Ignore current and right side items, move to left.
+            else:
+                maxBeauty = max(maxBeauty, items[mid][1])
+                l = mid + 1
+
+        return maxBeauty
+
     def maximumBeauty(self, items: List[List[int]], queries: List[int]) -> List[int]:
-        #queris and items sorted on price
-        #note that we need to presereve the old index of he query 
-        #when sorting so that when we cget the answer for current 
-        #price we look , we ned to know , in which index we need to 
-        #store this ans 
-        queries =[(p,i) for i,p in enumerate(queries)]
-        items  = [(p,b) for p,b in items ]
-        queries.sort()
-        items.sort()
-        len_items  = len(items)
-        len_queries = len(queries)
-        #final res array 
-        res = [0 for i in range(len_queries)]
-        j= 0
-        ans = 0
-        #we start processing the queries , which is 
-        #sorted on price.
-        for p,ind_old in queries:
-            #for current query price , we 
-            #resume traversing the sorted items array
-            #until the prices are <=the current query price
-            #while doing this find max beauty so far
-            while(j<len_items and items[j][0]<= p):
-                ans = max(ans, items[j][1])
-                j+=1 
-            #once we get the beauty so far store it under 
-            #current query index poation in the old query aray
-            res[ind_old] = ans 
-        return res 
+        n = len(items)
+        m = len(queries)
+        result = [0] * m
 
+        # Step-1: Sort the array based on price in ascending order
+        items.sort()  # items = [[price_i, beauty_i]]
 
+        # Update items to store the maximum beauty seen so far for each price
+        maxBeautySeen = items[0][1]
+        for i in range(1, n):
+            maxBeautySeen = max(maxBeautySeen, items[i][1])
+            items[i][1] = maxBeautySeen
 
+        # Perform a binary search for each query to find the maximum beauty
+        for i in range(m):
+            queryPrice = queries[i]
+            result[i] = self.customBinarySearch(items, queryPrice)
 
-
-        
-
-        
-        
+        return result
