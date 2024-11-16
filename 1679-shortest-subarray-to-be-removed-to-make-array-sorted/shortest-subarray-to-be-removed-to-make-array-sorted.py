9c2a045eb0ffc52e3e9ecc1b0c7b9dd663e0fe08
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-11-16 00:53:40 +0000

    Time: 11 ms (86.96%) | Memory: 32.1 MB (6.17%) - LeetSync

--- 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.py
+++ 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.py
@@ -1,31 +1,21 @@
 class Solution:
     def findLengthOfShortestSubarray(self, arr: List[int]) -> int:
         n = len(arr)
-        
-        # Step 1: Find the longest non-decreasing prefix
-        left = 0
-        while left + 1 < n and arr[left] <= arr[left + 1]:
-            left += 1
-        
-        # If the entire array is already sorted
-        if left == n - 1:
-            return 0
-        
-        # Step 2: Find the longest non-decreasing suffix
-        right = n - 1
-        while right > 0 and arr[right - 1] <= arr[right]:
-            right -= 1
-        
-        # Step 3: Find the minimum length to remove by comparing prefix and suffix
-        result = min(n - left - 1, right)
-        
-        # Step 4: Use two pointers to find the smallest middle part to remove
-        i, j = 0, right
-        while i <= left and j < n:
-            if arr[i] <= arr[j]:
-                result = min(result, j - i - 1)
-                i += 1
-            else:
-                j += 1
-        
-        return result
+        ans = n
+        i,j = 0 , n-1
+        while(j>0 and   arr[j]>=arr[j-1]):
+            j-=1
+
+        #in case 15, 10,1,2,3,4,5,6 
+        #front portion might need ro be removed which is == j 
+        print(j)
+        ans  = min(ans, j)
+
+
+        while(i<j and (i==0 or arr[i]>= arr[i-1])) :
+            while(j<n and arr[i]>arr[j]):
+                j+=1
+            ans  = min(ans , j-i-1)
+            i+=1
+
+        return ans 
