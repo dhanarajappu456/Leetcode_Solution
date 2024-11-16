72ad0c1264744197d2a92b140b71373f05f81ac4
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-11-16 01:05:59 +0000

    Time: 11 ms (86.96%) | Memory: 30.4 MB (90.12%) - LeetSync

--- 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.py
+++ 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.py
@@ -8,10 +8,16 @@ class Solution:
 
         #in case 15, 10,1,2,3,4,5,6 
         #front portion might need ro be removed which is == j 
-        print(j)
+      
         ans  = min(ans, j)
 
-
+        #there will be portion that need to be removed will be in between
+        #1,2,8,[2,4],30,40,50,60 - > ans =2, remove [2,4]
+        #10,20,30,32,38,40,[54,60],43,44,48,49,50 ans 2 , remove[54,60]
+        
+        # incrementing i , if nums[i] <= nums[j]
+        # else , we may try incrementing j, till nums[j]< nums[i]
+        
         while(i<j and (i==0 or arr[i]>= arr[i-1])) :
             while(j<n and arr[i]>arr[j]):
                 j+=1
