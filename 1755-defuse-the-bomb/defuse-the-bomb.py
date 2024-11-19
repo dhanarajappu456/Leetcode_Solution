c8a563440ce46a1bd51986383c945bc039b4787e
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-11-19 01:33:40 +0000

    Time: 0 ms (100.00%) | Memory: 16.7 MB (23.22%) - LeetSync

--- 1755-defuse-the-bomb/defuse-the-bomb.py
+++ 1755-defuse-the-bomb/defuse-the-bomb.py
@@ -2,12 +2,23 @@ class Solution:
     def decrypt(self, code: List[int], k: int) -> List[int]:
         N = len(code)
         res = [0] * N
-
+        win_sum = 0 
+        if k!=0:
+        
+            win_sum = sum(code[1:k+1]) if k>0 else sum (code[k:])
+        print(win_sum)
         for i in range(N):
-            if k > 0:
-                for j in range(i + 1, i + 1 + k):
-                    res[i] += code[j % N]
-            elif k < 0:
-                for j in range(i - 1, i - 1 - abs(k), -1):
-                    res[i] += code[j % N]
+            res[i] = win_sum
+            if k<0:
+
+                win_sum -= code[(N+k+i)%N]
+                win_sum+= code[i%N]
+                 
+            elif k>0:
+                
+                win_sum -= code[(i+1)%N]
+                win_sum+= code[(i+k+1)%N]
+
+
+           
         return res
