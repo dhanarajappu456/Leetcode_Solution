54203d6e465d0d07ad3cd85c6fb792b4c5fc7f12
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-11-26 23:46:21 +0000

    Time: 107 ms (22.84%) | Memory: 25.4 MB (6.04%) - LeetSync

--- 2089-maximum-matrix-sum/maximum-matrix-sum.py
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.py
@@ -3,48 +3,21 @@ class Solution:
         nums =  matrix
         m  = len(matrix)
         n = len(matrix[0])
-        ans = 0 
+        total = 0
+        row_neg_counts = 0
+        col_neg = 0
+        min_abs = abs(nums[0][0])
         for i in range(m):
-            count_neg  = 0
-            min_abs_ind = 0
-            min_abs_val = float("inf")
-            total_row = 0
+            row_neg_counts = 0
             for j in range(n):
-                
-                if abs(nums[i][j])<=min_abs_val:
-                    min_abs_ind = j
-                    min_abs_val = abs(nums[i][j])
-                #change everythin to +ve
-               
-                if nums[i][j]<0:
-                    count_neg+=1
-                nums[i][j] = abs(nums[i][j])
-                total_row += abs(nums[i][j])
-                
-            #change the min in the row to -ve  
-            if count_neg%2  == 1 :
-                nums[i][min_abs_ind] = -abs(nums[i][min_abs_ind])
-            
-            ans+= total_row
-        print(ans)
-        print(matrix)
-        count_neg = 0 
-        min_abs_val = float("inf")
-        tot_neg = 0
-        for j in range(n):
-            
-            
-            for i in range(m):
-
-                if abs(nums[i][j])<=min_abs_val:
-                    min_abs_val = abs(nums[i][j])
-                if nums[i][j]<0:
-                    tot_neg+=1
-
-        if tot_neg%2  == 1 :
-               ans-= 2*min_abs_val
-
-        return ans
+                total+= abs(nums[i][j])
+                min_abs = min(abs(nums[i][j]),min_abs)
+                if nums[i][j]  <0:
+                    row_neg_counts+=1
+            col_neg += (row_neg_counts%2)
+        if col_neg %2 ==1:
+            total -= 2*min_abs
+        return total
 
 
         
