02927831604716bc75cf920bbc0990f86e2e2656
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-11-30 00:34:43 +0000

    Time: 901 ms (62.56%) | Memory: 34.8 MB (57.72%) - LeetSync

--- 2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.py
+++ 2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.py
@@ -12,31 +12,37 @@ class Solution:
         d[0][0] = grid[0][0]
         h.heapify(min_heap)
         dir_ = [(0,1),(0,-1),(1,0),(-1,0)]
-        dist_mat =[[float("inf") for j in range(n)] for i in range(m)]
-        dist_mat[0][0] = 0
+        time_mat =[[float("inf") for j in range(n)] for i in range(m)]
+        time_mat[0][0] = 0
         while(min_heap):
-            dist_,x,y= h.heappop(min_heap)
-            
+            time_,x,y= h.heappop(min_heap)
+          
             if (x== m-1) and (y == n-1):
-                return dist_
+                return time_
             for i,j in dir_:
                 a,b = x+i , y+j
                 if (0<=a<m) and (0<=b<n):
-                    # calculated time to reach neibr 
-                    # 1. when diff is odd and diff is even
-                    new_dist = 0 
-                    distance = grid[a][b] - dist_
-                    if  distance<=0:
-                        new_dist  = dist_+1
+                   
+                    next_time = 0 
+                    time_diff = grid[a][b] - time_
+                    if  time_diff<=0:
+                        #if neibr has a value <=time_taken so far
+                        #then time time to reach it is time_+1 
+                        #where time_ is , time taken so far 
+                        next_time  = time_+1
                     else:
-                        if  distance %2 ==0:
-                            new_dist = grid[a][b]+1
+                        # calculate time to reach neibr 
+                        # 1. when time diff is odd and diff is even
+                  
+                        if  time_diff %2 ==0:
+                        
+                            next_time = grid[a][b]+1
                         else:
-                            new_dist = grid[a][b]
+                            next_time = grid[a][b]
                     
-                    if new_dist < dist_mat[a][b]:
-                        dist_mat[a][b] = dist_
-                        h.heappush(min_heap,(new_dist ,a,b))
+                    if next_time < time_mat[a][b]:
+                        time_mat[a][b] = time_
+                        h.heappush(min_heap,(next_time ,a,b))
        
         return -1
 
