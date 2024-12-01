846016317b90250c85d9370be630ab36f699291b
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-12-01 11:27:29 +0000

    Time: 3 ms (57.43%) | Memory: 17.3 MB (5.25%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
@@ -3,6 +3,8 @@ class Solution:
         cnts = Counter(arr)
         for num in cnts:
             if 2*num in arr:
+                #if num ==0 then there should be more than 1 0's 
+                #in the array
                 if (num  == 0 and cnts[0]>1) or (num!=0):
                     return True
         return False
