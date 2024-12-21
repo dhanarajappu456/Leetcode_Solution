d3e986c513481cdf33ef6aaad53a7b378ae95832
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-12-21 13:06:29 +0000

    Time: 307 ms (7.32%) | Memory: 43.8 MB (8.54%) - LeetSync

---
+++ 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.py
@@ -0,0 +1,42 @@
+class Solution:
+    def maxKDivisibleComponents(self, n: int, edges: List[List[int]], values: List[int], k: int) -> int:
+        
+        adj  = defaultdict(list)
+
+        for a,b in edges: 
+            adj[a].append(b)
+            adj[b].append(a)
+
+        for a in range(0,n):
+            if len(adj[a])<=1:
+                root = a 
+                break
+        root = 0 
+        self.cnt = 0
+
+
+        vis = set()
+        print(root)
+        print(adj)
+        def rec(root):
+            print("idn")
+            vis.add(root)
+            ans  = values[root]
+            for nb in adj[root]:
+                if nb not in vis:
+                    val = rec(nb)
+
+                    if (val!=-1) and(  val%k ==0):
+                        print("fdkjdfs")
+                        self.cnt+=1
+                    else:
+                        if val!=-1:
+                            ans+= val
+            if ans%k==0:
+               
+                self.cnt +=1
+                print("fd",self.cnt)
+                ans = -1
+            return ans
+        rec(root)
+        return self.cnt
