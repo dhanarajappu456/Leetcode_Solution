f85963c532ad8f122bd62b30de70409ddd3a6148
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-12-21 00:44:23 +0000

    Time: 0 ms (100.00%) | Memory: 18.5 MB (6.13%) - LeetSync

--- 665-non-decreasing-array/non-decreasing-array.py
+++ 665-non-decreasing-array/non-decreasing-array.py
@@ -1,3 +1,19 @@
+#when there is a conflict , 
+'''
+it is always better to make the
+nums[i-1] reduced to nums[i], until unless it is not possible 
+that is nums[i-2] would conflict , if we change that .
+In that case, we increase the nums[i] to nums[i-1]
+
+
+
+when we chage we increase the counter , and 
+we have already made a change , and need to solve the conflict 
+again in another location ,  we return false .
+
+else if we just need to change at max one time the conflict 
+then we return true
+'''
 class Solution:
     def checkPossibility(self, nums: List[int]) -> bool:
         
