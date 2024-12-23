af779d647fb0ee62d238e88da80a3015f1f6bb18
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-12-23 17:21:04 +0000

    Time: 175 ms (40.52%) | Memory: 50.6 MB (13.73%) - LeetSync

--- 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/minimum-number-of-operations-to-sort-a-binary-tree-by-level.py
+++ 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/minimum-number-of-operations-to-sort-a-binary-tree-by-level.py
@@ -1,39 +1,35 @@
-from collections import deque
-from typing import Optional
-
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def minimumOperations(self, root: Optional[TreeNode]) -> int:
         
-        ans = 0
+        ans  =  0
         q = deque([root])
-        
         def conflict_cnt(arr):
             sorted_ = sorted(arr)
-            cnt = 0
-            mp = {num: i for i, num in enumerate(arr)}
-            
+            cnt =  0 
+            mp = { num : i for i,num in enumerate(arr)}
+      
             for i in range(len(arr)):
-                if arr[i] != sorted_[i]:
+                if arr[i]!= sorted_[i]:
+                    #put correct element at i , by swwpping
                     j = mp[sorted_[i]]
-                    # Swap elements
-                    arr[i], arr[j] = arr[j], arr[i]
-                    # Update the mapping
+                    arr[i] , arr[j] =  arr[j] , arr[i]
+                    cnt+=1
+                    #update the mp, after swapping
+                    mp[arr[i]] = i 
                     mp[arr[j]] = j
-                    mp[arr[i]] = i
-                    cnt += 1
+          
          
             return cnt
 
-        while q:
+        while(q):
             l = len(q)
-            arr = []
+            arr =[]
             for i in range(l):
                 nd = q.popleft()
                 if nd.left:
@@ -43,8 +39,7 @@ class Solution:
                     arr.append(nd.right.val)
                     q.append(nd.right)
         
-            cnt = conflict_cnt(arr)
-           
+            cnt   = conflict_cnt(arr)
+            print(cnt)
             ans += cnt
-        
-        return ans
+        return ans 
