ad3067c2663f568900cc7f168a1c4360a234d22a
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-12-28 13:51:19 +0000

    Time: 3 ms (88.86%) | Memory: 19 MB (22.85%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,42 +1,36 @@
-#cycle detection in dir grpah
-from collections import defaultdict
+from collections import defaultdict as dic
+
 
 #make each course that can be succesfully completed to the dict as True,
 
 # when the same course appear in ehe same path , then return False
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        topo=[]
+        q=[]
+        v= numCourses
+        e = prerequisites
+        indegree = [0 for i in range(v)]
+        adj = dic(list)
 
-
-        ableToFinish = dict()
-        adj = defaultdict(list)
-        for s,e in prerequisites:
-            adj[s].append(e)
+        for i in range(len(prerequisites)):
+            indegree[prerequisites[i][1]] += 1
+            adj[prerequisites[i][0]].append(prerequisites[i][1])  
+        
+        for i in range(v):
+            if indegree[i]==0:
+                q.append(i)
+   
+        while q:
+            front = q.pop(0)
+            topo.append(front)
+            for node in adj[front]:
+                indegree[node]-=1
+                if indegree[node]==0:
+                    q.append(node)
       
+        return len(topo)== v
 
-        def dfs(node):
-          
-            if node in ableToFinish:
-       
-                return ableToFinish[node]
             
-            ableToFinish[node] = False
-
-
-            for neib in adj[node]:
-
-                if dfs(neib) == False:
-                    return False
-            
-            ableToFinish[node] = True
-            return True 
-
-
-        for i in range(numCourses):
-        
-            if dfs(i ) ==False:
-                return False
-        return True
-        
 
         
