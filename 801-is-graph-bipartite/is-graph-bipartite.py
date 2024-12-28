11925bb093fc30c66654a66ed75affcc13e55b6e
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-12-28 13:09:00 +0000

    Time: 4 ms (25.67%) | Memory: 18.5 MB (6.38%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.py
+++ 801-is-graph-bipartite/is-graph-bipartite.py
@@ -1,33 +1,23 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-
-        
-
-        def rec(node,prevColor):
-
-            
-            if color[node]!=-1:
-                #take care of both parent and other nodes
-                return prevColor!=color[node]
-
-            color[node] = 1-prevColor
-            
-            for  neib in graph[node]:
-                
-                if rec(neib, color[node]) == False:
-                    return False
-            return True
-        n = len(graph)
-        color =[ -1 for i in range(n)]
-        
+        n=len(graph)
+        color =[-1 for i in range(n)]
         for i in range(n):
             if color[i]==-1:
-
-                if rec(i,0) == False :
+                    if self.dfs(i,graph,color,-1) == False:
+                        return False
+        return True
+    def dfs(self,node,graph,color,parent):
+        if parent == - 1:
+            color[node]="B"
+        else:
+            color[node] = "G" if color[parent]=="B" else "B"
+        for i in  graph[node]:
+            if color[i] == -1:
+                if self.dfs(i,graph,color,node) ==False:
                     return False
-        return  True
-        
-
-
-
-        
+           
+            if color[i]==color[node]:
+                return False
+        return True
+                
