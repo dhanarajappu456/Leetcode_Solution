9f4fc5a79c0e5e24b1c7fbffe8caa03dd2f54b33
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-12-29 01:15:34 +0000

    Time: 187 ms (12.33%) | Memory: 78.2 MB (5.20%) - LeetSync

--- 689-maximum-sum-of-3-non-overlapping-subarrays/maximum-sum-of-3-non-overlapping-subarrays.py
+++ 689-maximum-sum-of-3-non-overlapping-subarrays/maximum-sum-of-3-non-overlapping-subarrays.py
@@ -1,35 +1,46 @@
 class Solution:
     def maxSumOfThreeSubarrays(self, nums, k):
-        n = len(nums)
-        sum1 = sum(nums[:k])
-        sum2 = sum(nums[k:2*k])
-        sum3 = sum(nums[2*k:3*k])
+        n  = len(nums)
+        sub_sums = [sum(nums[:k])]
 
-        max1 = sum1
-        max12 = sum1 + sum2
-        max123 = sum1 + sum2 + sum3
+        for i in range(1,n-k+1):
+            sm = sub_sums[-1]+nums[i+k-1] -nums[i-1]
+            sub_sums.append(sm)
+        print(sub_sums)
+        @lru_cache(None)
+        def max_sm(i,cnt):
 
-        index1 = 0
-        index12_1 = 0
-        index12_2 = k
-        ans = [0, k, 2 * k]
+            if (cnt  ==0) or   (i ==n):
+                return  0
 
-        for i in range(1, n - 3 * k + 1):
-            sum1 = sum1 - nums[i - 1] + nums[i + k - 1]
-            sum2 = sum2 - nums[i + k - 1] + nums[i + 2 * k - 1]
-            sum3 = sum3 - nums[i + 2 * k - 1] + nums[i + 3 * k - 1]
+            tk = 0
+            not_ =0 
+            if i<=n-k:
+                tk = sub_sums[i]+ max_sm(i+k, cnt-1) 
+                not_ = max_sm(i+1,cnt)
 
-            if sum1 > max1:
-                max1 = sum1
-                index1 = i
+            return max(tk,not_)
+        i=0 
+        cnt= 3
+        
+        choosen =[]
+       
+        while((len(choosen)<3 ) and (i<=(n-k))):
+            tk = sub_sums[i] + max_sm(i+k,cnt-1)
+            not_ =  max_sm(i,cnt)
+            '''
+            remember we need to choose first i if 
+            tk and not_tk gives a tie, 
+            that is why we in case tk_ == not_ 
+            also we append i
+            '''
+            if tk>=not_:
+                choosen.append(i)
+                i+=k
+                cnt-=1
+            else:
+                i+=1
+                
+        return choosen
 
-            if max1 + sum2 > max12:
-                max12 = max1 + sum2
-                index12_1 = index1
-                index12_2 = i + k
-
-            if max12 + sum3 > max123:
-                max123 = max12 + sum3
-                ans = [index12_1, index12_2, i + 2 * k]
-
-        return ans
+     
