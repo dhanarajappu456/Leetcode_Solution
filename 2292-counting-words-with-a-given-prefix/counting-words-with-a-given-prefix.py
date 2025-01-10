6ba091a0aa5eb853ef2001ca4ac39a1dd13ae517
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-01-10 00:05:52 +0000

    Time: 39 ms (5.73%) | Memory: 18.8 MB (9.50%) - LeetSync

--- 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.py
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.py
@@ -1,9 +1,42 @@
 class Solution:
     def prefixCount(self, words: List[str], pref: str) -> int:
+        
+        class Node:
+            def __init__(self):
+                self.children = {}
+                self.cnt = 0 
+            
+            def get(self, c):
+                if c in self.children:
+                    node  = self.children[c]
+                    return node
+                return False
+            def put(self,c):
+                self.children[c] = Node()
+                return True
+        class Trie:
+
+            def __init__(self):
+                self.root  = Node()
+            
+            def insert(self,w):
+                node  = self.root
+                for c in  w:
+                    if c not in node.children:
+                        node.put(c) 
+                    node = node.get(c)
+                    node.cnt+=1
+                
+            def search(self, w):
+                node  = self.root
+                for c in  w:
+                    if c not in node.children:
+                        return (False, 0 )
+                    node = node.get(c)
+                return (True, node.cnt)           
         cnt = 0
+        trie  = Trie()
         for word in words:
-            if len(pref)<= len(word):
-                m = len(pref)
-                if word[:m] == pref:
-                    cnt+=1
+            trie.insert(word[:min(len(word),len(pref))])
+        present, cnt = trie.search(pref)
         return cnt
