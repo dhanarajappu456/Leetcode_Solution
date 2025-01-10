86f12fe8c97e652406525829fadf4a69498b87af
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-01-10 02:29:59 +0000

    Time: 298 ms (78.88%) | Memory: 21.4 MB (6.37%) - LeetSync

--- 952-word-subsets/word-subsets.py
+++ 952-word-subsets/word-subsets.py
@@ -1,13 +1,10 @@
 class Solution:
     def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
-        
         counts = defaultdict(int)
-
         for w in words2:
             curr_cnts = defaultdict(int)
             for c in w:
                 curr_cnts[c]+=1
-            
             for c in curr_cnts:
                 counts[c] = max(counts[c],curr_cnts[c])
         ans = set(words1)
