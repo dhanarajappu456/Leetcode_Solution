e6d540bd201d57b9b5959b95fafcafa26addc455
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-01-12 23:34:49 +0000

    Time: 107 ms (5.14%) | Memory: 17.6 MB (81.70%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.py
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.py
@@ -1,12 +1,58 @@
 class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
-        final=[]
-        for i in words:
-            for j in words:
-                if i!=j and i in j:
-                    
-                    final.append(i)
-                    break
-        return final            
+        n  = len(words)
+        def kmp(txt,pat):
+
+            
+            m,n  = len(pat),len(txt)
+            lps = [0 for i in range(m)]
+            j = 0
+            i =1 
+            while(i<m):
+                if pat[i] == pat[j]:
+                    lps[i] = j+1
+                    i+=1
+                    j+=1
+                else:
+                    if j>0:
+                        j = lps[j-1]
+                    else:
+                        i+=1
+          
+            j =0
+            i= 0
+
+            while(i<n):
+
+                if pat[j] == txt[i]:
+                    i+=1
+                    j+=1
                 
+                else:
+                    if j!=0:
+                        j =  lps[j-1]
+                    else:
+
+                        i+=1
+                if j == m:
+                    return True
+            return False
+                
+
+
+                
+
+
+
+        ans  = []
+        for i in range(n):
+            match_ = 1
+            for j in range(n):
+
+                if j!=i:
+                    if kmp(words[j], words[i]):
+                        ans.append(words[i])
+                        break
+        return ans
+
         
