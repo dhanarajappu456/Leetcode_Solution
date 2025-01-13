1b83d1e2c605294ab1e9ff3140510f18c02410de
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-01-13 18:52:43 +0000

    Time: 309 ms (14.28%) | Memory: 18.8 MB (32.70%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
@@ -9,8 +9,12 @@ class Solution:
             chars_cnt[c]+=1
         min_len = 0 
         for c in chars_cnt:
+            #if even number of a char is present , then at end
+            # 2 of it will be left 
             if chars_cnt[c]%2  == 0: 
                 min_len+=2
             else:
+                #if odd number of char is present , then at end
+                #1 of it will be left at the end
                 min_len+=1
         return min_len
