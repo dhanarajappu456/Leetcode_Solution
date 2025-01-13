19085106916ff9b31378686c33d0152712c94546
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-01-13 18:51:41 +0000

    Time: 296 ms (14.28%) | Memory: 18.8 MB (32.70%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
@@ -2,18 +2,15 @@ from collections import defaultdict as dict
 
 class Solution:
     def minimumLength(self, s: str) -> int:
-        chars_cnt = [0 for i in range(26)]
+        chars_cnt = dict(int)
         n = len(s)
-        min_len = n
         for i in range(n):
             c  = s[i]
-            val  = ord(c)-ord('a')
-            chars_cnt[val]+=1
-            #as soon as there are 3 chars available , we can delete the left
-            #and right chars, keep the middle one 
-            if chars_cnt[val] == 3:
-                min_len -=2
-                #persisting the middle char and assumed to moved to left most 
-                #postition in the 3 position
-                chars_cnt[val]=1
+            chars_cnt[c]+=1
+        min_len = 0 
+        for c in chars_cnt:
+            if chars_cnt[c]%2  == 0: 
+                min_len+=2
+            else:
+                min_len+=1
         return min_len
