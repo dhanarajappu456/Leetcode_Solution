6f3e0be8aa237cda0518d0237ce84c99bb134884
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-01-13 18:33:19 +0000

    Time: 442 ms (8.57%) | Memory: 18.7 MB (61.90%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
@@ -2,14 +2,14 @@ from collections import defaultdict as dict
 
 class Solution:
     def minimumLength(self, s: str) -> int:
-        d= dict(int)
-        
+        chars_cnt = [0 for i in range(26)]
         n = len(s)
         min_len = n
         for i in range(n):
             c  = s[i]
-            d[c]+=1
-            if d[c] == 3:
+            val  = ord(c)-ord('a')
+            chars_cnt[val]+=1
+            if chars_cnt[val] == 3:
                 min_len -=2
-                d[c]=1
+                chars_cnt[val]=1
         return min_len
