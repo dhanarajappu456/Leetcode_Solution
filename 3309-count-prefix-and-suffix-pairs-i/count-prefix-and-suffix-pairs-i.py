2b165837f57478323edd5ee6e7f32594db8dbf7d
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-01-14 23:27:45 +0000

    Time: 39 ms (5.05%) | Memory: 17.8 MB (58.30%) - LeetSync

--- 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.py
+++ 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.py
@@ -37,6 +37,13 @@ class Solution:
     def countPrefixSuffixPairs(self, words: List[str]) -> int:
         ans = 0
         n = len(words)
+        #startinf from the end of the words list  ,we  make a 
+        '''
+         pref and suff trie, for the word words[j] and 
+        check for all the words words[i] , that comes  before it 
+        in the words list ,  if  it could be suff and pref of the 
+        word words[j]
+        '''
         for j in range(n - 1, 0, -1):
             w = words[j]
             pref_trie = Trie()
