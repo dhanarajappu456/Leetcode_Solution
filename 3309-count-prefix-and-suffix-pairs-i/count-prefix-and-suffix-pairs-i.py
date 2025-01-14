03d43028a1e50b360b280a8cd30c14a846a2518e
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-01-14 23:24:31 +0000

    Time: 67 ms (5.05%) | Memory: 17.8 MB (37.70%) - LeetSync

--- 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.py
+++ 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.py
@@ -1,23 +1,52 @@
+class Node:
+    def __init__(self):
+        self.arr=[None for i in range(26)]
+        self.flag=False
+    def contains(self,letter):
+        return self.arr[ord(letter)-97]!=None    
+    def put(self,letter , node):
+        self.arr[ord(letter)-97]=node
+    def get(self,letter):
+        return self.arr[ord(letter)-97]
+    def setEnd(self):
+        self.flag=True
+    def getEnd(self):
+        return self.flag==True
+class Trie:
+    def __init__(self):
+        self.root=Node()
+    def insert(self, word: str) -> None:
+        temp =self.root
+        for i in word :
+            if not temp.contains(i):
+                temp.put(i,Node())
+            temp=temp.get(i)    
+        temp.setEnd()    
+    def search(self, word: str) -> bool:
+        temp =self.root
+        for i in word :
+            if not temp.contains(i):
+                return False
+            temp=temp.get(i)    
+        return True 
+  
+
+
 class Solution:
     def countPrefixSuffixPairs(self, words: List[str]) -> int:
-        
-        def check(i,j):
-            pref,suff = 0, 0
-            if len(words[i])<= len(words[j]):
-                ln  = len(words[i])
-                return words[j].startswith(words[i]) and words[j].endswith(words[i])
-            return False                   
-                    
-
-
-        i=0
+        ans = 0 
         n   = len(words)
-        cnt =  0 
-        while(i<n-1):
-            j = i+1
-            while(j<n):
-                if check(i,j):
-                    cnt+=1
-                j+=1
-            i+=1
-        return cnt
+        for  j in range(n-1,0,-1):
+            w = words[j]
+            pref_trie = Trie()
+            suff_trie  = Trie()
+
+            pref_trie.insert(w)
+            suff_trie.insert(w[::-1])
+
+            for i in range(j):
+                if len(words[i]) <= len(words[j]):
+                    pat  = words[i]
+                    if pref_trie.search(pat) and suff_trie.search(pat[::-1]):
+                        ans+=1
+        return ans
