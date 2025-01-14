f583304ae2a713b5dabf63e2e43878b68a23d7a4
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-01-14 23:24:46 +0000

    Time: 31 ms (5.78%) | Memory: 17.8 MB (58.30%) - LeetSync

--- 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.py
+++ 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.py
@@ -1,52 +1,58 @@
 class Node:
     def __init__(self):
-        self.arr=[None for i in range(26)]
-        self.flag=False
-    def contains(self,letter):
-        return self.arr[ord(letter)-97]!=None    
-    def put(self,letter , node):
-        self.arr[ord(letter)-97]=node
-    def get(self,letter):
-        return self.arr[ord(letter)-97]
+        self.children = {}  # Using a dictionary instead of an array
+        self.flag = False
+    
+    def contains(self, letter):
+        return letter in self.children
+    
+    def put(self, letter, node):
+        self.children[letter] = node
+    
+    def get(self, letter):
+        return self.children.get(letter)
+    
     def setEnd(self):
-        self.flag=True
+        self.flag = True
+    
     def getEnd(self):
-        return self.flag==True
+        return self.flag
+
 class Trie:
     def __init__(self):
-        self.root=Node()
+        self.root = Node()
+    
     def insert(self, word: str) -> None:
-        temp =self.root
-        for i in word :
-            if not temp.contains(i):
-                temp.put(i,Node())
-            temp=temp.get(i)    
-        temp.setEnd()    
+        temp = self.root
+        for char in word:
+            if not temp.contains(char):
+                temp.put(char, Node())
+            temp = temp.get(char)    
+        temp.setEnd()
+    
     def search(self, word: str) -> bool:
-        temp =self.root
-        for i in word :
-            if not temp.contains(i):
+        temp = self.root
+        for char in word:
+            if not temp.contains(char):
                 return False
-            temp=temp.get(i)    
+            temp = temp.get(char)    
         return True 
-  
-
 
 class Solution:
     def countPrefixSuffixPairs(self, words: List[str]) -> int:
-        ans = 0 
-        n   = len(words)
-        for  j in range(n-1,0,-1):
+        ans = 0
+        n = len(words)
+        for j in range(n - 1, 0, -1):
             w = words[j]
             pref_trie = Trie()
-            suff_trie  = Trie()
+            suff_trie = Trie()
 
             pref_trie.insert(w)
             suff_trie.insert(w[::-1])
 
             for i in range(j):
                 if len(words[i]) <= len(words[j]):
-                    pat  = words[i]
+                    pat = words[i]
                     if pref_trie.search(pat) and suff_trie.search(pat[::-1]):
-                        ans+=1
+                        ans += 1
         return ans
