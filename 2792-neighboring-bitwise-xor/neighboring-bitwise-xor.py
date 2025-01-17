d05dfd39384a4ef501f960a41227ff5c85f75284
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-01-17 14:34:35 +0000

    Time: 58 ms (44.53%) | Memory: 22.2 MB (70.07%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.py
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.py
@@ -2,14 +2,8 @@ class Solution:
     def doesValidArrayExist(self, derived: List[int]) -> bool:
         
         n   = len(derived)
-     
-        original =[-1 for i in range(n)]
-        original[0] = 0
+        initial = 0 
+        current = 0
         for i in range(n):
-          
-            if original[(i+1)%n]==-1:
-                original[(i+1)%n] = derived[i] ^ original[i]
-            else:
-                if original[(i+1)%n] != (derived[i] ^ original[i] ):
-                    return False
-        return True
+            current = derived[i] ^ current    
+        return initial == current
