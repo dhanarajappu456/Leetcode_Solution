dfeac91eece07b46b2b202239c0838976d29674d
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-01-20 21:19:43 +0000

    Time: 165 ms (19.50%) | Memory: 47.3 MB (82.97%) - LeetSync

--- 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.py
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.py
@@ -1,37 +1,41 @@
 class Solution:
     def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
-        # 1.create the map that maps , the val to (i,j)
-        #2.for each value iterated over the arr mark a value visited
-        #in the corresponding col and row .
-        #3.at any point , when any row or col is completely visited, return 
-        #that col or row
-        val_ind = {}
-        cols_rem = {}
-        rows_rem = {}
-        
-        m,n = len(mat), len(mat[0])
-        #this map indicate , the number of univisted elements
-        #in each col
-        for j in range(n):
-            cols_rem[j] = m
-        #this map indicate , the number of univisted elements
-        #in each row
-        for i in range(m):
-            rows_rem[i] = n
-        
-        #creating the map that maps , val-> (i,j)
-        for i in range(m):
-            for j in range(n):
-                val_ind[mat[i][j]] = (i,j)
+        '''
+        solution 2 - space optimised little ,
+        If we store the  index of each val in the arr
+        in a map, 
+        1.then we can iterate over each row to see , what's the 
+        max ind in arr at which entire element of that row 
+        is marked visited
+        2.similary we can iterate over each col to see , what's the
+        max ind in arr at which entire elements in that col is marked
+        as visited
 
-        #when each val is visited in mat , corresponding row and col
-        #is marked (by decrementing the number of unvisited value in
-        #each of them)   
-        for k in range(len(arr)):
-            val  = arr[k]
-            i,j = val_ind[val]
-            rows_rem[i] -=1
-            cols_rem[j]-=1
-            if( rows_rem[i] == 0 ) or (cols_rem[j] == 0):
-                return  k
+
+
+
+        '''
+        val_ind = {}
         
+        ans  = len( arr)-1
+        m,n = len(mat), len(mat[0])
+        #maps each val to their ind in arr
+        for j in range(len(arr)):
+            val  = arr[j]
+            val_ind[val]  = j 
+        #for each row see max ind in arr , at which entire row is marked
+        for i in range(m):
+            max_ind  = 0 
+            for j in range(n):
+                val  = mat[i][j]
+                max_ind = max(max_ind , val_ind[val])
+            ans = min(ans, max_ind)
+        for j in range(n):
+            max_ind  = 0 
+            for i in range(m):
+                val  = mat[i][j]
+                max_ind = max(max_ind , val_ind[val])
+            ans = min(ans, max_ind)
+                
+        return ans
+       
