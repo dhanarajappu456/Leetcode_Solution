1cb86a067a14b5a9bd7d48660a1a660061a8972d
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-02-02 08:42:18 +0000

    Time: 0 ms (100.00%) | Memory: 17.8 MB (29.61%) - LeetSync

---
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
@@ -0,0 +1,33 @@
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+        rot_point = None
+        i = 0
+        n = len(nums)
+        while(i<n):
+            if (nums[(i-1)%n]>nums[i]<=nums[(i+1)%n]):
+                rot_point =i
+                break
+            if (nums[(i-1)%n]>=nums[i]<=nums[(i+1)%n]):
+                rot_point =i
+                
+
+
+               
+            i+=1
+        if rot_point == None:
+            return False
+        cnt = 0
+        j = rot_point
+        print(j)
+        while(j!=None and cnt!=n):
+            print("insid",j)
+            if (j!=rot_point) and (nums[(j-1)%n]>nums[j]):
+                print(nums[(j-1)%n],nums[j])
+                return False
+                
+            if (cnt!=n-1) and (nums[(j+1)%n]<nums[j]):
+                print(nums[(j+1)%n],nums[j])
+                return False
+            j = (j+1)%n
+            cnt+=1
+        return True
