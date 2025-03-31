11f7e15b7c66fe5bfc52e57d9794cb485d306483
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-03-31 18:08:20 +0100

    Time: 358 ms (7.85%) | Memory: 33.3 MB (8.68%) - LeetSync

--- 2681-put-marbles-in-bags/put-marbles-in-bags.py
+++ 2681-put-marbles-in-bags/put-marbles-in-bags.py
@@ -12,12 +12,19 @@ class Solution:
             sm = weights[i]+ weights[i-1]
             h.heappush(split_adj_sum_mx_heap , -sm)
             h.heappush(split_adj_sum_mn_heap , sm)
-      
+        #the values at the extreme ends gonna be end of 
+        #some parition , which contribute to the sum
         min_ = weights[0] + weights[-1]
         max_ = weights[0]  + weights[-1]
+
+        #we need to make k-1 splits for the k partition
         rem_splits  = k-1
         while(rem_splits):
+            #choose to split at a point where sum 
+            #of element on either side of the split is max
             max_ += -h.heappop(split_adj_sum_mx_heap)
+            #choose to split at a point where sum of 
+            #element on either side of split is  min
             min_ +=  heappop(split_adj_sum_mn_heap)
             rem_splits-=1
         return max_ - min_
