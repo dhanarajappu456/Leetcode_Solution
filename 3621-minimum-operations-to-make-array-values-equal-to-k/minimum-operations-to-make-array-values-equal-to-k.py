ef83d8037d35b21235a9d9ce696c87e2a25024ff
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-04-10 20:54:36 +0100

    Time: 65 ms (69.20%) | Memory: 17.8 MB (35.41%) - LeetSync

--- 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.py
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.py
@@ -1,24 +1,13 @@
 import heapq as h 
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
-
-        for num in nums:
-            if num < k:
-                return -1
-        
         cnt  = 0
-        arr = list(-num for num in set(nums))
-        # for num in arr: 
-        #     h.heappush(arr , -num)
-        h.heapify(arr)
-        print(arr)
-        while(arr and  -arr[0]!=k):
-            cnt+=1
-            h.heappop(arr)
+        for num in set(nums):
+            if num<k:
+                return -1
+            elif num >k :
+                cnt+=1
         return cnt
-
-
-        
         
 
 
