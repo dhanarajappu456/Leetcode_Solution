34fcbd3b3e873b7cec993e063539e36c029894d5
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-04-20 22:27:28 +0100

    Time: 3 ms (28.10%) | Memory: 18.1 MB (9.12%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.py
+++ 797-rabbits-in-forest/rabbits-in-forest.py
@@ -7,7 +7,22 @@ class Solution:
         for num in answers:
             mp[num]+=1 
    
+        '''
 
+        the idea is for a  answer[i], let's call it num
+        # for a num , max number of rabbits that has 
+        #a specific color is num+1
+        # so if you know count of rabbit with same color 
+        # max that could fit in a particular color is count  / num+1
+        # for rest it wuld be assigned a different color of same color 
+        #but in a different group , let this number be chunks
+        #this chunks is got by  math.ceil(count /( num +1) )
+        #total rabbits = chunks * (num+1) , WHERE each chunks 
+        #can max hold num+1 rabbits rabbits
+
+        t n
+        s n 
+        ''' 
         for num in mp:
             count = mp[num]
             chunk  =   math.ceil(count /( num +1) )
