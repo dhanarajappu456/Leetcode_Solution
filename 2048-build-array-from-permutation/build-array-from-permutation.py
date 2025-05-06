eb4f25265c5f90692533d5a06c2f4f137ab51007
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-05-06 16:13:09 +0100

    Time: 6 ms (6.80%) | Memory: 18.3 MB (13.03%) - LeetSync

--- 2048-build-array-from-permutation/build-array-from-permutation.py
+++ 2048-build-array-from-permutation/build-array-from-permutation.py
@@ -1,10 +1,20 @@
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
-        
-
-
-        ans  =  nums[::-1]
         n = len(nums)
+        
+        #making the value at index i such that of form
+        # old + new * a constant_value , here the const_val we chose is 5000
+        # this way it preserve old value which can be recovered by 
+        #nums[nums[i]] % 5000
+        # after the transoformation each index  will contain
+        # the new value that is supposed to be there
+        # which can be returned , by nums[i] // 5000
+
+        #transoformation to the array elements 
         for i in range(n):
-            ans[i]  = nums[nums[i]]
-        return ans
+            nums[i]  = nums[i] +  (nums[nums[i]] % 5000)*5000
+        
+        #getting the value at each index, back
+        for i in range(n):
+            nums[i]  =  nums[i] //5000
+        return nums
