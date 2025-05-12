63767c2ece2ca374d125d216b0f70ca5b682a0cd
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-05-12 12:36:21 +0100

    Time: 0 ms (100.00%) | Memory: 18.2 MB (25.23%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.py
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.py
@@ -2,14 +2,17 @@ class Solution:
     def flatten(self, root: Optional[TreeNode]) -> None:        
         def rec(root):
             if root  == None:
-                return None
-            a = rec( root.right)
-            b = rec( root.left)
-            if b:
-              b.right = root.right
+                return  None
+            right_tail= rec( root.right)
+            left_tail= rec( root.left)
+            if left_tail:
+              left_tail.right = root.right
+            #make sure to attach the lefr flattened list to the root.right , 
+            #iff it is not null ,otherwise it gonna give wrong ans , by attacjing none 
+            #to the root.right
             if root.left: 
                 root.right = root.left
-                root.left = None
-            return a or b or   root
+                root.left  = None
+            return right_tail or left_tail or   root
         rec(root)
         return root
