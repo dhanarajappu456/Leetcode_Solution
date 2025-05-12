baab328a0f3310e01164cdbeeb4327701f7e8aff
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-05-12 15:16:53 +0100

    Time: 67 ms (83.54%) | Memory: 56.5 MB (62.43%) - LeetSync

---
+++ 1554-minimum-time-to-collect-all-apples-in-a-tree/minimum-time-to-collect-all-apples-in-a-tree.py
@@ -0,0 +1,31 @@
+from collections import defaultdict as dict
+
+class Solution:
+    def minTime(self, n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
+
+
+
+        adj = dict(list)
+        for e in edges:
+
+            adj[e[0]].append(e[1])
+            adj[e[1]].append(e[0])
+        
+        # par to mark as parent, which is already visited
+        def rec(root,par):
+            if root == None:
+                return 0
+            time  =0 
+            ans = 0 
+            for nb in adj[root]:
+                if par!= nb:
+                    time += rec(nb,root)
+                
+            ans  += time
+            if hasApple[root] or time: 
+                ans+=1
+            #print(root, ans)
+            return ans
+        return max( 0 ,  (rec(0,-1) -1 )*2 )
+
+
