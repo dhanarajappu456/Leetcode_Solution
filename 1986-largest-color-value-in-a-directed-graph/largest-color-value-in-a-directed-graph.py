aac17c8adf0f6a9cd88427cf44083df714ff2654
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-05-26 12:45:18 +0100

    Time: 2105 ms (40.90%) | Memory: 121.8 MB (14.38%) - LeetSync

--- 1986-largest-color-value-in-a-directed-graph/largest-color-value-in-a-directed-graph.py
+++ 1986-largest-color-value-in-a-directed-graph/largest-color-value-in-a-directed-graph.py
@@ -2,42 +2,33 @@ from collections import defaultdict as dict
 #dp 
 class Solution:
     def largestPathValue(self, colors: str, edges: List[List[int]]) -> int:
-
-
         cycle  ={}
         memo= {}
         adj = dict(list)
         for s, e in edges:
             adj[s].append(e)
-          
-
         res = [0]
         freq = [0]
 
         def dfs(node):
-        
             if node in cycle:
-
                 #if cycle found we need to return cycle found(true), based on which -1 is returned
                 if cycle[node] ==True:
-                   
                     return True
                 #if the node is not part of cycle , rather, already processed, then return the cached res
                 else:
                     return memo[node]
-                
-            cycle[node]= True
-        
-            ans =[0 for i in range(26)]
+
+            cycle[node] = True
+            ans = [0 for i in range(26)]
             for neib in adj[node]:
                 pathCol = dfs(neib)
                 if  pathCol  ==  True: 
                     return True
                 else:
-                    #the pathCol  is an array of 26 col, so choose the max of each color among all paths
+                    #the pathCol is an array of 26 col, so lor among all paths
                     for  i in range(26):
                         ans[i] = max(ans[i] , pathCol[i])
-            
             col   = ord(colors[node])-97
             #increment count of this current node as well by 1 
             ans[col] +=1 
@@ -46,7 +37,6 @@ class Solution:
             #storing the maxfreq color in the global variable
             for i in range(26):
                 if ans[i]> freq[0] :
-                    res[0] =i
                     freq[0]= ans[i]
             return ans
         n =len(colors)
