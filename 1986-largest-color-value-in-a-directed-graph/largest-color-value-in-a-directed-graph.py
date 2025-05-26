192c18c7b4a4afea7be3cc205d815058cbccc90e
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-05-26 12:46:20 +0100

    Time: 2509 ms (8.77%) | Memory: 122 MB (14.38%) - LeetSync

--- 1986-largest-color-value-in-a-directed-graph/largest-color-value-in-a-directed-graph.py
+++ 1986-largest-color-value-in-a-directed-graph/largest-color-value-in-a-directed-graph.py
@@ -11,6 +11,7 @@ class Solution:
         freq = [0]
 
         def dfs(node):
+
             if node in cycle:
                 #if cycle found we need to return cycle found(true), based on which -1 is returned
                 if cycle[node] ==True:
@@ -29,16 +30,19 @@ class Solution:
                     #the pathCol is an array of 26 col, so lor among all paths
                     for  i in range(26):
                         ans[i] = max(ans[i] , pathCol[i])
+
             col   = ord(colors[node])-97
             #increment count of this current node as well by 1 
             ans[col] +=1 
             cycle[node] =False
             memo[node] = ans
+
             #storing the maxfreq color in the global variable
             for i in range(26):
                 if ans[i]> freq[0] :
                     freq[0]= ans[i]
             return ans
+            
         n =len(colors)
         for i in range(n):
             #cylce
