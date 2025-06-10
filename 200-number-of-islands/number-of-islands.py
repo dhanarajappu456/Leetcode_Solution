4c2c800f4f591b1b79922aa4e484163ec93ba8d2
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-06-10 14:38:12 +0100

    Time: 259 ms (42.19%) | Memory: 29 MB (6.02%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,47 +1,34 @@
-class Solution(object):
-    
-    
-    def numIslands(self, grid):
-        m,n = len(grid),len(grid[0])
-        visited=[[0 for i in range(n)] for j in range(m)]
-        islands=0
-        dir1=[0,1,0,-1]
-        dir2=[1,0,-1,0]
-        def bfs(x,y,m,n,grid,visited):
-            
-            
-            q=[(x,y)]
-            while(q):
-                print(q)
-                front = q.pop(0)
-                x,y = front[0],front[1]
-                for i in range(4):
-                    new_x = x+dir1[i]
-                    new_y = y+dir2[i]
-                    if(0<=new_x<m and 0<=new_y<n and visited[new_x][new_y]==0 and grid[new_x][new_y]=="1"):
-                        visited[new_x][new_y]=1
-                        q.append((new_x,new_y))
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+
+
+        m = len(grid)
+        n = len(grid[0])
+        def dfs(i,j):
+
+
+            #if out of bound return 0
+            # if visited return  0 
+            if i< 0 or j<0  or i>= m or  j>=n  or (i,j) in vis:
+                return 0 
+            if grid[i][j] =="0":
+                return 0
+            ans = 1 
+            vis.add((i,j))
+            ans += dfs(i+1, j)
+            ans += dfs(i-1,j)
+            ans += dfs(i,j+1)
+            ans += dfs(i, j-1)
+            return ans
+
+        
+        vis  = set()
+        ans = 0
         for i in range(m):
             for j in range(n):
-                
-                if visited[i][j]==0 and grid[i][j]=="1":
-         
-                    islands+=1
-                    visited[i][j]=1
-                    bfs(i,j,m,n,grid,visited)
-        return islands
-        
-        
-  
-            
-       
-                        
+                if (i,j) not in vis and grid[i][j] =="1":
+                    dfs(i,j)
+                    ans +=1 
+        return ans
 
-                
-                
-            
-                        
-                        
-                        
-                
         
