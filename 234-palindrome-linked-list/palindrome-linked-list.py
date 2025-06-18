91da307f910ed63dd52529e85342105725cffb76
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-06-18 19:32:33 +0100

    Time: 31 ms (63.47%) | Memory: 34.7 MB (82.62%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -1,43 +1,26 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-
-'''
-find the middle
-reverese the right portion after the middle , 
-now  traverese both portion with 2 pointers
-
-t n
-s 1
-'''
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         if head ==None and head.next==None:
             return True
-        
         middle = self.middle(head)
-        middle.next=self.reverse(middle.next)
-        temp=middle.next 
+        temp  = self.reverse(middle.next)
+        middle.next = None
         while(temp):
             if temp.val!=head.val:
                 return False
             temp=temp.next
             head=head.next
-            
         return True    
         
-    
     def middle(self,head):
-        fast=head
-        slow=head
+        fast = head
+        slow = head
         while(fast.next and fast.next.next):
-            fast=fast.next.next
-            slow= slow.next
+            fast = fast.next.next
+            slow = slow.next
         return slow
+
     def reverse(self,head):
-        
         prev=None
         curr,next_=head,head 
         while(curr):
@@ -46,7 +29,4 @@ class Solution:
             prev=curr
             curr=next_
         return prev    
-        
-    
-    
         
