098aa227b20a7eac33921830046153f4eae79639
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-06-19 19:13:37 +0100

    Time: 79 ms (83.92%) | Memory: 29 MB (73.61%) - LeetSync

--- 2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.py
+++ 2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.py
@@ -3,15 +3,24 @@ class Solution:
         
         nums.sort()
         n = len(nums)
-        i = 0
-        j = 0
-        cnt = 0 
-        while(i<n):
+        # i = 0
+        # j = 0
+        # cnt = 0 
+        # while(i<n):
            
-            while(j<n and ((nums[j]-nums[i])<=k)):
-                j+=1
-            cnt+=1
-            i = j
-        return cnt 
+        #     while(j<n and ((nums[j]-nums[i])<=k)):
+        #         j+=1
+        #     cnt+=1
+        #     i = j
+        # return cnt 
 
+
+        ans  = 1 
+        min_val = nums[0]
+     
+        for i in range(1,n):
+            if((nums[i] - min_val) > k):
+                ans += 1
+                min_val  = nums[i]
+        return ans
         
