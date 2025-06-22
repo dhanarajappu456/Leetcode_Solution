17e9897e9bff07ad1fca78da655b0fcb9fa7b9fd
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-06-22 10:50:33 +0100

    Time: 1840 ms (55.89%) | Memory: 18.1 MB (68.61%) - LeetSync

--- 3754-maximum-manhattan-distance-after-k-changes/maximum-manhattan-distance-after-k-changes.py
+++ 3754-maximum-manhattan-distance-after-k-changes/maximum-manhattan-distance-after-k-changes.py
@@ -15,13 +15,18 @@ class Solution:
                 w+=1
             if c == "E":
                 e+=1
-            
+            #current net manhattan dist
             current_man  = abs(w-e) + abs(s-n)
+            #max possible mandiat at a time
             steps = i+1
+            #extrea movement that can be achieved using the k moves
+            #note that on using each of the k moves, it increment the
+            #mandist by +2 
             extra_possible = 0 
             if current_man != steps:
                 wasted  = steps - current_man
                 extra_possible  =  min(2*k, wasted)
+            #this is the max man dist we can achieve at a poiny on using the k moves 
             final_man  =  current_man + extra_possible
 
             ans  = max(ans, final_man)
