92d1a4ccfa4fa4f18975d98f0294eae6e9ea005c
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-07-01 22:49:26 +0100

    Time: 0 ms (100.00%) | Memory: 17.6 MB (97.00%) - LeetSync

--- 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.py
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.py
@@ -2,27 +2,9 @@ import heapq as h
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
         cnt = 0 
-        #nlogn
-        h.heapify(nums)
-        while(nums):
-            #logn
-            mn = h.heappop(nums)
-            if mn ==0 :
-                continue
-            flag = False
-            #n
-            for i in range(len(nums)):
-                if nums[i] != 0:
-                    flag  =True
-                nums[i] = max(0, nums[i] - mn)
-            if flag:
-                cnt+=1
-            else:
-                return cnt+1
-        return cnt
+        unique_ele= set(nums)
+        unique_ln = len(unique_ele)
         
-    '''
-    time complexity  is nlogn
-    space complexity is n
-    '''
-
+        if 0 in unique_ele:
+            return unique_ln -1
+        return unique_ln
