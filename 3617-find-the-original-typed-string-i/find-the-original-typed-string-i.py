333f67d2f923ed22a15563df6f4d43f6f4c667f8
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-07-01 02:12:12 +0100

    Time: 31 ms (92.39%) | Memory: 17.7 MB (72.11%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.py
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.py
@@ -15,7 +15,11 @@ class Solution:
             count = 0
             while(j<n and  (word[j] == word[i])):
                 j+=1
-                count+=1     
+                count+=1
+            #every character except last contiguous set of characater ,
+            #contribute count -1 times 
+
+            #and last contiguous character contribute coount times
             ans+= max(0, count-1)   
             i = j
         return ans + 1 
