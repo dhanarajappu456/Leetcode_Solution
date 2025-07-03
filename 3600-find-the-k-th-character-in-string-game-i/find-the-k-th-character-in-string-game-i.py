767bd5de6c71cc4f3156a4b81869d190ee755532
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-07-03 19:18:49 +0100

    Time: 0 ms (100.00%) | Memory: 17.6 MB (85.53%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.py
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.py
@@ -1,4 +1,7 @@
 class Solution:
+    # the idea is the answer is  "a" + p shifts
+    #where p is the number of set bits in k-1, which is hte index value at which we 
+    #need to find the character
     def kthCharacter(self, k: int) -> str:
         index = k-1
         set_bits  = 0
