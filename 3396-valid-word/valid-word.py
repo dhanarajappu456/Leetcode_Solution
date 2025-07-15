ae74d5ba9a292e94a57c09dfb02ae077dcb3719f
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-07-15 20:04:20 +0100

    Time: 3 ms (18.53%) | Memory: 17.6 MB (86.06%) - LeetSync

---
+++ 3396-valid-word/valid-word.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isValid(self, word: str) -> bool:
+        vows = ["a","e","i","o","u","A","E","I","O","U"]
+        nums = ["0","1","2","3","4","5","6","7","8","9"]
+        def check(word):
+            dig = 0 
+            vow  =  0
+            cons = 0 
+            for c in word:
+                if c in vows:
+                    vow+=1
+                elif ( 0 <= ( ord(c) - ord('a') ) <= 26):
+                    cons+=1
+                elif ( 0 <= ( ord(c) - ord('A') ) <= 26):
+                    cons+=1
+                elif c in nums:
+                    dig+=1
+                else:
+                    print("haai")
+                    return False
+        
+            if (vow>=1) and (cons>=1 )and (len(word)>=3):
+                return True
+            return False
+        return check(word)
+   
+
+        
+
+            
