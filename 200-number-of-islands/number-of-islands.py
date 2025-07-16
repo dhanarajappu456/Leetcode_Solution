a0438fc8a002cbb62673ccaeaa2f58ec06e8b803
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-07-16 19:25:12 +0100

    Time: 269 ms (31.61%) | Memory: 29 MB (7.69%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,34 +1,24 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-
-
-        m = len(grid)
-        n = len(grid[0])
-        def dfs(i,j):
-
-
-            #if out of bound return 0
-            # if visited return  0 
-            if i< 0 or j<0  or i>= m or  j>=n  or (i,j) in vis:
-                return 0 
-            if grid[i][j] =="0":
-                return 0
-            ans = 1 
-            vis.add((i,j))
-            ans += dfs(i+1, j)
-            ans += dfs(i-1,j)
-            ans += dfs(i,j+1)
-            ans += dfs(i, j-1)
-            return ans
-
-        
+        m,n = len(grid ),  len(grid[0])
         vis  = set()
-        ans = 0
+        def out(i,j):
+            return i<0 or j<0 or i>=m  or j>=n
+
+        def dfs(i,j):
+            if out(i,j) or ((i,j) in vis ) or grid[i][j] =="0":
+                return
+            vis.add((i,j))
+            dfs(i+1, j)
+            dfs(i,j+1)
+            dfs(i-1, j)
+            dfs(i,j-1)
+        cnt  = 0 
         for i in range(m):
             for j in range(n):
-                if (i,j) not in vis and grid[i][j] =="1":
+                if  (i,j)   not in vis  and grid[i][j] == "1":
+                    cnt+=1
                     dfs(i,j)
-                    ans +=1 
-        return ans
+        return cnt
 
         
