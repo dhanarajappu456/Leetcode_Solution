3f4a398d8d79c81eb42109edf370af48d392e7f7
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-07-22 14:08:10 +0100

    Time: 205 ms (62.60%) | Memory: 28.8 MB (78.00%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.py
+++ 1813-maximum-erasure-value/maximum-erasure-value.py
@@ -1,7 +1,12 @@
 class Solution:
     def maximumUniqueSubarray(self, nums: List[int]) -> int:
         
-
+        #use the sliding window approach here, 
+        # we keep unique element in the window ,  and find the sum of the same
+        '''
+        t n 
+        s 1
+        '''
         n = len(nums)
         i,j  = 0,0
         set_ = set()
