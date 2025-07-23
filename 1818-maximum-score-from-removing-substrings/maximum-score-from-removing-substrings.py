7beeacd26f34e9c02462c9a70aaf46afd0156732
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-07-23 19:54:42 +0100

    Time: 228 ms (49.68%) | Memory: 19.5 MB (38.06%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.py
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.py
@@ -1,39 +1,26 @@
 class Solution:
     def maximumGain(self, s: str, x: int, y: int) -> int:
-        large  = ""
-        small =""
-        large_val  =  0
-        small_val  =  0
-        if x>y:
-            large ="ab"
-            small = "ba"
-            large_val  = x
-            small_val  = y
-        else:
-            large  = "ba"
-            small = "ab"
-            large_val  = y
-            small_val  = x
-
-
-
-        stk = []
-        ans = 0
-        for c in s:
-            if stk and stk[-1]+c == large:
-                stk.pop(-1)
-                ans += large_val
-            else:
-                stk.append(c)
-        stk1 =  []
-        for c in stk:
-            if stk1 and  stk1[-1]+c == small:
-                stk1.pop(-1)
-                ans += small_val
-            else:
-                stk1.append(c)
-            
-
-
-        return ans 
         
+        def remove_s(st,rm):
+            ans  = ""
+            stk  = []
+            for c in st:
+                if (stk and ( stk[-1] == rm[0] ) and (c == rm[1])):
+                    stk.pop(-1)
+                else:
+                    stk.append(c)
+       
+            ans = "".join(stk)
+            return ans
+    
+        n = len(s)
+        res = 0 
+        max_str =    "ab"  if x > y  else  "ba"
+        min_str =    "ab"  if x<=y   else  "ba"
+        str1 = remove_s(s,  max_str)
+        res  = (n-len(str1))//2 * (x if max_str =="ab" else y )
+
+        str2 = remove_s(str1, min_str)
+      
+        res += (len(str1)-len(str2))//2 * (x if min_str =="ab" else y )
+        return res
