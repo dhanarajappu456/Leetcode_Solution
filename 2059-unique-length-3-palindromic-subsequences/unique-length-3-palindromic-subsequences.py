8b6854f6366375cc4224415d34544cc073aea6f1
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-07-30 00:50:52 +0100

    Time: 2885 ms (5.31%) | Memory: 18.2 MB (68.54%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -1,52 +1,21 @@
 from collections import defaultdict as dict
-'''
-idea is simple, 
-
-we traverse the array and for each char as the middle char in the palindrome we see if we can make palindromes where endchar on either side is anyof the 26 letter, to find if the endchar present on either 
-side we keep a set which collect all the letter seen previously and 
- a dictionary that keeps the last index of a character 
 
 
- t n *26 
- s 26*26 the ans dict size 
- 
- (for any charcter as middle charcter at max ,we can have 26
-    plaindromes 
-    
-    for all 26 letter as middle char, we can therefore have 26*26
-    palindromes 
-   )
-
-'''
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-
+        last_occ = dict(int)
+        for i,c in enumerate(s):
+            last_occ[c] = i
         prev  = set()
-        #maps a character  c to set of plaindromes
-        # where c is the middle char of each palindrome in the set  
-        ans  = dict(set)
-        lastIndex = dict(int)
-
+        mid  = set()
+        ans = set()
         for i,c in enumerate(s):
-            lastIndex[c]= i 
-
-
-        for i,c in enumerate(s):
-            #checks if each character is present on left and right side of the current character as the middle character 
-            for endChar in "abcdefghijklmnopqrstuvwxyz":
-                if endChar in prev and lastIndex[endChar]>i:
-                    ans[c].add(endChar)
+            for j in range(26):
+                ch = chr( ord('a') + j)
+                if (last_occ[ch] > i  ) and (ch in prev):
+                    ans.add(ch+c+ch)
+            prev.add(c)
+        return len(ans)
             
-          
-            prev.add(c) 
-        
-        res =0 
-        #find total palindromes got
-        for c in ans:
-            res += len(ans[c])
-        return res 
-        
-        
-        
-        
+             
         
