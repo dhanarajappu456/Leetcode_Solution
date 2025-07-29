a0548725f92c70798e3e11d42cf5205c9a4ae0e2
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-07-29 01:02:30 +0100

    Time: 51 ms (19.25%) | Memory: 30 MB (71.50%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,24 +1,10 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        
-        # 0th index will always be part of the cycle where the duplicate number fall
-        s,f = 0, 0
-        
-        
-        while(True):
-            
-            s = nums[s]
-            f = nums[nums[f]]
-            #like the problem - finding the start of the linked list  
-            #when fast and slow meets , moving equidistant from this node and starting node , gives the star of cycle , 
-            #which is the duplicate number
-            
-            if s==f:
-                
-                t1 ,t2 = 0,s
-                while(t1!=t2):
-                    t1 = nums[t1]
-                    t2 = nums[t2]
-                
-                return t1
+        n = len(nums)
+        for i in range(n):
+            if nums[abs(nums[i]) -1] < 0:
+                return abs(nums[i])
+            else:
+                nums[abs(nums[i]) -1] = -nums[abs(nums[i]) -1]
+       
         
