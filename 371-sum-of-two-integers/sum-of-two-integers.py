f454ca2aa4c39dbf74142ee1813711f00caa6462
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-11-14 13:14:05 +0000

    Time: 0 ms (100.00%) | Memory: 17.5 MB (95.86%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.py
+++ 371-sum-of-two-integers/sum-of-two-integers.py
@@ -1,9 +1,14 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int: 
-        mask = (2**32) - 1 
+        #12 bit mask
+        mask = 0b111111111111
         while(b & mask):
             temp = a^b
             carry = (a&b)<<1
             a = temp
             b = carry
-        return a & mask if (a&mask) <= 2**31 - 1 else -(2**32) + (a & mask)
+        # 1. if a is unsinged , return the number as such( but remebere we care about
+        #only 11 bits)
+        # 2. else, we return  = -2**12 + unsigned of this number 
+        #which is the actual -ve numebr represented by this binary form
+        return a & mask if (a&mask) <= 2**11 - 1 else -(2**12) + (a & mask)
