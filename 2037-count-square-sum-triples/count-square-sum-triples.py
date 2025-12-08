d01c6c6710ebe60fa7b393f41525516f6a0ebe4d
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-12-08 10:07:41 +0000

    Time: 501 ms (22.83%) | Memory: 17.8 MB (33.15%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.py
+++ 2037-count-square-sum-triples/count-square-sum-triples.py
@@ -1,9 +1,12 @@
 class Solution:
     def countTriples(self, n: int) -> int:
         ans = 0 
-        for i in range(1,n+1):
-            for j in range(1,n):
-                if  i!=j and  ( i**2 +j**2)  <= n**2    and  ( int(math.sqrt(i**2 +j**2))  ==  math.sqrt(i**2 +j**2)):
+        for a in range(1,n+1):
+            for b in range(1,n):
+
+                # a and b should be such that , the sum of their sqaures should be less than n**2
+                # and also , a perfrect square,  which is what the condition is testing
+                if  a!=b and  ( a**2 +b**2)  <= n**2    and  ( int(math.sqrt(a**2 +b**2))  ==  math.sqrt(a**2 +b**2)):
                     ans+=1
         return ans
 
