f49c7d441c823bb17af2303412577c4a5d272665
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2025-12-22 16:11:16 +0000

    Time: 7 ms (38.46%) | Memory: 17.6 MB (96.15%) - LeetSync

--- 992-delete-columns-to-make-sorted-ii/delete-columns-to-make-sorted-ii.py
+++ 992-delete-columns-to-make-sorted-ii/delete-columns-to-make-sorted-ii.py
@@ -4,21 +4,27 @@ class Solution:
         m = len(strs[0])
         j = 0 
         cnt = 0 
+        #already_sorted[i] tells if the strs[i] and strs[i-1] is sorted or not
         already_sorted = [False for i in range(n)]
         #checks for each position
         while(j<m):
             i=1 
+            #flag for knowing if the current col is deleted
             col_deleted = False
-         
             while(i<n):
                 if  not(already_sorted[i]) and (strs[i][j] < strs[i-1][j]):
                     cnt+=1 
                     col_deleted = True
                     break
                 i+=1
+            
             if col_deleted:
                 j+=1
                 continue
+            #stroting the info if the strs[i] and strs[i-1] is sorted
+            #remeber we din stroe this information while traversing the strings in whti while i<n loop
+            #cuz there will be case where we stored it for  and index i , but for indexx > i we  come to know the the column j is entirely deleted in future 
+            # so we need the chnages , that is why we mark it at end of the loop , if the col_deleted = false
             for i in range(1,n):
                 already_sorted[i]|=( strs[i][j] > strs[i-1][j])
             
