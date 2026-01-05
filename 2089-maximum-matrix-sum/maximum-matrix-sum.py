9037ebc200ecf51043ee31ccbd5909b4b2698782
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2026-01-05 21:14:41 +0000

    Time: 96 ms (42.68%) | Memory: 26.8 MB (7.01%) - LeetSync

--- 2089-maximum-matrix-sum/maximum-matrix-sum.py
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.py
@@ -1,23 +1,31 @@
 class Solution:
     def maxMatrixSum(self, matrix: List[List[int]]) -> int:
-        nums =  matrix
-        m  = len(matrix)
-        n = len(matrix[0])
-        total = 0
-        row_neg_counts = 0
-        col_neg = 0
-        min_abs = abs(nums[0][0])
-        for i in range(m):
-            row_neg_counts = 0
-            for j in range(n):
-                total+= abs(nums[i][j])
-                min_abs = min(abs(nums[i][j]),min_abs)
-                if nums[i][j]  <0:
-                    row_neg_counts+=1
-            col_neg += (row_neg_counts%2)
-        if col_neg %2 ==1:
-            total -= 2*min_abs
-        return total
+        cnt  = 0 
+        cnt_neg = 0 
+        tot_sum = 0
+        min_abs_val = float("inf")
+        for arr in matrix:
+            row_neg = 0 
+            for n1 in arr:
+                if n1<0:
+                    row_neg +=1 
+                tot_sum+= abs(n1)
+                min_abs_val  = min(min_abs_val, abs(n1))
+            if row_neg%2 ==1:
+                cnt_neg +=1
+        #if number pf rows in  which odd number of neg number exist is odd 
+        if cnt_neg%2 ==1:
+            return tot_sum  - 2*min_abs_val
+        else:
+            return  tot_sum
+
+        
+        
+
+        if cnt%2==1:
+            return tot_sum-1
+        return tot_sum
 
 
+        
         
