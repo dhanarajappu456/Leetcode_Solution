9683b0b8ebe847b00924378f30c5a68752b5fb9a
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2026-01-27 14:03:09 +0000

    Time: 705 ms (51.31%) | Memory: 79.4 MB (17.66%) - LeetSync

--- 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.py
+++ 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.py
@@ -6,28 +6,30 @@ class Solution:
         adj  = defaultdict(list)
         indegree = defaultdict(list)
 
-        #
+        #create the indegree  node and their 2*wt
         for a,b,w in edges:
             adj[a].append((b,w))
-            indegree[b].append((a,w))
+
+            indegree[b].append((a,2*w))
 
         min_heap =[(0,0)]
         d = [ float("inf") for i in range(n)]
         d[0] = 0
         while(min_heap):
-            #print( min_heap)
+
             node_wt, node = h.heappop(min_heap)
             if node  == n-1:
                 return d[node]
+            # push the nb  node to current node
             for nb,nb_wt in adj[node]:
                 if node_wt + nb_wt < d[nb]:
                     h.heappush(min_heap,( node_wt + nb_wt,nb))
                     d[nb] = node_wt +nb_wt
-
+            # push the nbs indegree to this current node
             for nb,nb_wt in indegree[node]:
-                if node_wt + 2*nb_wt < d[nb]:
-                    h.heappush(min_heap,( node_wt + 2*nb_wt, nb))
-                    d[nb] = node_wt +2*nb_wt
+                if node_wt + nb_wt < d[nb]:
+                    h.heappush(min_heap,( node_wt + nb_wt, nb))
+                    d[nb] = node_wt +nb_wt
             
 
 
