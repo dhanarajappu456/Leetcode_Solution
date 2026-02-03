6b30786f3900aeede2996929e3c9b063f44019ac
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2026-02-03 13:15:47 +0000

    Time: 0 ms (100.00%) | Memory: 19.4 MB (42.44%) - LeetSync

--- 3952-trionic-array-i/trionic-array-i.py
+++ 3952-trionic-array-i/trionic-array-i.py
@@ -1,6 +1,8 @@
 class Solution:
     def isTrionic(self, nums: List[int]) -> bool:
+        #cnt keeps track if there is incr or dec portion for each 3 of the compartment 
         cnt  = 0 
+        #if length is < 4 then we can have 3 parition 
         if len(nums) <4:
             return False
         n = len(nums)
